2f7b0416eae6586a2707781669b8100e1634ebee
bbenligiray <bbenligiray@gmail.com>
2020-06-19 15:40:41 +0300

    Use the dependencies directly for lint-staged

--- packages/contracts/src/Aggregator.test.ts
+++ packages/contracts/src/Aggregator.test.ts
@@ -8,8 +8,8 @@ describe('Aggregator', () => {
     const provider = new ethers.providers.Web3Provider(ganache.provider());
     const accounts = await provider.listAccounts();
     const signer = await provider.getSigner(0);
-    const contractArtifact =     JSON.parse(fs.readFileSync('build/contracts/Aggregator.json', 'utf8'));
-    const contractFactory = new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
+    const contractArtifact = JSON.parse(fs.readFileSync('build/contracts/Aggregator.json', 'utf8'));
+    const contractFactory =      new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
     contract = await contractFactory.deploy('5', accounts.slice(1, 8));
   });
   it('should have the correct minResponses', async () => {
