3198f654a2238c9e5a6bf4e1cb7ecfd5c471e179
André Ogle <ogleandre@gmail.com>
2020-06-20 11:45:58 +0200

    Use existing build script

--- packages/contracts/src/Aggregator.test.ts
+++ packages/contracts/src/Aggregator.test.ts
@@ -8,7 +8,7 @@ describe('Aggregator', () => {
     const provider = new ethers.providers.Web3Provider(ganache.provider());
     const accounts = await provider.listAccounts();
     const signer = await provider.getSigner(0);
-    const contractArtifact = JSON.parse(fs.readFileSync('./build/contracts/Aggregator.json', 'utf8'));
+    const contractArtifact = JSON.parse(fs.readFileSync('build/contracts/Aggregator.json', 'utf8'));
     const contractFactory = new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
     contract = await contractFactory.deploy('5', accounts.slice(1, 8));
   });
