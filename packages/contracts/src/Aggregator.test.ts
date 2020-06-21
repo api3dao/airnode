e5953e36589692d60bc51717daa9ff01091b352a
bbenligiray <bbenligiray@gmail.com>
2020-06-21 21:45:13 +0300

    Fix test for updated Aggregator

--- packages/contracts/src/Aggregator.test.ts
+++ packages/contracts/src/Aggregator.test.ts
@@ -3,16 +3,17 @@ const ganache = require('ganache-core');
 const ethers = require('ethers');
 
 describe('Aggregator', () => {
-  let contract;
+  let contractFactory;
+  let accounts;
   beforeEach(async () => {
     const provider = new ethers.providers.Web3Provider(ganache.provider());
-    const accounts = await provider.listAccounts();
+    accounts = await provider.listAccounts();
     const signer = await provider.getSigner(0);
     const contractArtifact = JSON.parse(fs.readFileSync('build/contracts/Aggregator.json', 'utf8'));
-    const contractFactory = new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
-    contract = await contractFactory.deploy('5', accounts.slice(1, 8));
+    contractFactory = new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
   });
   it('should have the correct minResponses', async () => {
+    const contract = await contractFactory.deploy(5, accounts.slice(1, 8), 0);
     const minResponses = (await contract.minResponses()).toString();
     expect(minResponses).toEqual('5');
   });
