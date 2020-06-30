d7420ce5cf7c10c5795c252cb02dca9a27f75af5
André Ogle <ogleandre@gmail.com>
2020-06-30 18:54:08 +0200

    Update config types

--- packages/node/src/core/config/index.ts
+++ packages/node/src/core/config/index.ts
@@ -1,23 +1,30 @@
 import { randomString } from '../utils/string-utils';
+import rawConfig from '../../../config.json';
 import rawSecurity from '../../../security.json';
-import rawSpecs from '../../../specs.json';
-import { SecuritySpecification, Specification } from './types';
+import { Config, OIS, SecuritySpecification } from './types';
 
-function parseSpecs(specs: any): Specification[] {
+function parseOises(oises: OIS[]): OIS[] {
   // Assign unique identifiers to each API and Oracle specification.
-  return specs.map((spec: Specification) => {
-    const oracleSpecifications = spec.oracleSpecifications.map((oracleSpec) => ({
+  return oises.map((ois) => {
+    const oracleSpecifications = ois.oracleSpecifications.map((oracleSpec) => ({
       ...oracleSpec,
       id: randomString(16),
     }));
 
-    const apiSpecifications = { ...spec.apiSpecifications, id: randomString(16) };
-    return { ...spec, apiSpecifications, oracleSpecifications };
+    const apiSpecifications = { ...ois.apiSpecifications, id: randomString(16) };
+    return { ...ois, apiSpecifications, oracleSpecifications };
   });
 }
 
+function parseConfig(config: Config): Config {
+  const ois = parseOises(config.ois);
+  return { ...config, ois };
+}
+
 // Cast the raw JSON files with the defined types
-export const specs = parseSpecs(rawSpecs) as Specification[];
+const typedConfig = rawConfig as Config;
+export const specs = parseConfig(typedConfig);
+
 export const security = rawSecurity as SecuritySpecification;
 
 // 600 blocks = roughly 1 hour in the past
