efc0eb8f4932eb2ffc09857fbf082790a20cd762
André Ogle <ogleandre@gmail.com>
2020-06-30 16:13:08 +0200

    Check fulfillter instead of requester

--- packages/node/src/core/handlers/flux-aggregator/event-detector.ts
+++ packages/node/src/core/handlers/flux-aggregator/event-detector.ts
@@ -77,7 +77,7 @@ function filterPendingRequests(oracleEvents: OracleLogEvent[]) {
     const newRequestsFromOtherNodes = newRequests.filter((nr) => nr.args.requester !== NODE_WALLET_ADDRESS);
 
     // We only care about fulfilled events for this node
-    const fulfillmentsForThisNode = fulfilledRequests.filter((fr) => fr.args.requester === NODE_WALLET_ADDRESS);
+    const fulfillmentsForThisNode = fulfilledRequests.filter((fr) => fr.args.fulfiller === NODE_WALLET_ADDRESS);
 
     // If there are any new requests from other nodes that this node has not yet fulfilled, add that to the list
     const isFulfilled = newRequestsFromOtherNodes.some((newRequest) => {
