2c159936b27dc1a6083e84fe75d88a68e20dc807
bbenligiray <bbenligiray@gmail.com>
2020-07-03 00:16:06 +0300

    Get the user flow script to work again

--- packages/chainapi/src/FullUserFlowScript.test.ts
+++ packages/chainapi/src/FullUserFlowScript.test.ts
@@ -4,19 +4,23 @@ const ethers = require('ethers');
 
 describe('Full user flow', () => {
   let ethProvider;
-  let contractFactory;
+  let chainApiContractFactory;
   let clientContractFactory;
   let accounts;
-  let requesterInterface;
+  let chainApiInterface;
 
   beforeEach(async () => {
     ethProvider = new ethers.providers.Web3Provider(ganache.provider());
     accounts = await ethProvider.listAccounts();
     const signer = await ethProvider.getSigner(0);
-    const contractArtifact = JSON.parse(fs.readFileSync('build/contracts/ChainApi.json', 'utf8'));
-    contractFactory = new ethers.ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
-    requesterInterface = new ethers.utils.Interface(contractArtifact.abi);
-    const clientContractArtifact = JSON.parse(fs.readFileSync('build/contracts/Client.json', 'utf8'));
+    const chainApiContractArtifact = JSON.parse(fs.readFileSync('build/contracts/ChainApi.json', 'utf8'));
+    chainApiContractFactory = new ethers.ContractFactory(
+      chainApiContractArtifact.abi,
+      chainApiContractArtifact.bytecode,
+      signer
+    );
+    chainApiInterface = new ethers.utils.Interface(chainApiContractArtifact.abi);
+    const clientContractArtifact = JSON.parse(fs.readFileSync('build/contracts/ExampleClient.json', 'utf8'));
     clientContractFactory = new ethers.ContractFactory(
       clientContractArtifact.abi,
       clientContractArtifact.bytecode,
@@ -27,51 +31,43 @@ describe('Full user flow', () => {
   it('works', async () => {
     // The contract deployer doesn't have any special powers
     // so it doesn't matter who deployed the contract (in this case, it's platformAgent)
-    const contract = await contractFactory.deploy();
+    const chainApiContract = await chainApiContractFactory.deploy();
     let tx, logs, log;
 
     // ~~~~~ Roles ~~~~~
     const platformAgent = accounts[0];
-    const contractAsPlatformAgent = contract.connect(ethProvider.getSigner(0));
     const providerAdmin = accounts[1];
-    const contractAsProviderAdmin = contract.connect(ethProvider.getSigner(1));
+    const chainApiContractAsProviderAdmin = chainApiContract.connect(ethProvider.getSigner(1));
     const requesterAdmin = accounts[2];
     const requesterSigner = ethProvider.getSigner(2);
-    const contractAsRequesterAdmin = contract.connect(ethProvider.getSigner(2));
+    const chainApiContractAsRequesterAdmin = chainApiContract.connect(ethProvider.getSigner(2));
     // ~~~~~ Roles ~~~~~
 
     // The provider signs up to ChainAPI. The dapp has the provider make the transaction below.
-    // The API subscribed for a month
-    let validUntilDate = new Date();
+    // The provider has subscribed for a month. The provider requires a minimum 0.05 ETH deposit
+    // for a requester to reserve a wallet.
+    const validUntilDate = new Date();
     validUntilDate.setMonth(validUntilDate.getMonth() + 1);
-    let validUntilTimestamp = (validUntilDate.getTime() / 1000) | 0;
-    tx = await contractAsProviderAdmin.createProvider(providerAdmin, platformAgent, validUntilTimestamp);
-
-    // ChainAPI checks that providerAdmin, platformAgent and validUntilTimestamp are correct
-    logs = await ethProvider.getLogs({ address: contract.address });
+    const validUntilTimestamp = (validUntilDate.getTime() / 1000) | 0;
+    const authorizationDeposit = ethers.utils.parseEther('0.05');
+    tx = await chainApiContractAsProviderAdmin.createProvider(
+      providerAdmin,
+      platformAgent,
+      validUntilTimestamp,
+      authorizationDeposit
+    );
+    logs = await ethProvider.getLogs({ address: chainApiContract.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    const providerId = requesterInterface.parseLog(log).args.id;
-    let provider = await contract.getProvider(providerId);
-    // If any of these assertions fail, we don't accept the providerId at the ChainAPI end
-    expect(provider.admin).toEqual(providerAdmin);
-    expect(provider.platformAgent).toEqual(platformAgent);
-    expect(provider.validUntil.toString()).toEqual(validUntilTimestamp.toString());
-
-    // The provider decided to extend their subscription for 2 months
-    // They pay ChainAPI and ChainAPI makes this transaction
-    validUntilDate = new Date(provider.validUntil * 1000);
-    validUntilDate.setMonth(validUntilDate.getMonth() + 2);
-    validUntilTimestamp = (validUntilDate.getTime() / 1000) | 0;
-    await contractAsPlatformAgent.extendProviderValidityDeadline(providerId, validUntilTimestamp);
+    const providerId = chainApiInterface.parseLog(log).args.id;
 
     // Provider defines the OIS that integrates their API to their node using ChainAPI GUI
 
     // Provider creates an endpoint with no authorizers (i.e., it's publicly accessible)
     // using the ChainAPI dapp
-    tx = await contractAsProviderAdmin.createEndpoint(providerId, []);
-    logs = await ethProvider.getLogs({ address: contract.address });
+    tx = await chainApiContractAsProviderAdmin.createEndpoint(providerId, []);
+    logs = await ethProvider.getLogs({ address: chainApiContract.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    const endpointId = requesterInterface.parseLog(log).args.id;
+    const endpointId = chainApiInterface.parseLog(log).args.id;
 
     // Provider creates a specs.json file using the ChainAPI GUI
     // where this endpointId is mapped to an oracle endpoint
@@ -83,82 +79,81 @@ describe('Full user flow', () => {
     // Do we want the user to be able to deploy a node with mnemonics?
     const providerHDNode = ethers.utils.HDNode.fromMnemonic(providerWallet.mnemonic.phrase);
     const providerXpub = providerHDNode.neuter().extendedKey;
-    await contractAsProviderAdmin.updateProviderXpub(providerId, providerXpub);
+    const authorizerWallet = providerHDNode.derivePath(`m/0/0`);
+    await chainApiContractAsProviderAdmin.initializeProviderKeys(providerId, providerXpub, authorizerWallet.address);
 
-    // The requester signs up to ChainAPI. The dapp has the provider make the transaction below.
-    tx = await contractAsRequesterAdmin.createRequester(requesterAdmin);
-    logs = await ethProvider.getLogs({ address: contract.address });
+    // The requester signs up to ChainAPI. The dapp has the requester make the transaction below.
+    tx = await chainApiContractAsRequesterAdmin.createRequester(requesterAdmin);
+    logs = await ethProvider.getLogs({ address: chainApiContractAsRequesterAdmin.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    const requesterId = requesterInterface.parseLog(log).args.id;
-    const requester = await contract.getRequester(requesterId);
-    // If any of these assertions fail, we don't accept the requesterId at the ChainAPI end
-    expect(requester).toEqual(requesterAdmin);
+    const requesterId = chainApiInterface.parseLog(log).args.id;
 
     // The requester asks to get a wallet allocated through the ChainAPI dapp
-    tx = await contractAsRequesterAdmin.allocateWallet(providerId, requesterId);
-    logs = await ethProvider.getLogs({ address: contract.address });
+    tx = await chainApiContractAsRequesterAdmin.reserveWallet(providerId, requesterId, { value: authorizationDeposit });
+    logs = await ethProvider.getLogs({ address: chainApiContractAsRequesterAdmin.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    // Maybe this should be allowed only if provider xpub is set
-    const walletInd = requesterInterface.parseLog(log).args.walletInd;
+    const walletInd = chainApiInterface.parseLog(log).args.walletInd;
+    const requesterHDNode = ethers.utils.HDNode.fromExtendedKey(providerXpub);
+    const reservedWallet = requesterHDNode.derivePath(`m/0/${walletInd}`);
+    // The provider received authorizationDeposit at their authorizerWallet
+    const walletAuthorizerBalance = await ethProvider.getBalance(authorizerWallet.address);
+    expect(walletAuthorizerBalance.toString()).toEqual(authorizationDeposit.toString());
+    // Now it will use that to authorize the reserved wallet to fulfill requests
+    const providerHDNodeAtRuntime = ethers.utils.HDNode.fromMnemonic(providerWallet.mnemonic.phrase);
+    const authorizerWalletNodeDerivedAgain = providerHDNodeAtRuntime.derivePath(`m/0/0`);
+    const authorizerWalletDerivedAgain = new ethers.Wallet(authorizerWalletNodeDerivedAgain.privateKey, ethProvider);
+    const contractAsAuthorizer = chainApiContract.connect(authorizerWalletDerivedAgain);
+    await contractAsAuthorizer.updateProviderWalletStatus(providerId, walletInd, reservedWallet.address);
+    // Sends the change to reservedWallet
+
     // The requester derives the address of the wallet it got allocated
-    provider = await contract.getProvider(providerId);
-    const requesterHDNode = ethers.utils.HDNode.fromExtendedKey(provider.xpub);
+
+    // const provider = await chainApiContract.getProvider(providerId);
     const allocatedWallet = requesterHDNode.derivePath(`m/0/${walletInd}`); // for example
-    let allocatedWalletBalance = await ethProvider.getBalance(allocatedWallet.address);
-    // It's empty
-    expect(allocatedWalletBalance.toString()).toEqual('0');
     // Requester funds it
     await requesterSigner.sendTransaction({
       to: allocatedWallet.address,
       value: ethers.utils.parseEther('1'),
     });
 
-    // IMPORTANT: We need to have the provider enable that wallet somehow
-    // Alternatively, the requester may request this specific wallet to respond to its request
-    await contractAsProviderAdmin.updateProviderWalletStatus(providerId, allocatedWallet.address, true);
-
     // Requester deploys their client contract and marks it as theirs
-    const clientContract = await clientContractFactory.deploy(requesterId);
+    const clientContract = await clientContractFactory.deploy(chainApiContract.address, requesterId);
 
     // Requester associates their contract with their ID
-    await contractAsRequesterAdmin.updateContractRequesterId(requesterId, clientContract.address);
+    await chainApiContractAsRequesterAdmin.endorseClient(requesterId, clientContract.address);
 
     // Requester creates a request template for the provider endpoint using the ChainAPI dapp
-    tx = await contractAsRequesterAdmin.createTemplate(
-      providerId,
-      endpointId,
-      ethers.BigNumber.from('500000'),
-      '0x12341234'
-    );
-    logs = await ethProvider.getLogs({ address: contract.address });
+    tx = await chainApiContractAsRequesterAdmin.createTemplate(endpointId, '0x12341234');
+    logs = await ethProvider.getLogs({ address: chainApiContractAsRequesterAdmin.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    const templateId = requesterInterface.parseLog(log).args.id;
+    const templateId = chainApiInterface.parseLog(log).args.id;
 
     // Requester client contract makes the request using the templateId
-    tx = await clientContract.request(contract.address, providerId, templateId);
-    logs = await ethProvider.getLogs({ address: contract.address });
+    tx = await clientContract.request(providerId, templateId, '0x123123');
+    logs = await ethProvider.getLogs({ address: chainApiContract.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
-    // const requestId = requesterInterface.parseLog(log).args.requestId;
+    //const requestId = chainApiInterface.parseLog(log).args.requestId;
 
     // Provider was listening for events from ChainApi with its providerId
     // and heard the log above. It first derives the wallet for the requester.
-    const requesterAddress = await requesterInterface.parseLog(log).args.requester;
-    const walletIndFromEvent = await contract.getWalletWithRequesterAddress(providerId, requesterAddress);
+    //const requesterAddress = await chainApiInterface.parseLog(log).args.requester;
+    // const requesterIdFromLog = await chainApiContract.getClientEndorserId(requesterAddress);
+    // TODO: get the wallets with requesterIdFromLog
+    //const walletIndFromEvent = await contract.getWalletWithRequesterAddress(providerId, requesterAddress);
     // Does the requester have a wallet allocated?
-    expect(walletIndFromEvent.toString()).not.toEqual('0');
+    //expect(walletIndFromEvent.toString()).not.toEqual('0');
     // Fetch the mnemonics from Secrets Manager and derive their wallet
-    const providerHDNodeAtRuntime = ethers.utils.HDNode.fromMnemonic(providerWallet.mnemonic.phrase);
     const allocatedWalletNodeAtRuntime = providerHDNodeAtRuntime.derivePath(`m/0/${walletInd}`);
     const allocatedWalletAtRuntime = new ethers.Wallet(allocatedWalletNodeAtRuntime.privateKey, ethProvider);
     // Probably check the wallet balance here
-    allocatedWalletBalance = await ethProvider.getBalance(allocatedWalletAtRuntime.address);
+    const allocatedWalletBalance = await ethProvider.getBalance(allocatedWalletAtRuntime.address);
     expect(allocatedWalletBalance.toString()).not.toEqual('0');
     // Fulfill the request with the wallet
-    const contractAsAllocatedWallet = contract.connect(allocatedWalletAtRuntime);
+    const contractAsAllocatedWallet = chainApiContract.connect(allocatedWalletAtRuntime);
     await contractAsAllocatedWallet.fulfillRequest(
-      requesterInterface.parseLog(log).args.callbackAddress,
-      requesterInterface.parseLog(log).args.callbackFunctionId,
-      requesterInterface.parseLog(log).args.requestId,
+      chainApiInterface.parseLog(log).args.callbackAddress,
+      chainApiInterface.parseLog(log).args.callbackFunctionId,
+      chainApiInterface.parseLog(log).args.requestId,
       '0x0000000000000000000000000000000000000000000000000000000000000001',
       {
         gasLimit: '500000',
@@ -170,9 +165,9 @@ describe('Full user flow', () => {
     expect(data.toString()).toEqual('0x0000000000000000000000000000000000000000000000000000000000000001');
 
     // The requester wants their money back now
-    tx = await contractAsRequesterAdmin.emptyWallet(providerId, requesterId, requesterAdmin);
+    tx = await chainApiContractAsRequesterAdmin.withdrawRequest(providerId, allocatedWallet.address, requesterAdmin);
     // The node is listening for this
-    logs = await ethProvider.getLogs({ address: contract.address });
+    logs = await ethProvider.getLogs({ address: chainApiContract.address });
     log = logs.filter((log) => log.transactionHash === tx.hash)[0];
     // TODO: Get balance, estimate transaction gas, send everything
     // Alternatively, we can send the transaction to ProviderStore to be redirected to the requester
