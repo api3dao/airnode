14c23a6fb199e6ab391bea13ae309b4aeadb57c5
André Ogle <ogleandre@gmail.com>
2020-07-07 14:49:14 +0200

    Remove ois dist files

--- packages/ois/dist/validator/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// TODO
-function validate() {
-    return;
-}
-exports.validate = validate;
-//# sourceMappingURL=index.js.map
