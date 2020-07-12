aaa27b18ee24e305c0b4efce3a2f72770d55482a
André Ogle <ogleandre@gmail.com>
2020-07-12 12:10:22 +0200

    Prettify

--- packages/adapter/src/response-processor/caster.test.ts
+++ packages/adapter/src/response-processor/caster.test.ts
@@ -99,10 +99,18 @@ describe('castValue', () => {
 
     it('throws an error for array values', () => {
       expect(() => caster.castValue([], 'bytes32')).toThrowError(new Error("Unable to convert: '[]' to bytes32"));
-      expect(() => caster.castValue([false], 'bytes32')).toThrowError(new Error("Unable to convert: '[false]' to bytes32"));
-      expect(() => caster.castValue([true], 'bytes32')).toThrowError(new Error("Unable to convert: '[true]' to bytes32"));
-      expect(() => caster.castValue(['unknown'], 'bytes32')).toThrowError(new Error("Unable to convert: '[\"unknown\"]' to bytes32"));
-      expect(() => caster.castValue([{ a: 1 }], 'bytes32')).toThrowError(new Error("Unable to convert: '[{\"a\":1}]' to bytes32"));
+      expect(() => caster.castValue([false], 'bytes32')).toThrowError(
+        new Error("Unable to convert: '[false]' to bytes32")
+      );
+      expect(() => caster.castValue([true], 'bytes32')).toThrowError(
+        new Error("Unable to convert: '[true]' to bytes32")
+      );
+      expect(() => caster.castValue(['unknown'], 'bytes32')).toThrowError(
+        new Error('Unable to convert: \'["unknown"]\' to bytes32')
+      );
+      expect(() => caster.castValue([{ a: 1 }], 'bytes32')).toThrowError(
+        new Error('Unable to convert: \'[{"a":1}]\' to bytes32')
+      );
     });
 
     it('converts values to strings', () => {
