a4a4b408ce6f049092d2705fa7dd8606d104cd3b
André Ogle <ogleandre@gmail.com>
2020-07-18 17:45:55 +0200

    Prettify

--- jest.config.base.js
+++ jest.config.base.js
@@ -83,10 +83,7 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: [
-    '<rootDir>/.build',
-    '<rootDir>/dist/',
-  ],
+  modulePathIgnorePatterns: ['<rootDir>/.build', '<rootDir>/dist/'],
 
   // Activates notifications for test results
   // notify: false,
