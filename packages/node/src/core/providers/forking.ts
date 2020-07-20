4695a0a5e67bcc63c57636874bc6ae1117dc03b7
André Ogle <ogleandre@gmail.com>
2020-07-20 14:06:15 +0200

    Prettify

--- packages/node/src/core/providers/forking.ts
+++ packages/node/src/core/providers/forking.ts
@@ -2,7 +2,7 @@ import { ProviderState } from '../../types';
 import * as ethereum from '../ethereum';
 import * as forking from '../forking';
 
-type CleanProviderState = Omit<ProviderState, 'provider'>
+type CleanProviderState = Omit<ProviderState, 'provider'>;
 
 export async function spawn(index: number): Promise<ProviderState> {
   const payload = forking.isLocal() ? { pathParameters: { index } } : { index };
@@ -10,7 +10,7 @@ export async function spawn(index: number): Promise<ProviderState> {
   const parameters = { functionName: 'initializeProvider', payload };
 
   // If this throws, it will be caught by the calling function
-  const initialState = await forking.spawn(parameters) as CleanProviderState;
+  const initialState = (await forking.spawn(parameters)) as CleanProviderState;
 
   // The serverless function does not return an instance
   // of an Ethereum provider, so we create a new one
