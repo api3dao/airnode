4b24008a316b10ecac06f867aa09f1619198b40d
André Ogle <ogleandre@gmail.com>
2020-07-21 11:24:30 +0200

    Update new provider timeout to 20 seconds

--- packages/node/src/core/coordinator/state.ts
+++ packages/node/src/core/coordinator/state.ts
@@ -8,13 +8,13 @@ export async function initialize(providerConfigs: ProviderConfig[]): Promise<Sta
     throw new Error('At least one provider must be defined in config.json');
   }
 
-  // Initialize each provider state (in parallel) with a maximum time limit of 10 seconds
+  // Initialize each provider state (in parallel) with a maximum time limit of 20 seconds
   //
   // Providers are identified by their index in the array. This allows users
   // to configure duplicate providers safely (if they want the added redundancy)
   const initializations = providerConfigs.map(async (_config, index) => {
     const initialization = spawnNewProvider(index);
-    const [err, state] = await goTimeout(10_000, initialization);
+    const [err, state] = await goTimeout(20_000, initialization);
     if (err) {
       return null;
     }
