a165a8189d6e905e051c81152a00137b0ac92fd5
bbenligiray <bbenligiray@gmail.com>
2020-07-27 15:36:26 +0300

    Add new convenience function to fetch multiple client data

--- packages/chainapi/contracts/interfaces/ConvenienceInterface.sol
+++ packages/chainapi/contracts/interfaces/ConvenienceInterface.sol
@@ -30,4 +30,18 @@ interface ConvenienceInterface {
             uint256 walletBalance,
             uint256 minBalance
             );
+
+    function getDataWithClientAddresses(
+        bytes32 providerId,
+        address[] calldata clientAddresses
+        )
+        external
+        view
+        returns (
+            bytes32[] memory requesterIds,
+            uint256[] memory walletInds,
+            address[] memory walletAddresses,
+            uint256[] memory walletBalances,
+            uint256[] memory minBalances
+            );
 }
