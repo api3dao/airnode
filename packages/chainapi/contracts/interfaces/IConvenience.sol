2db37436be0ab0878eb558037831630fd9e2dd50
bbenligiray <bbenligiray@gmail.com>
2020-07-31 14:48:09 +0300

    Format

--- packages/chainapi/contracts/interfaces/IConvenience.sol
+++ packages/chainapi/contracts/interfaces/IConvenience.sol
@@ -5,17 +5,17 @@ pragma experimental ABIEncoderV2;
 
 interface IConvenience {
     function getTemplates(bytes32[] calldata templateIds)
-          external
-          view
-          returns (
-              bytes32[] memory providerIds,
-              bytes32[] memory endpointIds,
-              address[] memory fulfillAddresses,
-              address[] memory errorAddresses,
-              bytes4[] memory fulfillFunctionIds,
-              bytes4[] memory errorFunctionIds,
-              bytes[] memory parameters
-          );
+        external
+        view
+        returns (
+            bytes32[] memory providerIds,
+            bytes32[] memory endpointIds,
+            address[] memory fulfillAddresses,
+            address[] memory errorAddresses,
+            bytes4[] memory fulfillFunctionIds,
+            bytes4[] memory errorFunctionIds,
+            bytes[] memory parameters
+        );
 
     function getDataWithClientAddress(
         bytes32 providerId,
