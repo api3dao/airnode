f3526b98adbf1ee2ff62dd90ee2099e539f5ced4
André Ogle <ogleandre@gmail.com>
2020-08-19 14:04:26 +0200

    Add log messages when each API call finishes

--- packages/node/src/core/coordinator/coordinated-api-caller.ts
+++ packages/node/src/core/coordinator/coordinated-api-caller.ts
@@ -9,17 +9,28 @@ export async function callApis(state: CoordinatorState): Promise<AggregatedApiCa
   const validAggregatedCalls = state.aggregatedApiCalls.filter((ac) => !ac.error);
 
   const calls = validAggregatedCalls.map(async (aggregatedApiCall) => {
+    const startedAt = new Date();
+    const logMsg = `API call to Endpoint:${aggregatedApiCall.endpointName}`;
+
     const [err, res] = await goTimeout(WORKER_TIMEOUT, spawnNewApiCall(aggregatedApiCall));
+
+    const finishedAt = new Date();
+    const durationMs = Math.abs(finishedAt.getTime() - startedAt.getTime());
+
     // If the worker crashes for whatever reason, mark the request as failed
     if (err || !res) {
+      logger.logJSON('ERROR', `${logMsg} errored after ${durationMs}. ${err}`);
       return { ...aggregatedApiCall, error: { errorCode: RequestErrorCode.ApiCallFailed } };
     }
 
     // If the request completed but has an errorCode, then it means that something
     // went wrong. Save the errorCode and message if one exists.
     if (res.errorCode) {
+      logger.logJSON('ERROR', `${logMsg} failed in ${durationMs}ms with error code:${res.errorCode}`);
       return { ...aggregatedApiCall, error: res as ApiCallError };
     }
+
+    logger.logJSON('INFO', `${logMsg} responded successfully in ${durationMs}ms`);
 
     // We can assume that the request was successful at this point
     return { ...aggregatedApiCall, response: res as ApiCallResponse };
