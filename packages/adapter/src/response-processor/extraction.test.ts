9e5bfbb841679410716dd56f77e309c8c8a882fe
André Ogle <ogleandre@gmail.com>
2020-08-20 10:55:59 +0200

    Update test wording

--- packages/adapter/src/response-processor/extraction.test.ts
+++ packages/adapter/src/response-processor/extraction.test.ts
@@ -2,21 +2,22 @@ import { getRawValue } from './extraction';
 
 describe('getRawValue', () => {
   it('returns the data as is if no path is provided', () => {
-    const obj = 'somevalue';
-    expect(getRawValue(obj)).toEqual('somevalue');
+    expect(getRawValue('somevalue')).toEqual('somevalue');
+    expect(getRawValue(false)).toEqual(false);
+    expect(getRawValue(500)).toEqual(500);
   });
 
-  it('gets the value from a deeply nested object', () => {
+  it('gets the value from a deeply nested object path', () => {
     const obj = { a: { b: [{ c: 1 }, { d: 5 }] } };
     expect(getRawValue(obj, 'a.b.1.d')).toEqual(5);
   });
 
-  it('gets the value from an array', () => {
+  it('gets the value from an array path', () => {
     const obj = [{ values: [422, 413, 401] }, { values: [731, 728, 739] }];
     expect(getRawValue(obj, '1.values.2')).toEqual(739);
   });
 
-  it('prefers the value from the array an ambiguous object', () => {
+  it('prefers the nested value from an ambiguous object', () => {
     const obj = {
       'foo.bar.0': { baz: 555 },
       foo: { bar: [{ baz: 888 }] },
