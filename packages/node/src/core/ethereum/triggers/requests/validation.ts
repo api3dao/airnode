907601a1a70b3979655908b6afeac27aa8f93220
André Ogle <ogleandre@gmail.com>
2020-09-16 16:08:30 +0200

    Ignore API calls and withdrawals where the wallet does not have enough funds

--- packages/node/src/core/ethereum/triggers/requests/validation.ts
+++ packages/node/src/core/ethereum/triggers/requests/validation.ts
@@ -2,9 +2,9 @@ import { ethers } from 'ethers';
 import flatMap from 'lodash/flatMap';
 import * as logger from '../../../utils/logger';
 import * as utils from '../../utils';
-import { ClientRequest, GroupedRequests, LogsErrorData, RequestErrorCode, RequestStatus } from '../../../../types';
+import { ApiCall, ClientRequest, GroupedRequests, LogsErrorData, RequestErrorCode, RequestStatus, Withdrawal } from '../../../../types';
 
-function validateRequest<T>(request: ClientRequest<T>): LogsErrorData<ClientRequest<T>> {
+function validateWalletIndex<T>(request: ClientRequest<T>): LogsErrorData<ClientRequest<T>> {
   // If the request is already invalid, we don't want to overwrite the error
   if (request.status !== RequestStatus.Pending) {
     return [[], null, request];
@@ -21,6 +21,15 @@ function validateRequest<T>(request: ClientRequest<T>): LogsErrorData<ClientRequ
     return [[log], null, validatedRequest];
   }
 
+  return [[], null, request];
+}
+
+function validateSufficientBalance<T>(request: ClientRequest<T>): LogsErrorData<ClientRequest<T>> {
+  // If the request is already invalid, we don't want to overwrite the error
+  if (request.status !== RequestStatus.Pending) {
+    return [[], null, request];
+  }
+
   const balance = utils.weiToBigNumber(request.walletBalance);
   const minBalance = utils.weiToBigNumber(request.walletMinimumBalance);
 
@@ -34,7 +43,7 @@ function validateRequest<T>(request: ClientRequest<T>): LogsErrorData<ClientRequ
     );
     const validatedRequest = {
       ...request,
-      status: RequestStatus.Errored,
+      status: RequestStatus.Ignored,
       errorCode: RequestErrorCode.InsufficientBalance,
     };
     return [[log], null, validatedRequest];
@@ -43,15 +52,43 @@ function validateRequest<T>(request: ClientRequest<T>): LogsErrorData<ClientRequ
   return [[], null, request];
 }
 
+function validateApiCall(request: ClientRequest<ApiCall>): LogsErrorData<ClientRequest<ApiCall>> {
+  const [indexLogs, indexErr, indexRequest] = validateWalletIndex(request);
+  if (indexRequest.status !== RequestStatus.Pending || indexErr) {
+    return [indexLogs, indexErr, indexRequest];
+  }
+
+  const [sufficientBalLogs, sufficientBalErr, sufficientBalRequest] = validateSufficientBalance(request);
+  if (sufficientBalRequest.status !== RequestStatus.Pending || sufficientBalErr) {
+    return [sufficientBalLogs, sufficientBalErr, sufficientBalRequest];
+  }
+
+  return [[], null, request];
+}
+
+function validateWithdrawal(request: ClientRequest<Withdrawal>): LogsErrorData<ClientRequest<Withdrawal>> {
+  const [sufficientBalLogs, sufficientBalErr, sufficientBalRequest] = validateSufficientBalance(request);
+  if (sufficientBalRequest.status !== RequestStatus.Pending || sufficientBalErr) {
+    return [sufficientBalLogs, sufficientBalErr, sufficientBalRequest];
+  }
+
+  return [[], null, request];
+}
+
 export function validateRequests(requests: GroupedRequests): LogsErrorData<GroupedRequests> {
-  const apiCallsWithLogs = requests.apiCalls.map((apiCall) => validateRequest(apiCall));
+  const apiCallsWithLogs = requests.apiCalls.map((apiCall) => validateApiCall(apiCall));
   const apiCallLogs = flatMap(apiCallsWithLogs, (a) => a[0]);
   const apiCalls = flatMap(apiCallsWithLogs, (a) => a[2]);
+
+  const withdrawalsWithLogs = requests.withdrawals.map((withdrawal) => validateWithdrawal(withdrawal));
+  const withdrawalLogs = flatMap(withdrawalsWithLogs, (w) => w[0]);
+  const withdrawals = flatMap(withdrawalsWithLogs, (w) => w[2]);
 
   const validatedRequests = {
     ...requests,
     apiCalls,
+    withdrawals,
   };
 
-  return [apiCallLogs, null, validatedRequests];
+  return [[...apiCallLogs, ...withdrawalLogs], null, validatedRequests];
 }
