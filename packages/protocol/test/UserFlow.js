1c617d126d0b58d985667b10ed54466a39ee04b0
bbenligiray <bbenligiray@gmail.com>
2020-10-04 00:04:09 +0300

    Update the user flow script

--- packages/protocol/test/UserFlow.js
+++ packages/protocol/test/UserFlow.js
@@ -1,58 +1,53 @@
-/* global ethers, waffle */
+// This is the documentation of the user flow more than a test
 const { expect } = require('chai');
 
-describe('Airnode', function () {
-  let roles;
+describe('User flow', function () {
   let airnode;
   let convenience;
   let airnodeClient;
-  let providerKey;
-  let providerId;
-  let requesterInd;
-  let designatedWalletAddress;
-  let endpointId;
-  let templateId;
+  let roles;
 
   beforeEach(async () => {
+    const accounts = await ethers.getSigners();
+    roles = {
+      deployer: accounts[0],
+      providerAdmin: accounts[1],
+      requesterAdmin: accounts[2],
+    };
+
     const airnodeFactory = await ethers.getContractFactory('Airnode');
     airnode = await airnodeFactory.deploy();
     const convenienceFactory = await ethers.getContractFactory('Convenience');
     convenience = await convenienceFactory.deploy(airnode.address);
-    const airnodeClientFactory = await ethers.getContractFactory('ExampleAirnodeClient');
+    const airnodeClientFactory = await ethers.getContractFactory('MockAirnodeClient', roles.deployer);
     airnodeClient = await airnodeClientFactory.deploy(airnode.address);
-    const accountList = await ethers.getSigners();
-    roles = {
-      providerAdmin: accountList[0],
-      requesterAdmin: accountList[1],
-    };
   });
 
   it('works', async function () {
-    // Normally, the Airnode would retrieve its key from a secret management service.
+    // Normally, Airnode would retrieve its key from a secret management service.
     // Here, we will generate it instead. Only the node will have access to
-    // providerKey.mnemonics, while providerKey.xpub will be publicly announced.
-    await generateProviderKey();
+    // providerMnemonic, while providerXpub will be publicly announced.
+    ({ providerMnemonic, providerXpub } = await generateProviderKey());
     // The provider's master wallet needs to be funded for it to create a provider
     // record first. This is only done once while deploying the node on a chain
     // for the first time. The master wallet will send the remaining funds to the
     // provider admin.
-    await fundProviderMasterWallet();
+    await fundProviderMasterWallet(providerXpub);
     // Create the on-chain provider record. This will return the balance of the master
     // wallet to the provider admin.
-    await createProvider();
+    const providerId = await createProvider(providerMnemonic, providerXpub);
     // endpointId is the hash of the endpoint name from OIS
     // Note that the provider didn't need to make a transaction to create it
-    endpointId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['string'], ['convertToUsd']));
+    const endpointId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['string'], ['convertToUsd']));
     // However, the provider makes a transaction to update the endpoint's authorizers as [0x0],
-    // which means allow everyone
+    // which means "allow everyone in"
     await airnode
       .connect(roles.providerAdmin)
       .updateEndpointAuthorizers(providerId, endpointId, [ethers.constants.AddressZero]);
-
     // Create the on-chain requester record
-    await createRequester();
+    const requesterInd = await createRequester();
     // The requester can fund their designated wallet
-    await fundDesignatedWallet();
+    const designatedWalletAddress = await fundDesignatedWallet(requesterInd, providerXpub);
     // The requester introduces the client contract to the Airnode contract as one
     // of its own. This means that the requests made by the client contract will
     // be funded by the requester's designated wallet.
@@ -60,21 +55,25 @@ describe('Airnode', function () {
       .connect(roles.requesterAdmin)
       .updateClientEndorsementStatus(requesterInd, airnodeClient.address, true);
     // The requester creates the template which keeps the request parameters
-    await createTemplate();
-
+    const templateId = await createTemplate(providerId, endpointId, requesterInd, designatedWalletAddress);
     // Someone calls the client contract, which triggers a short request
-    await airnodeClient.triggerShortRequest(templateId, ethers.utils.randomBytes(16));
+    await airnodeClient.makeShortRequest(templateId, ethers.utils.randomBytes(16));
     // The Airnode fulfills the short request
-    await fulfillShortRequest();
+    await fulfillShortRequest(providerId, providerMnemonic, providerXpub);
     // We got our response!
-    console.log(ethers.utils.parseBytes32String(await airnodeClient.data()));
-
+    const airnodeClientLogs = await waffle.provider.getLogs({
+      address: airnodeClient.address,
+      fromBlock: 0
+    });
+    const parsedAirnodeClientLog = airnodeClient.interface.parseLog(airnodeClientLogs[0]);
+    console.log(ethers.utils.parseBytes32String(parsedAirnodeClientLog.args.data));
+    
     // Now the requester wants the amount deposited at their reserved wallet back
     await airnode
       .connect(roles.requesterAdmin)
       .requestWithdrawal(providerId, requesterInd, designatedWalletAddress, roles.requesterAdmin._address);
     // and the Airnode fulfills it
-    await fulfillWithdrawalRequest();
+    await fulfillWithdrawalRequest(providerId, providerMnemonic);
   });
 
   async function generateProviderKey() {
@@ -83,15 +82,15 @@ describe('Airnode', function () {
     // Derive the extended public key that will be announced publicly
     const hdNode = ethers.utils.HDNode.fromMnemonic(wallet.mnemonic.phrase);
     const xpub = hdNode.neuter().extendedKey;
-    providerKey = {
-      mnemonic: wallet.mnemonic.phrase,
-      xpub: xpub,
+    return {
+      providerMnemonic: wallet.mnemonic.phrase,
+      providerXpub: xpub,
     };
   }
 
-  async function fundProviderMasterWallet() {
+  async function fundProviderMasterWallet(providerXpub) {
     // The provider knows their xpub so they can derive their master wallet address
-    const masterWalletAddress = deriveWalletAddressFromPath('m');
+    const masterWalletAddress = deriveWalletAddressFromPath(providerXpub, 'm');
     // Creating the provider (calling createProvider()) will cost 200,00 gas.
     // 0.1 ETH should be able to cover it. Note that this depends on the current
     // gas prices. The provider should make sure to send at least 200,000*gasPrice.
@@ -103,63 +102,56 @@ describe('Airnode', function () {
     });
   }
 
-  function deriveWalletFromPath(path) {
+  function deriveWalletFromPath(providerMnemonic, path) {
     // The node uses this function to derive a wallet using its path
-    const masterHdNode = ethers.utils.HDNode.fromMnemonic(providerKey.mnemonic);
+    const masterHdNode = ethers.utils.HDNode.fromMnemonic(providerMnemonic);
     const designatorHdNode = masterHdNode.derivePath(path);
     return new ethers.Wallet(designatorHdNode.privateKey, waffle.provider);
   }
 
-  async function deriveWalletAddressFromPath(path) {
+  async function deriveWalletAddressFromPath(providerXpub, path) {
     // Anyone can use this function derive the address of a wallet using its path
-    const hdNode = ethers.utils.HDNode.fromExtendedKey(providerKey.xpub);
+    const hdNode = ethers.utils.HDNode.fromExtendedKey(providerXpub);
     const wallet = hdNode.derivePath(path);
     return wallet.address;
   }
 
-  async function createProvider() {
+  async function createProvider(providerMnemonic, providerXpub) {
     // The provider's Airnode wakes up. Checks if its providerId is registered
     // at the Airnode contract.
-    const masterWallet = deriveWalletFromPath('m');
+    const masterWallet = deriveWalletFromPath(providerMnemonic, 'm');
     // The providerId of a provider is the hash of their master wallet address
-    providerId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['address'], [masterWallet.address]));
+    const providerId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['address'], [masterWallet.address]));
     let retrievedProvider = await airnode.getProvider(providerId);
-    // The retrieved provider will be empty because it's not created yet. The Airnode
-    // should create it in that case.
-    if (retrievedProvider.xpub === '') {
-      // minBalance is the minimum balance a designated wallet needs to have for
-      // it to be eligible to be used to fulfill a request. It is implemented to
-      // prevent requests being made to designated wallets not funded enough to
-      // fulfill requests, which would cause the respective API calls to be made
-      // each polling cycle.
-      // The Airnode reads both minBalance and providerAdmin from config.json
-      const minBalance = ethers.utils.parseEther('0.01');
+    // The retrieved provider will be empty because it's not created yet
+    expect(retrievedProvider.xpub).to.equal('');
+    // The Airnode should create it in that case.
+    
+    // Gas cost is 160,076
+    const estimatedGasCost = await airnode
+      .connect(masterWallet)
+      .estimateGas
+      .createProvider(roles.providerAdmin._address, providerXpub, { value: 1 });
+    // Overestimate a bit
+    const gasLimit = estimatedGasCost.add(ethers.BigNumber.from(20_000));
+    const gasPrice = await waffle.provider.getGasPrice();
+    const txCost = gasLimit.mul(gasPrice);
+    const masterWalletBalance = await waffle.provider.getBalance(masterWallet.address);
+    const fundsToSend = masterWalletBalance.sub(txCost);
+    // Create the provider and send the rest of the master wallet balance along with
+    // this transaction. Provider admin will receive these funds.
+    await airnode.connect(masterWallet).createProvider(roles.providerAdmin._address, providerXpub, {
+      value: fundsToSend,
+      gasLimit: gasLimit,
+      gasPrice: gasPrice,
+    });
 
-      /*
-      // Gas cost is 180,890
-      const esimatedGasCost = await airnode
-        .connect(masterWallet)
-        .estimateGas
-        .createProvider(roles.providerAdmin._address, providerKey.xpub, minBalance, {value: 1});
-      */
-
-      // Overestimate a bit
-      const gasLimit = ethers.BigNumber.from(200000);
-      const gasPrice = await waffle.provider.getGasPrice();
-      const txCost = gasLimit.mul(gasPrice);
-      const masterWalletBalance = await waffle.provider.getBalance(masterWallet.address);
-      const fundsToSend = masterWalletBalance.sub(txCost);
-      await airnode.connect(masterWallet).createProvider(roles.providerAdmin._address, providerKey.xpub, minBalance, {
-        value: fundsToSend,
-        gasLimit: gasLimit,
-        gasPrice: gasPrice,
-      });
-    }
-    // The Airnode goes back to sleep. The next time it wakes up, it checks if its
-    // provider record exists on-chain.
+    // The Airnode goes back to sleep
+    // The next time it wakes up, it checks if its provider record exists on-chain.
     retrievedProvider = await airnode.getProvider(providerId);
-    expect(retrievedProvider.xpub).to.equal(providerKey.xpub);
+    expect(retrievedProvider.xpub).to.equal(providerXpub);
     // It does exist, so the node can move on to handling requests
+    return providerId;
   }
 
   async function createRequester() {
@@ -169,21 +161,22 @@ describe('Airnode', function () {
       (log) => log.transactionHash === tx.hash
     )[0];
     const parsedLog = airnode.interface.parseLog(log);
-    requesterInd = parsedLog.args.requesterInd;
+    return parsedLog.args.requesterInd;
   }
 
-  async function fundDesignatedWallet() {
+  async function fundDesignatedWallet(requesterInd, providerXpub) {
     // The requester derives the address of their designated wallet using the provider's
     // xpub and their requesterInd and fund that wallet.
-    designatedWalletAddress = await deriveWalletAddressFromPath(`m/0/${requesterInd.toString()}`);
+    const designatedWalletAddress = await deriveWalletAddressFromPath(providerXpub, `m/0/${requesterInd.toString()}`);
     // The requester should send more than minBalance of the provider (it was 0.01 ETH in this case)
     await roles.requesterAdmin.sendTransaction({
       to: designatedWalletAddress,
       value: ethers.utils.parseEther('0.1'),
     });
+    return designatedWalletAddress;
   }
 
-  async function createTemplate() {
+  async function createTemplate(providerId, endpointId, requesterInd, designatedWalletAddress) {
     // Note that we are not connecting to the contract as requesterAdmin.
     // That's because it doesn't matter who creates the template.
     const tx = await airnode.createTemplate(
@@ -200,10 +193,10 @@ describe('Airnode', function () {
       (log) => log.transactionHash === tx.hash
     )[0];
     const parsedLog = airnode.interface.parseLog(log);
-    templateId = parsedLog.args.templateId;
+    return parsedLog.args.templateId;
   }
 
-  async function fulfillShortRequest() {
+  async function fulfillShortRequest(providerId, providerMnemonic, providerXpub) {
     // The provider node can get all events that concern it with a single call.
     const providerLogs = await waffle.provider.getLogs({
       address: airnode.address,
@@ -225,7 +218,7 @@ describe('Airnode', function () {
     );
     expect(parsedRequestLog.args.requestId).to.equal(expectedRequestId);
 
-    // Fetch the template (Convenience.sol would be used for this)
+    // Fetch the template
     const template = await airnode.getTemplate(parsedRequestLog.args.templateId);
     // Verify that the template parameters are not tampered with
     const expectedTemplateId = ethers.utils.keccak256(
@@ -244,27 +237,25 @@ describe('Airnode', function () {
     );
     expect(parsedRequestLog.args.templateId).to.equal(expectedTemplateId);
     // Verify that the designated wallet is correct
-    const expectedDesignatedWallet = await deriveWalletAddressFromPath(`m/0/${template.requesterInd.toString()}`);
+    const expectedDesignatedWallet = await deriveWalletAddressFromPath(providerXpub, `m/0/${template.requesterInd.toString()}`);
     expect(template.designatedWallet).to.equal(expectedDesignatedWallet);
     // Check authorization status
-    const authorizationStatus = await convenience.checkAuthorizationStatuses(
-      [parsedRequestLog.args.requestId],
-      [providerId],
-      [template.endpointId],
-      [template.requesterInd],
-      [parsedRequestLog.args.requester]
+    const authorizationStatus = await convenience.checkAuthorizationStatus(
+      providerId,
+      parsedRequestLog.args.requestId,
+      template.endpointId,
+      template.requesterInd,
+      template.designatedWallet,
+      parsedRequestLog.args.requester
     );
-    expect(authorizationStatus[0]).to.equal(true);
-
+    expect(authorizationStatus).to.equal(true);
     // Now the node can use template.endpointId to find in config.json which
     // endpoint it should call. Then, it decodes the static request parameters defined
     // in template.parameters, the dynamic request parameters defined in
-    // parsedRequestLog.parameters, insert these to correct fields and make the
+    // parsedRequestLog.args.parameters, insert these to correct fields and make the
     // API call. After it gets the response, it processes it according to the reserved
-    // integration parameters (_path, _times, _type) and fulfills the request with
-    // the outcome.
-
-    const designatedWallet = await deriveWalletFromPath(`m/0/${template.requesterInd.toString()}`);
+    // parameters (_path, _times, _type) and fulfills the request with the outcome.
+    const designatedWallet = await deriveWalletFromPath(providerMnemonic, `m/0/${template.requesterInd.toString()}`);
     await airnode.connect(designatedWallet).fulfill(
       parsedRequestLog.args.requestId,
       providerId,
@@ -273,15 +264,15 @@ describe('Airnode', function () {
       template.fulfillAddress,
       template.fulfillFunctionId,
       {
-        gasLimit: 500000, // For some reason, the default gas limit is too high
         // 500000 is a safe value and we can allow the requester to set this
         // with a _gasLimit reserved parameter (for example, if they want to run
         // a very gas-heavy aggregation method)
+        gasLimit: 500000
       }
     );
   }
 
-  async function fulfillWithdrawalRequest() {
+  async function fulfillWithdrawalRequest(providerId, providerMnemonic) {
     // The node gets the WithdrawalRequested log
     const providerLogs = await waffle.provider.getLogs({
       address: airnode.address,
@@ -294,23 +285,21 @@ describe('Airnode', function () {
     )[0];
 
     const designatedWallet = await deriveWalletFromPath(
-      `m/0/${parsedWithdrawalRequestLog.args.requesterInd.toString()}`
+      providerMnemonic, `m/0/${parsedWithdrawalRequestLog.args.requesterInd.toString()}`
     );
     // Verify that the provided designatedWallet was correct
     expect(parsedWithdrawalRequestLog.args.designatedWallet).to.equal(designatedWallet.address);
 
-    /*
-    // Gas cost is 41,797
-    const esimatedGasCost = await airnode
+    // Gas cost is 41,701
+    const estimatedGasCost = await airnode
         .connect(designatedWallet)
         .estimateGas
         .fulfillWithdrawal( parsedWithdrawalRequestLog.args.withdrawalRequestId,
           parsedWithdrawalRequestLog.args.providerId,
           parsedWithdrawalRequestLog.args.requesterInd,
           parsedWithdrawalRequestLog.args.destination, {value: 1});
-    */
     // Overestimate a bit
-    const gasLimit = ethers.BigNumber.from(50000);
+    const gasLimit = estimatedGasCost.add(ethers.BigNumber.from(20_000));
     const gasPrice = await waffle.provider.getGasPrice();
     const txCost = gasLimit.mul(gasPrice);
     const designatedWalletBalance = await waffle.provider.getBalance(designatedWallet.address);
