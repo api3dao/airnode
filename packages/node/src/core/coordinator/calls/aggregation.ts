46c641fb37d6feac6605a3298c845c7b2aa5dc6e
André Ogle <ogleandre@gmail.com>
2020-10-23 17:09:32 +0200

    No longer need to store a list of aggregated API calls for a given requst ID

--- packages/node/src/core/coordinator/calls/aggregation.ts
+++ packages/node/src/core/coordinator/calls/aggregation.ts
@@ -1,5 +1,4 @@
 import { config } from '../../config';
-import * as apiCalls from '../../requests/api-calls';
 import { AggregatedApiCall, AggregatedApiCallsById, ApiCall, ClientRequest } from '../../../types';
 
 function createAggregatedCall(request: ClientRequest<ApiCall>): AggregatedApiCall {
@@ -18,28 +17,13 @@ function createAggregatedCall(request: ClientRequest<ApiCall>): AggregatedApiCal
 
 export function aggregate(flatApiCalls: ClientRequest<ApiCall>[]): AggregatedApiCallsById {
   const aggregatedApiCallsById = flatApiCalls.reduce((acc: AggregatedApiCallsById, request) => {
-    const existingAggregatedCalls = acc[request.id];
+    const existingAggregatedCall = acc[request.id];
 
     // If this is the first time we're seeing this API call, then create a new aggregated API call
-    if (!existingAggregatedCalls) {
+    if (!existingAggregatedCall) {
       return {
         ...acc,
-        [request.id]: [createAggregatedCall(request)],
-      };
-    }
-
-    // Search the current list of aggregated API calls for a duplicate
-    const duplicateApiCall = existingAggregatedCalls.find((aggregatedCall) => {
-      return apiCalls.isDuplicate(request, aggregatedCall);
-    });
-
-    // NOTE: While unlikely, it is possible that a specific provider returns an API call
-    // with the same ID as another request, but different endpointId & parameters. We want
-    // to service all of these requests, so we need to keep both
-    if (!duplicateApiCall) {
-      return {
-        ...acc,
-        [request.id]: [...existingAggregatedCalls, createAggregatedCall(request)],
+        [request.id]: createAggregatedCall(request),
       };
     }
 
