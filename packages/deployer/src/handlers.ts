2e9f49d9072ca3a4df3e1fdba6a377b72571f2a5
bbenligiray <bbenligiray@gmail.com>
2020-10-24 16:22:43 +0300

    Remove the test serverless function

--- packages/deployer/src/handlers.ts
+++ packages/deployer/src/handlers.ts
@@ -1,8 +1,1 @@
 export { start, initializeProvider, callApi, processProviderRequests } from '@airnode/node';
-
-export async function test() {
-  return {
-    body: `${process.env.MASTER_KEY_MNEMONIC} ${process.env.coinlayer_coinlayerSecurityScheme}`,
-    statusCode: 200,
-  };
-}
