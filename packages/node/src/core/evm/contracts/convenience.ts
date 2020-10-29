617027ec6ae948c654176eede9d1794761d5ba58
André Ogle <ogleandre@gmail.com>
2020-10-29 17:30:26 +0200

    Update contract addresses

--- packages/node/src/core/evm/contracts/convenience.ts
+++ packages/node/src/core/evm/contracts/convenience.ts
@@ -6,7 +6,7 @@ export const Convenience: Contract = {
     1: '<TODO>',
     3: '0xd029Ec5D9184Ecd8E853dC9642bdC1E0766266A1',
     4: '0xC7CEFcf5e20EeB102527E855D0791155785EeeEE',
-    1337: '0xD5659F26A72A8D718d1955C42B3AE418edB001e0',
+    1337: '0x2393737d287c555d148012270Ce4567ABb1ee95C',
   },
   ABI: compiledContract.abi,
   topics: {},
