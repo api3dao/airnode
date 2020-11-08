aa1516a3817b1f3643fdcc447cba65ce2560ce31
André Ogle <ogleandre@gmail.com>
2020-11-08 16:41:39 +0200

    Clean up AWS function handlers

--- packages/node/src/aws/handler.ts
+++ packages/node/src/aws/handler.ts
@@ -1,6 +1,8 @@
 import * as config from '../core/config';
 import * as handlers from '../core/handlers';
 import * as logger from '../core/logger';
+import * as state from '../core/providers/state';
+import { go } from '../core/utils/promise-utils';
 import { removeKey } from '../core/utils/object-utils';
 
 export async function start(event: any) {
@@ -13,22 +15,25 @@ export async function start(event: any) {
 }
 
 export async function initializeProvider(event: any) {
-  const { config, state } = event.parameters;
+  const { config } = event.parameters;
   // State and config are sent separately
-  const stateWithConfig = { ...state, config };
-  // TODO: Wrap this in a 'go' to catch and log any unexpected errors
-  const initializedState = await handlers.initializeProvider(stateWithConfig);
-  if (!initializedState) {
+  const stateWithConfig = state.update(event.parameters.state, { config });
+
+  const [err, initializedState] = await go(handlers.initializeProvider(stateWithConfig));
+  if (err || !initializedState) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: `Failed to initialize provider: ${state.settings.name}` }),
+      body: JSON.stringify({
+        error: err,
+        message: `Failed to initialize provider: ${stateWithConfig.settings.name}`,
+      }),
     };
   }
 
   // NOTE: We can't return the instance of the provider. A new provider
   // will be created in the calling function
-  const body = removeKey(initializedState, 'provider');
-  return { statusCode: 200, body: JSON.stringify(body) };
+  const stateWithoutProvider = removeKey(initializedState, 'provider');
+  return { statusCode: 200, body: JSON.stringify(stateWithoutProvider) };
 }
 
 export async function callApi(event: any) {
@@ -39,10 +44,23 @@ export async function callApi(event: any) {
 }
 
 export async function processProviderRequests(event: any) {
-  const { config, state } = event.parameters;
+  const { config } = event.parameters;
   // State and config are sent separately
-  const stateWithConfig = { ...state, config };
-  const updatedState = await handlers.processTransactions(stateWithConfig);
-  const body = removeKey(updatedState, 'provider');
-  return { statusCode: 200, body: JSON.stringify(body) };
+  const stateWithConfig = state.update(event.parameters.state, { config });
+
+  const [err, updatedState] = await go(handlers.processTransactions(stateWithConfig));
+  if (err || !updatedState) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err,
+        message: `Failed to process provider requests: ${stateWithConfig.settings.name}`,
+      }),
+    };
+  }
+
+  // NOTE: We can't return the instance of the provider. A new provider
+  // will be created in the calling function
+  const stateWithoutProvider = removeKey(updatedState, 'provider');
+  return { statusCode: 200, body: JSON.stringify(stateWithoutProvider) };
 }
