9721522aa657995471d41ed7a69d0e66aa18d314
André Ogle <ogleandre@gmail.com>
2020-12-07 12:20:26 +0200

    Remove old AWS dev handler functions

--- packages/node/__dev__/invoke.ts
+++ packages/node/__dev__/invoke.ts
@@ -1,13 +1,10 @@
 import dotenv from 'dotenv';
-import fs from 'fs';
-import * as aws from '../src/aws/handler';
+import * as localHandlers from '../src/core/workers/local-handlers';
 
 dotenv.config();
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-
 async function invoke() {
-  await aws.startCoordinator({ parameters: { config } });
+  await localHandlers.startCoordinator();
 }
 
 invoke();
