117f886f9b34f0ddb1f3963782e23e1937e8a808
bbenligiray <bbenligiray@gmail.com>
2020-12-11 21:17:41 +0300

    Index request IDs in logs

--- packages/protocol/contracts/interfaces/IAirnode.sol
+++ packages/protocol/contracts/interfaces/IAirnode.sol
@@ -8,7 +8,7 @@ import "./ITemplateStore.sol";
 interface IAirnode is IEndpointStore, ITemplateStore {
     event ClientRequestCreated(
         bytes32 indexed providerId,
-        bytes32 requestId,
+        bytes32 indexed requestId,
         uint256 noRequests,
         address clientAddress,
         bytes32 templateId,
@@ -21,7 +21,7 @@ interface IAirnode is IEndpointStore, ITemplateStore {
 
     event ClientShortRequestCreated(
         bytes32 indexed providerId,
-        bytes32 requestId,
+        bytes32 indexed requestId,
         uint256 noRequests,
         address clientAddress,
         bytes32 templateId,
@@ -30,7 +30,7 @@ interface IAirnode is IEndpointStore, ITemplateStore {
 
     event ClientFullRequestCreated(
         bytes32 indexed providerId,
-        bytes32 requestId,
+        bytes32 indexed requestId,
         uint256 noRequests,
         address clientAddress,
         bytes32 endpointId,
@@ -43,21 +43,21 @@ interface IAirnode is IEndpointStore, ITemplateStore {
 
     event ClientRequestFulfilled(
         bytes32 indexed providerId,
-        bytes32 requestId,
+        bytes32 indexed requestId,
         uint256 statusCode,
         bytes32 data
         );
 
     event ClientRequestFulfilledWithBytes(
         bytes32 indexed providerId,
-        bytes32 requestId,
+        bytes32 indexed requestId,
         uint256 statusCode,
         bytes data
         );
 
     event ClientRequestFailed(
         bytes32 indexed providerId,
-        bytes32 requestId
+        bytes32 indexed requestId
         );
 
     function makeRequest(
