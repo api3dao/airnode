b069fd64d355ae1e4be94203aed254cfe2a3c808
bbenligiray <bbenligiray@gmail.com>
2020-12-13 21:30:14 +0300

    Source contract addresses from deployments

--- packages/node/src/evm/contracts/convenience.ts
+++ packages/node/src/evm/contracts/convenience.ts
@@ -1,11 +1,11 @@
 import { Contract } from './types';
-import { ConvenienceAbi } from '@airnode/protocol';
+import { ConvenienceAbi, ConvenienceAddresses } from '@airnode/protocol';
 
 export const Convenience: Contract = {
   addresses: {
     1: '<TODO>',
-    3: '0xa8025cA7d22825a663abdCf2a504a33c8F17C41a',
-    4: '0xB5Edd78ED86b7043D212391f5A6d84cd40111be7',
+    3: ConvenienceAddresses[3],
+    4: ConvenienceAddresses[4],
     1337: '0x2393737d287c555d148012270Ce4567ABb1ee95C',
   },
   ABI: ConvenienceAbi,
--- packages/protocol/index.js
+++ packages/protocol/index.js
@@ -1,4 +1,20 @@
 const AirnodeArtifact = require('./artifacts/contracts/Airnode.sol/Airnode.json');
 const ConvenienceArtifact = require('./artifacts/contracts/Convenience.sol/Convenience.json');
 
-module.exports = { AirnodeAbi: AirnodeArtifact.abi, ConvenienceAbi: ConvenienceArtifact.abi };
+const networkChainIds = { ropsten: 3, rinkeby: 4 };
+
+const AirnodeAddresses = {};
+const ConvenienceAddresses = {};
+for (const network in networkChainIds) {
+  const AirnodeDeployment = require(`./deployments/${network}/Airnode.json`);
+  AirnodeAddresses[networkChainIds[network]] = AirnodeDeployment.receipt.contractAddress;
+  const ConvenienceDeployment = require(`./deployments/${network}/Convenience.json`);
+  ConvenienceAddresses[networkChainIds[network]] = ConvenienceDeployment.receipt.contractAddress;
+}
+
+module.exports = {
+  AirnodeAbi: AirnodeArtifact.abi,
+  ConvenienceAbi: ConvenienceArtifact.abi,
+  AirnodeAddresses,
+  ConvenienceAddresses,
+};
