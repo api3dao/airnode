8afba3e97a141019f025bc7f5009d8700cea5581
André Ogle <ogleandre@gmail.com>
2020-12-23 17:26:00 +0200

    Add explanation for account balances

--- packages/operation/hardhat.config.ts
+++ packages/operation/hardhat.config.ts
@@ -8,7 +8,9 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
-        // 1 million ETH
+        // The "deployer" (provider.getSigner(0)) account is used to fund all
+        // others accounts. So we need to make sure it's got enough ETH. The
+        // below amount is 1 million ETH.
         accountsBalance: '1000000000000000000000000',
       },
     },
