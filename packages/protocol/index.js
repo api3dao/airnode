02f03834f3cdca14eb6a5df3c8b719b3f631b999
bbenligiray <bbenligiray@gmail.com>
2020-12-24 13:49:07 +0300

    Apply review comments

--- packages/protocol/index.js
+++ packages/protocol/index.js
@@ -2,19 +2,20 @@ const AirnodeArtifact = require('./artifacts/contracts/Airnode.sol/Airnode.json'
 const ConvenienceArtifact = require('./artifacts/contracts/Convenience.sol/Convenience.json');
 const MockAirnodeClientArtifact = require('./artifacts/contracts/mock/MockAirnodeClient.sol/MockAirnodeClient.json');
 
-const AirnodeAddresses = {};
-const ConvenienceAddresses = {};
-let AirnodeDeployment, ConvenienceDeployment;
-// Ropsten - 3
-AirnodeDeployment = require(`./deployments/ropsten/Airnode.json`);
-AirnodeAddresses[3] = AirnodeDeployment.receipt.contractAddress;
-ConvenienceDeployment = require(`./deployments/ropsten/Convenience.json`);
-ConvenienceAddresses[3] = ConvenienceDeployment.receipt.contractAddress;
-// Rinkeby - 4
-AirnodeDeployment = require(`./deployments/rinkeby/Airnode.json`);
-AirnodeAddresses[4] = AirnodeDeployment.receipt.contractAddress;
-ConvenienceDeployment = require(`./deployments/rinkeby/Convenience.json`);
-ConvenienceAddresses[4] = ConvenienceDeployment.receipt.contractAddress;
+const AirnodeRopsten = require('./deployments/ropsten/Airnode.json');
+const ConvenienceRopsten = require('./deployments/ropsten/Convenience.json');
+
+const AirnodeRinkeby = require('./deployments/rinkeby/Airnode.json');
+const ConvenienceRinkeby = require('./deployments/rinkeby/Convenience.json');
+
+const AirnodeAddresses = {
+  3: AirnodeRopsten.receipt.contractAddress,
+  4: AirnodeRinkeby.receipt.contractAddress,
+};
+const ConvenienceAddresses = {
+  3: ConvenienceRopsten.receipt.contractAddress,
+  4: ConvenienceRinkeby.receipt.contractAddress,
+};
 
 module.exports = {
   AirnodeArtifact,
