b08c3ac6ec978b44d4ea84f55e76ce38f0692edc
drgy <drgonjak@gmail.com>
2020-12-28 13:15:41 +0100

    Add apiSpecification parameters into converter

--- packages/validator/src/converter.test.ts
+++ packages/validator/src/converter.test.ts
@@ -3,19 +3,40 @@ import { validate } from './validate';
 const generatedOIS = {
   title: 'OAS2OIS',
   version: '1.0.0',
-  paths: {
-    '/test': {
-      get: {
-        parameters: [
-          {
-            in: 'query',
-            name: 'limit',
-          },
-        ],
+  oisFormat: '1.0.0',
+  apiSpecifications: {
+    servers: [
+      {
+        url: 'http://petstore.swagger.io/api',
+      },
+    ],
+    paths: {
+      '/test': {
+        get: {
+          parameters: [
+            {
+              in: 'query',
+              name: 'limit',
+            },
+          ],
+        },
+      },
+    },
+    components: {
+      securitySchemes: {
+        api_key: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'api_key',
+        },
+        http_basic: {
+          type: 'http',
+          in: 'query',
+          scheme: 'Basic',
+        },
       },
     },
   },
-  oisFormat: '1.0.0',
 };
 
 describe('converter', () => {
