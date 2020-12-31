3da65ecbd5ff5adf6bce6fd9d21d17d3656c10c0
bbenligiray <bbenligiray@gmail.com>
2020-12-31 14:18:08 +0300

    Migrate airnode-admin

---
+++ packages/admin/publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sed -i 's=@airnode/protocol=@api3/airnode-protocol=g' ./src/evm.ts
+mv package.json package.monorepo.json
+mv package.publish.json package.json
+cd ..
+cd ..
+yarn run bootstrap
+yarn run build:admin
+cd packages/admin
+npm publish
+sed -i 's=@api3/airnode-protocol=@airnode/protocol=g' ./src/evm.ts
+mv package.json package.publish.json
+mv package.monorepo.json package.json
