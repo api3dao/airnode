0750d85239505940a9af19a5a1fcdee3b8105efe
André Ogle <ogleandre@gmail.com>
2021-01-03 17:50:32 +0200

    Add fixtures for onchain requests

---
+++ packages/node/src/evm/handlers/fetch-pending-requests.test.ts
@@ -0,0 +1,6 @@
+import { fetchPendingRequests } from './fetch-pending-requests';
+import * as fixtures from 'test/fixtures';
+
+describe('fetchPendingRequests', () => {
+  it('returns grouped requests', async () => {});
+});
