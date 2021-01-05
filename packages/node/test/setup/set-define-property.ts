adce39ef3a88494fa3a371f0fca31eb4e645d545
André Ogle <ogleandre@gmail.com>
2021-01-05 17:44:53 +0200

    Add explanation comment

--- packages/node/test/setup/set-define-property.ts
+++ packages/node/test/setup/set-define-property.ts
@@ -1,3 +1,7 @@
+// Sometimes Jest can have issues when trying to spy on an internal module. Import
+// and use this function as a workaround at the top of your test.
+//
+// Credit: https://github.com/facebook/jest/issues/6914#issuecomment-654710111
 const { defineProperty } = Object;
 Object.defineProperty = function (object, name, meta) {
   if (meta.get && !meta.configurable) {
