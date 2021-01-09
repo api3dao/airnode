b6322d19283f0425c910934cd930305b9b8c1ad6
André Ogle <ogleandre@gmail.com>
2021-01-09 14:32:17 +0200

    Remove extra config setting

--- packages/node/jest-unit.config.js
+++ packages/node/jest-unit.config.js
@@ -5,7 +5,6 @@ module.exports = {
   // Add custom settings below
   displayName: 'unit',
   name: 'unit',
-  ...config,
   setupFiles: ['<rootDir>/test/setup/init/set-define-property.ts', '<rootDir>/test/setup/init/set-env-vars.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 };
