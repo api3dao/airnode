344065e815d0d20ef1bc468c51586e8e0a3c183b
André Ogle <ogleandre@gmail.com>
2021-01-09 14:29:09 +0200

    Ensure all constant values are tested

--- packages/node/src/constants.test.ts
+++ packages/node/src/constants.test.ts
@@ -1,49 +1,23 @@
-import {
-  API_CALL_TIMEOUT,
-  API_CALL_TOTAL_TIMEOUT,
-  BLOCK_COUNT_HISTORY_LIMIT,
-  BLOCK_COUNT_IGNORE_LIMIT,
-  BLOCK_MIN_CONFIRMATIONS,
-  CONVENIENCE_BATCH_SIZE,
-  DEFAULT_RETRY_OPERATION_TIMEOUT,
-  EVM_PROVIDER_TIMEOUT,
-  OPERATION_RETRIES,
-  WORKER_PROVIDER_INITIALIZATION_TIMEOUT,
-  WORKER_CALL_API_TIMEOUT,
-} from './constants';
+import * as constants from './constants';
 
 describe('constants', () => {
-  it('returns constant API_CALL_TOTAL_TIMEOUT value', () => {
-    expect(API_CALL_TOTAL_TIMEOUT).toEqual(29_000);
-  });
-  it('returns constant API_CALL_TIMEOUT value', () => {
-    expect(API_CALL_TIMEOUT).toEqual(20_000);
-  });
-  it('returns constant BLOCK_COUNT_HISTORY_LIMIT value', () => {
-    expect(BLOCK_COUNT_HISTORY_LIMIT).toEqual(600);
-  });
-  it('returns constant BLOCK_COUNT_IGNORE_LIMIT value', () => {
-    expect(BLOCK_COUNT_IGNORE_LIMIT).toEqual(20);
-  });
-  it('returns constant BLOCK_MIN_CONFIRMATIONS value', () => {
-    expect(BLOCK_MIN_CONFIRMATIONS).toEqual(0);
-  });
-  it('returns constant CONVENIENCE_BATCH_SIZE value', () => {
-    expect(CONVENIENCE_BATCH_SIZE).toEqual(10);
-  });
-  it('returns constant DEFAULT_RETRY_OPERATION_TIMEOUT value', () => {
-    expect(DEFAULT_RETRY_OPERATION_TIMEOUT).toEqual(5000);
-  });
-  it('returns constant EVM_PROVIDER_TIMEOUT value', () => {
-    expect(EVM_PROVIDER_TIMEOUT).toEqual(10_000);
-  });
-  it('returns constant OPERATION_RETRIES value', () => {
-    expect(OPERATION_RETRIES).toEqual(2);
-  });
-  it('returns constant WORKER_PROVIDER_INITIALIZATION_TIMEOUT value', () => {
-    expect(WORKER_PROVIDER_INITIALIZATION_TIMEOUT).toEqual(20_000);
-  });
-  it('returns constant WORKER_CALL_API_TIMEOUT value', () => {
-    expect(WORKER_CALL_API_TIMEOUT).toEqual(29_500);
+  it('returns constant values', () => {
+    const constCount = Object.keys(constants).length;
+
+    // Ensure all constant values are checked
+    expect.assertions(constCount + 1);
+    expect(Object.keys(constants).length).toEqual(11);
+
+    expect(constants.API_CALL_TIMEOUT).toEqual(20_000);
+    expect(constants.API_CALL_TOTAL_TIMEOUT).toEqual(29_000);
+    expect(constants.BLOCK_COUNT_HISTORY_LIMIT).toEqual(600);
+    expect(constants.BLOCK_COUNT_IGNORE_LIMIT).toEqual(20);
+    expect(constants.BLOCK_MIN_CONFIRMATIONS).toEqual(0);
+    expect(constants.CONVENIENCE_BATCH_SIZE).toEqual(10);
+    expect(constants.DEFAULT_RETRY_OPERATION_TIMEOUT).toEqual(5000);
+    expect(constants.EVM_PROVIDER_TIMEOUT).toEqual(10_000);
+    expect(constants.OPERATION_RETRIES).toEqual(2);
+    expect(constants.WORKER_CALL_API_TIMEOUT).toEqual(29_500);
+    expect(constants.WORKER_PROVIDER_INITIALIZATION_TIMEOUT).toEqual(20_000);
   });
 });
