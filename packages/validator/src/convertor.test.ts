6cc19c0af656e30c19f06886d3c44aff1290fe44
drgy <drgonjak@gmail.com>
2021-02-06 21:12:07 +0100

    Add OIS to config & security conversion

--- packages/validator/src/convertor.test.ts
+++ packages/validator/src/convertor.test.ts
@@ -1,254 +1,53 @@
 import { convert } from './convertor';
-import { missingParamMessage } from './utils/messages';
+import { formattingMessage, missingParamMessage } from './utils/messages';
+import fs from 'fs';
 
-const generatedOIS = {
-  title: 'CoinGecko API V3',
-  version: '3.0.0',
-  oisFormat: '1.0.0',
-  apiSpecifications: {
-    servers: [
-      {
-        url: 'https://api.coingecko.com/api/v3',
-      },
-    ],
-    paths: {
-      '/ping': {
-        get: {
-          parameters: [],
-        },
-      },
-      '/simple/price': {
-        get: {
-          parameters: [
-            {
-              in: 'query',
-              name: 'ids',
-            },
-            {
-              in: 'query',
-              name: 'vs_currencies',
-            },
-            {
-              in: 'query',
-              name: 'include_market_cap',
-            },
-            {
-              in: 'query',
-              name: 'include_24hr_vol',
-            },
-            {
-              in: 'query',
-              name: 'include_24hr_change',
-            },
-            {
-              in: 'query',
-              name: 'include_last_updated_at',
-            },
-          ],
-        },
-      },
-      '/simple/token_price/{id}': {
-        get: {
-          parameters: [
-            {
-              in: 'path',
-              name: 'id',
-            },
-            {
-              in: 'query',
-              name: 'contract_addresses',
-            },
-            {
-              in: 'query',
-              name: 'vs_currencies',
-            },
-            {
-              in: 'query',
-              name: 'include_market_cap',
-            },
-            {
-              in: 'query',
-              name: 'include_24hr_vol',
-            },
-            {
-              in: 'query',
-              name: 'include_24hr_change',
-            },
-            {
-              in: 'query',
-              name: 'include_last_updated_at',
-            },
-          ],
-        },
-      },
+const generatedOIS = JSON.parse(fs.readFileSync('exampleSpecs/OIS2.specs.json').toString());
+const generatedCS = {
+  config: {
+    id: '{FILL}',
+    nodeSettings: {
+      providerIdShort: '{FILL}',
+      nodeVersion: '{FILL}',
+      cloudProvider: '{FILL}',
+      stage: '{FILL}',
+      region: '{FILL}',
+      logFormat: '{FILL}',
+      chains: [],
+      triggers: {},
+      ois: [generatedOIS],
     },
-    components: {
-      securitySchemes: {},
-    },
-    security: {},
   },
-  endpoints: [
-    {
-      name: '/ping',
-      operation: {
-        method: 'get',
-        path: '/ping',
-      },
-      reservedParameters: [
+  security: {
+    id: '{FILL}',
+    apiCredentials: {
+      'CoinGecko API V3': [
         {
-          name: '_type',
-        },
-        {
-          name: '_path',
-        },
-        {
-          name: '_times',
-        },
-      ],
-      fixedOperationParameters: [],
-    },
-    {
-      name: '/simple/price',
-      operation: {
-        method: 'get',
-        path: '/simple/price',
-      },
-      reservedParameters: [
-        {
-          name: '_type',
-        },
-        {
-          name: '_path',
-        },
-        {
-          name: '_times',
-        },
-      ],
-      fixedOperationParameters: [],
-      parameters: [
-        {
-          name: 'ids',
-          operationParameter: {
-            name: 'ids',
-            in: 'query',
-          },
-        },
-        {
-          name: 'vs_currencies',
-          operationParameter: {
-            name: 'vs_currencies',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_market_cap',
-          operationParameter: {
-            name: 'include_market_cap',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_24hr_vol',
-          operationParameter: {
-            name: 'include_24hr_vol',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_24hr_change',
-          operationParameter: {
-            name: 'include_24hr_change',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_last_updated_at',
-          operationParameter: {
-            name: 'include_last_updated_at',
-            in: 'query',
-          },
+          securitySchemeName: 'api_key',
+          value: '{FILL}',
         },
       ],
     },
-    {
-      name: '/simple/token_price/{id}',
-      operation: {
-        method: 'get',
-        path: '/simple/token_price/{id}',
-      },
-      reservedParameters: [
-        {
-          name: '_type',
-        },
-        {
-          name: '_path',
-        },
-        {
-          name: '_times',
-        },
-      ],
-      fixedOperationParameters: [],
-      parameters: [
-        {
-          name: 'id',
-          operationParameter: {
-            name: 'id',
-            in: 'path',
-          },
-        },
-        {
-          name: 'contract_addresses',
-          operationParameter: {
-            name: 'contract_addresses',
-            in: 'query',
-          },
-        },
-        {
-          name: 'vs_currencies',
-          operationParameter: {
-            name: 'vs_currencies',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_market_cap',
-          operationParameter: {
-            name: 'include_market_cap',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_24hr_vol',
-          operationParameter: {
-            name: 'include_24hr_vol',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_24hr_change',
-          operationParameter: {
-            name: 'include_24hr_change',
-            in: 'query',
-          },
-        },
-        {
-          name: 'include_last_updated_at',
-          operationParameter: {
-            name: 'include_last_updated_at',
-            in: 'query',
-          },
-        },
-      ],
-    },
-  ],
+  },
 };
 
 describe('convertor', () => {
   it('OAS2OIS', () => {
     expect(convert('exampleSpecs/OAS.specs.json', 'templates/OAS2OIS.json')).toEqual({
       valid: false,
-      messages: [missingParamMessage('components.securitySchemes')],
+      messages: [
+        formattingMessage('components.schemas.securitySchemes.petstore_auth.type'),
+        missingParamMessage('components.schemas.securitySchemes.petstore_auth.in'),
+      ],
       output: generatedOIS,
+    });
+  });
+
+  it('OIS2C&S', () => {
+    expect(convert('exampleSpecs/OIS2.specs.json', 'templates/OIS2C&S.json')).toEqual({
+      valid: true,
+      messages: [],
+      output: generatedCS,
     });
   });
 });
