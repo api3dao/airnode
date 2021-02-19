b52cd6d63cfb08d5f0572aa435ec28f9ef1ce8e6
André Ogle <ogleandre@gmail.com>
2021-02-19 16:37:02 +0200

    Fix unit tests

--- packages/node/src/evm/initialization.test.ts
+++ packages/node/src/evm/initialization.test.ts
@@ -22,14 +22,15 @@ describe('providerExistsOnchain', () => {
   const options = {
     authorizers: [ethers.constants.AddressZero],
     masterHDNode: wallet.getMasterHDNode(),
-    providerAdmin: '0x5e0051B74bb4006480A1b548af9F1F0e0954F410'
+    providerAdmin: '0x5e0051B74bb4006480A1b548af9F1F0e0954F410',
   };
 
   const validData = {
     authorizers: [ethers.constants.AddressZero],
     blockNumber: 12,
     providerAdmin: '0x5e0051B74bb4006480A1b548af9F1F0e0954F410',
-    xpub: 'xpub661MyMwAqRbcGeCE1g3KTUVGZsFDE3jMNinRPGCQGQsAp1nwinB9Pi16ihKPJw7qtaaTFuBHbRPeSc6w3AcMjxiHkAPfyp1hqQRbthv4Ryx',
+    xpub:
+      'xpub661MyMwAqRbcGeCE1g3KTUVGZsFDE3jMNinRPGCQGQsAp1nwinB9Pi16ihKPJw7qtaaTFuBHbRPeSc6w3AcMjxiHkAPfyp1hqQRbthv4Ryx',
   };
 
   it('is false if the provider onchain data matches the expected data', () => {
@@ -208,6 +209,7 @@ describe('create', () => {
     expect(createProviderMock).toHaveBeenCalledWith(
       '0x5e0051B74bb4006480A1b548af9F1F0e0954F410',
       'xpub661MyMwAqRbcGeCE1g3KTUVGZsFDE3jMNinRPGCQGQsAp1nwinB9Pi16ihKPJw7qtaaTFuBHbRPeSc6w3AcMjxiHkAPfyp1hqQRbthv4Ryx',
+      [ethers.constants.AddressZero],
       {
         // 250_000_000 - ((50_000 + 20_000) * 1000)
         value: ethers.BigNumber.from(180_000_000),
@@ -300,6 +302,7 @@ describe('create', () => {
     expect(createProviderMock).toHaveBeenCalledWith(
       '0x5e0051B74bb4006480A1b548af9F1F0e0954F410',
       'xpub661MyMwAqRbcGeCE1g3KTUVGZsFDE3jMNinRPGCQGQsAp1nwinB9Pi16ihKPJw7qtaaTFuBHbRPeSc6w3AcMjxiHkAPfyp1hqQRbthv4Ryx',
+      [ethers.constants.AddressZero],
       {
         // 250_000_000 - ((50_000 + 20_000) * 1000)
         value: ethers.BigNumber.from(180_000_000),
@@ -426,6 +429,7 @@ describe('findOrCreateProvider', () => {
     expect(createProviderMock).toHaveBeenCalledWith(
       '0x5e0051B74bb4006480A1b548af9F1F0e0954F410',
       'xpub661MyMwAqRbcGeCE1g3KTUVGZsFDE3jMNinRPGCQGQsAp1nwinB9Pi16ihKPJw7qtaaTFuBHbRPeSc6w3AcMjxiHkAPfyp1hqQRbthv4Ryx',
+      [ethers.constants.AddressZero],
       {
         // 250_000_000 - ((50_000 + 20_000) * 1000)
         value: ethers.BigNumber.from(180_000_000),
