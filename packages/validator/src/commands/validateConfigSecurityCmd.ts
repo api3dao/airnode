cc626feb5a53499d6c200cdcc615ec48eee3775e
bbenligiray <bbenligiray@gmail.com>
2021-03-03 18:14:01 +0300

    Remove the validator package from master

--- packages/validator/src/commands/validateConfigSecurityCmd.ts
+++
@@ -1,7 +0,0 @@
-import { validateConfigSecurity } from '../validator';
-
-if (process.env.npm_config_config || process.env.npm_config_security) {
-  console.log(validateConfigSecurity(process.env.npm_config_config, process.env.npm_config_security));
-} else {
-  console.log(validateConfigSecurity(process.argv[2], process.argv[3]));
-}
