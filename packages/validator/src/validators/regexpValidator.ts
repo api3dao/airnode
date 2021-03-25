bfa29e522b4e8a11fbfe56a09ecd11a155f1807c
drgy <drgonjak@gmail.com>
2021-03-25 20:32:21 +0100

    Replace parameter values and names in '__regexp'

--- packages/validator/src/validators/regexpValidator.ts
+++ packages/validator/src/validators/regexpValidator.ts
@@ -1,22 +1,32 @@
 import * as logger from '../utils/logger';
 import { Log } from '../types';
-import { combinePaths } from '../utils/utils';
+import * as utils from '../utils/utils';
 
 /**
  * Checks if key or value matches regular expression in template
  * @param specs - specification that is being validated
  * @param template - template containing the regular expression
  * @param paramPath - string of parameters separated by ".", representing path to current specs location
+ * @param rootSpecs - root of specification that is being validated
  * @param isKeyRegexp - determines if key or value should be checked
  * @returns incorrect formatting message if the parameter key or value did not match regular expression
  */
-export function validateRegexp(specs: any, template: any, paramPath: string, isKeyRegexp = false): Log[] {
+export function validateRegexp(
+  specs: any,
+  template: any,
+  paramPath: string,
+  rootSpecs: any,
+  isKeyRegexp = false
+): Log[] {
   const messages: Log[] = [];
+
+  template = utils.replaceParamIndexWithName(template, paramPath);
+  template = utils.replacePathsWithValues(specs, rootSpecs, template);
 
   if (isKeyRegexp) {
     for (const item of Object.keys(specs)) {
       if (!item.match(new RegExp(template['__keyRegexp']))) {
-        messages.push(logger.error(`Key ${item} in ${combinePaths(paramPath, item)} is formatted incorrectly`));
+        messages.push(logger.error(`Key ${item} in ${utils.combinePaths(paramPath, item)} is formatted incorrectly`));
       }
     }
 
