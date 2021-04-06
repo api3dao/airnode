bf0fca8f7313f81f393f28ee6d8e85c5942ec7fd
Emanuel Tesar <e.tesarr@gmail.com>
2021-04-06 18:16:35 +0200

    Add basic typescript support

--- packages/admin/src/evm.ts
+++ packages/admin/src/evm.ts
@@ -1,15 +1,23 @@
 import { ethers } from 'ethers';
 import { AirnodeRrpArtifact, AirnodeRrpAddresses } from '@airnode/protocol';
 
-export async function getAirnodeRrp(providerUrl) {
+export async function getAirnodeRrp(providerUrl: string) {
   const provider = new ethers.providers.JsonRpcProvider(providerUrl);
   const network = await provider.getNetwork();
-  return new ethers.Contract(AirnodeRrpAddresses[network.chainId], AirnodeRrpArtifact.abi, provider);
+  return new ethers.Contract(
+    AirnodeRrpAddresses[network.chainId]! /** TODO: shouldn't this be defined? */,
+    AirnodeRrpArtifact.abi,
+    provider
+  );
 }
 
-export async function getAirnodeRrpWithSigner(mnemonic, providerUrl) {
+export async function getAirnodeRrpWithSigner(mnemonic: string, providerUrl: string) {
   const provider = new ethers.providers.JsonRpcProvider(providerUrl);
   const wallet = ethers.Wallet.fromMnemonic(mnemonic).connect(provider);
   const network = await provider.getNetwork();
-  return new ethers.Contract(AirnodeRrpAddresses[network.chainId], AirnodeRrpArtifact.abi, wallet);
+  return new ethers.Contract(
+    AirnodeRrpAddresses[network.chainId]! /** TODO: shouldn't this be defined? */,
+    AirnodeRrpArtifact.abi,
+    wallet
+  );
 }
--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -1,7 +1,9 @@
 import { ethers } from 'ethers';
 import * as airnodeAbi from '@airnode/airnode-abi';
 
-export async function createRequester(airnodeRrp, requesterAdmin) {
+// TODO: add stronger types for airnodeRrp contract
+
+export async function createRequester(airnodeRrp: ethers.Contract, requesterAdmin: string) {
   const receipt = await airnodeRrp.createRequester(requesterAdmin);
   return new Promise((resolve) =>
     airnodeRrp.provider.once(receipt.hash, (tx) => {
@@ -11,7 +13,7 @@ export async function createRequester(airnodeRrp, requesterAdmin) {
   );
 }
 
-export async function setRequesterAdmin(airnodeRrp, requesterIndex, requesterAdmin) {
+export async function setRequesterAdmin(airnodeRrp: ethers.Contract, requesterIndex: string, requesterAdmin: string) {
   const receipt = await airnodeRrp.setRequesterAdmin(requesterIndex, requesterAdmin);
   return new Promise((resolve) =>
     airnodeRrp.provider.once(receipt.hash, (tx) => {
@@ -21,14 +23,14 @@ export async function setRequesterAdmin(airnodeRrp, requesterIndex, requesterAdm
   );
 }
 
-export async function deriveDesignatedWallet(airnodeRrp, airnodeId, requesterIndex) {
+export async function deriveDesignatedWallet(airnodeRrp: ethers.Contract, airnodeId: string, requesterIndex: string) {
   const airnode = await airnodeRrp.getAirnodeParameters(airnodeId);
   const hdNode = ethers.utils.HDNode.fromExtendedKey(airnode.xpub);
   const designatedWalletNode = hdNode.derivePath(`m/0/${requesterIndex}`);
   return designatedWalletNode.address;
 }
 
-export async function endorseClient(airnodeRrp, requesterIndex, clientAddress) {
+export async function endorseClient(airnodeRrp: ethers.Contract, requesterIndex: string, clientAddress: string) {
   const receipt = await airnodeRrp.setClientEndorsementStatus(requesterIndex, clientAddress, true);
   return new Promise((resolve) =>
     airnodeRrp.provider.once(receipt.hash, (tx) => {
@@ -38,7 +40,7 @@ export async function endorseClient(airnodeRrp, requesterIndex, clientAddress) {
   );
 }
 
-export async function unendorseClient(airnodeRrp, requesterIndex, clientAddress) {
+export async function unendorseClient(airnodeRrp: ethers.Contract, requesterIndex: string, clientAddress: string) {
   const receipt = await airnodeRrp.setClientEndorsementStatus(requesterIndex, clientAddress, false);
   return new Promise((resolve) =>
     airnodeRrp.provider.once(receipt.hash, (tx) => {
@@ -48,7 +50,14 @@ export async function unendorseClient(airnodeRrp, requesterIndex, clientAddress)
   );
 }
 
-export async function createTemplate(airnodeRrp, template) {
+// TODO: type me
+interface Template {
+  parameters: string | airnodeAbi.InputParameter[];
+  airnodeId: string;
+  endpointId: string;
+}
+
+export async function createTemplate(airnodeRrp: ethers.Contract, template: Template) {
   let encodedParameters;
   if (typeof template.parameters == 'string') {
     encodedParameters = template.parameters;
@@ -64,7 +73,12 @@ export async function createTemplate(airnodeRrp, template) {
   );
 }
 
-export async function requestWithdrawal(airnodeRrp, airnodeId, requesterIndex, destination) {
+export async function requestWithdrawal(
+  airnodeRrp: ethers.Contract,
+  airnodeId: string,
+  requesterIndex: string,
+  destination: string
+) {
   const designatedWalletAddress = deriveDesignatedWallet(airnodeRrp, airnodeId, requesterIndex);
   const receipt = await airnodeRrp.requestWithdrawal(airnodeId, requesterIndex, designatedWalletAddress, destination);
   return new Promise((resolve) =>
@@ -75,13 +89,14 @@ export async function requestWithdrawal(airnodeRrp, airnodeId, requesterIndex, d
   );
 }
 
-export async function checkWithdrawalRequest(airnodeRrp, withdrawalRequestId) {
+export async function checkWithdrawalRequest(airnodeRrp: ethers.Contract, withdrawalRequestId: string) {
   const logs = await airnodeRrp.provider.getLogs({
     address: airnodeRrp.address,
     fromBlock: 0,
     topics: [
       ethers.utils.id('WithdrawalFulfilled(bytes32,uint256,bytes32,address,address,uint256)'),
-      null,
+      // these are wrong typings by ethers.js: https://github.com/ethers-io/ethers.js/issues/1434
+      null as any,
       null,
       withdrawalRequestId,
     ],
@@ -89,14 +104,19 @@ export async function checkWithdrawalRequest(airnodeRrp, withdrawalRequestId) {
   if (logs.length === 0) {
     return undefined;
   }
-  const parsedLog = airnodeRrp.interface.parseLog(logs[0]);
+  const parsedLog = airnodeRrp.interface.parseLog(logs[0]!);
   return parsedLog.args.amount;
 }
 
-export async function setAirnodeParameters(airnodeRrp, airnodeAdmin, authorizers) {
-  const hdNode = ethers.utils.HDNode.fromMnemonic(airnodeRrp.signer.mnemonic.phrase);
+export async function setAirnodeParameters(
+  airnodeRrp: ethers.Contract,
+  airnodeAdmin: string,
+  authorizers: object /** TODO: */
+) {
+  const wallet = airnodeRrp.signer as ethers.Wallet;
+  const hdNode = ethers.utils.HDNode.fromMnemonic(wallet.mnemonic.phrase);
   const xpub = hdNode.neuter().extendedKey;
-  const masterWallet = ethers.Wallet.fromMnemonic(airnodeRrp.signer.mnemonic.phrase, 'm').connect(airnodeRrp.provider);
+  const masterWallet = ethers.Wallet.fromMnemonic(wallet.mnemonic.phrase, 'm').connect(airnodeRrp.provider);
   // Assuming masterWallet has funds to make the transaction below
   const receipt = await airnodeRrp
     .connect(masterWallet)
@@ -109,6 +129,6 @@ export async function setAirnodeParameters(airnodeRrp, airnodeAdmin, authorizers
   );
 }
 
-export async function deriveEndpointId(oisTitle, endpointName) {
+export async function deriveEndpointId(oisTitle: string, endpointName: string) {
   return ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['string'], [`${oisTitle}_${endpointName}`]));
 }
