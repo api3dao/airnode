fdfa215cb4934742096c7b9b201bd3270f2d8690
Emanuel Tesar <e.tesarr@gmail.com>
2021-04-13 09:29:08 +0200

    Workaround for TypeChain not exposing contract ABI

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,3 +1,4 @@
+import AirnodeRrpArtifact from '../artifacts/contracts/AirnodeRrp.sol/AirnodeRrp.json';
 import {
   MockAirnodeRrpClient__factory as MockAirnodeRrpClientFactory,
   AirnodeRrp__factory as AirnodeRrpFactory,
@@ -7,8 +8,10 @@ const AirnodeRrpAddresses: { [chainId: number]: string } = {};
 const mocks = {
   MockAirnodeRrpClientFactory,
 };
-// TODO
+// TODO:
 const authorizers = {};
+// TODO: refactor once https://github.com/ethereum-ts/TypeChain/pull/368 is merged
+const AirnodeRrpArtifactAbi = AirnodeRrpArtifact.abi;
 
-export { AirnodeRrpAddresses, AirnodeRrpFactory, mocks, authorizers };
+export { AirnodeRrpAddresses, AirnodeRrpFactory, mocks, authorizers, AirnodeRrpArtifactAbi };
 export type { AirnodeRrp, MockAirnodeRrpClient } from './contracts';
