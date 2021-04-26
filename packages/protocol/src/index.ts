409d083b03305f00718dc11001718bd96935bc82
Emanuel Tesar <e.tesarr@gmail.com>
2021-04-26 10:10:38 +0200

    Add TypeChain explanation in protocol package

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,3 +1,15 @@
+/**
+ * This package is integrated with https://github.com/ethereum-ts/TypeChain which generates TS
+ * bindings and wrappers for ethers wallet (version 5). TypeChain generates two important pieces for
+ * each solidity contract:
+ *  1) Factories - (e.g. AirnodeRrpFactory) are used to connect to ethers Signer and deploy the
+ *     contract (or just connect to an already deployed contract instance). You will get a strongly
+ *     typed contract instance in return.
+ *  2) Typed contracts - These are returned after deployed via contract Factory. It allows you to
+ *     call functions, mapping and transactions in type safe manner.
+ *
+ * The generated code "value exports" the factories, but "type exports" the contracts.
+ */
 import AirnodeRrpArtifact from '../artifacts/contracts/AirnodeRrp.sol/AirnodeRrp.json';
 import MockAirnodeRrpClientArtifact from '../artifacts/contracts/mock/MockAirnodeRrpClient.sol/MockAirnodeRrpClient.json';
 import {
@@ -13,6 +25,7 @@ const mocks = {
 // TODO:
 const authorizers = {};
 
+// NOTE: For now, we only want to expose AirnodeRrp contract (and it's mock)
 export { AirnodeRrpAddresses, AirnodeRrpFactory, mocks, authorizers };
 // Export the artifacts for API consumers using web3
 export { AirnodeRrpArtifact, MockAirnodeRrpClientArtifact };
