33791e601107ce1c50bff1f9950302ec2dbcae4f
bbenligiray <bbenligiray@gmail.com>
2021-05-01 20:15:47 +0300

    Remove unused dependency

--- packages/protocol/contracts/dapis/rrpDapi.sol
+++ packages/protocol/contracts/dapis/rrpDapi.sol
@@ -1,17 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "./reducers/Reducer.sol";
 import "./rrpDapiServer.sol";
 
-// A stub for an RRP dAPI contract
-// When the user wants to deploy a dAPI contract, they inherit the specific
-// reducer contract such as:
-//       contract myRrpDapi is rrpDapi, Median {
-//           // Any specific implementation required
-//       }
-abstract contract RrpDapi is Reducer {
-
+abstract contract RrpDapi {
     RrpDapiServer public rrpDapiServer;
 
     uint256 public immutable maxAnswersInStorage;
