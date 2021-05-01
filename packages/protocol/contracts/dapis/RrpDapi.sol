275d05abe9b052a59a1d67bc2171c6c265001a22
bbenligiray <bbenligiray@gmail.com>
2021-05-01 20:16:38 +0300

    Capitalize contract file names

--- packages/protocol/contracts/dapis/RrpDapi.sol
+++ packages/protocol/contracts/dapis/RrpDapi.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "./rrpDapiServer.sol";
+import "./RrpDapiServer.sol";
 
 abstract contract RrpDapi {
     RrpDapiServer public rrpDapiServer;
