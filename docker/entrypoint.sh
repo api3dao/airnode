ff4d7a3e843fed10f232500c217b54b45737bf92
Michal Kimle <kimle.michal@gmail.com>
2021-05-17 06:54:33 +0200

    Deployer Dockerfile refactor

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-yarn lerna run --scope @airnode/deployer sls:config
-yarn cli:deployer ${@}
+DEPLOYER_BINARY="/usr/local/bin/deployer"
+USER_ID="${USER_ID:-0}"
+GROUP_ID="${GROUP_ID:-0}"
+
+su-exec ${USER_ID}:${GROUP_ID} ${DEPLOYER_BINARY} "$@"
