52246a16f434524f611e5a1bf07d43a663dc8726
bbenligiray <bbenligiray@gmail.com>
2021-07-16 17:59:21 +0300

    Prettify

--- packages/protocol/contracts/authorizers/interfaces/IRankedAdminnable.sol
+++ packages/protocol/contracts/authorizers/interfaces/IRankedAdminnable.sol
@@ -2,15 +2,24 @@
 pragma solidity 0.8.6;
 
 interface IRankedAdminnable {
-  event SetRank(bytes32 indexed adminnedId, address indexed targetAdmin, uint256 newRank, address indexed callerAdmin);
+    event SetRank(
+        bytes32 indexed adminnedId,
+        address indexed targetAdmin,
+        uint256 newRank,
+        address indexed callerAdmin
+    );
 
-  event DecreasedSelfRank(bytes32 indexed adminnedId, address indexed admin, uint256 newRank);
+    event DecreasedSelfRank(
+        bytes32 indexed adminnedId,
+        address indexed admin,
+        uint256 newRank
+    );
 
-  function setRank(
-    bytes32 adminnedId,
-    address targetAdmin,
-    uint256 newRank
-  ) external;
+    function setRank(
+        bytes32 adminnedId,
+        address targetAdmin,
+        uint256 newRank
+    ) external;
 
-  function decreaseSelfRank(bytes32 adminnedId, uint256 newRank) external;
+    function decreaseSelfRank(bytes32 adminnedId, uint256 newRank) external;
 }
