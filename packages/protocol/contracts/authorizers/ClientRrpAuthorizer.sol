b90a4fb07ebff6477564d5e7686fc93893a6a4be
bbenligiray <bbenligiray@gmail.com>
2021-07-19 19:52:21 +0300

    Reword the ClientRrpAuthorizer to be reused for RrpBeaconServer

--- packages/protocol/contracts/authorizers/ClientRrpAuthorizer.sol
+++ packages/protocol/contracts/authorizers/ClientRrpAuthorizer.sol
@@ -1,90 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "./RankedAdminnable.sol";
+import "./ClientWhitelister.sol";
 import "./interfaces/IClientRrpAuthorizer.sol";
 
 /// @title Authorizer contract where clients are whitelisted until an
 /// expiration time or indefinitely (until the whitelisting is revoked)
 abstract contract ClientRrpAuthorizer is
-    RankedAdminnable,
+    ClientWhitelister,
     IClientRrpAuthorizer
 {
-    /// @notice Keeps the whitelisting statuses of clients for individual
-    /// Airnodes
-    mapping(bytes32 => mapping(address => WhitelistStatus))
-        public
-        override airnodeIdToClientToWhitelistStatus;
-
-    /// @notice Called by an admin to extend the whitelist expiration of a
-    /// client
-    /// @param airnodeId Airnode ID
-    /// @param client Client
-    /// @param expirationTimestamp Timestamp at which the client will no longer
-    /// be whitelisted
-    function extendWhitelistExpiration(
-        bytes32 airnodeId,
-        address client,
-        uint64 expirationTimestamp
-    ) external override onlyWithRank(airnodeId, uint256(AdminRank.Admin)) {
-        require(
-            expirationTimestamp >
-                airnodeIdToClientToWhitelistStatus[airnodeId][client]
-                .expirationTimestamp,
-            "Expiration not extended"
-        );
-        airnodeIdToClientToWhitelistStatus[airnodeId][client]
-        .expirationTimestamp = expirationTimestamp;
-        emit ExtendedWhitelistExpiration(
-            airnodeId,
-            client,
-            expirationTimestamp,
-            msg.sender
-        );
-    }
-
-    /// @notice Called by a super admin to set the whitelisting expiration of a
-    /// client
-    /// @dev Unlike `extendWhitelistExpiration()`, this can hasten the expiration
-    /// @param airnodeId Airnode ID
-    /// @param client Client
-    /// @param expirationTimestamp Timestamp at which the whitelisting of the
-    /// client will expire
-    function setWhitelistExpiration(
-        bytes32 airnodeId,
-        address client,
-        uint64 expirationTimestamp
-    ) external override onlyWithRank(airnodeId, uint256(AdminRank.SuperAdmin)) {
-        airnodeIdToClientToWhitelistStatus[airnodeId][client]
-        .expirationTimestamp = expirationTimestamp;
-        emit SetWhitelistExpiration(
-            airnodeId,
-            client,
-            expirationTimestamp,
-            msg.sender
-        );
-    }
-
-    /// @notice Called by a super admin to set the whitelist status of a client
-    /// past expiration
-    /// @param airnodeId Airnode ID
-    /// @param client Client
-    /// @param status Whitelist status that the client will have past expiration
-    function setWhitelistStatusPastExpiration(
-        bytes32 airnodeId,
-        address client,
-        bool status
-    ) external override onlyWithRank(airnodeId, uint256(AdminRank.SuperAdmin)) {
-        airnodeIdToClientToWhitelistStatus[airnodeId][client]
-        .whitelistPastExpiration = status;
-        emit SetWhitelistStatusPastExpiration(
-            airnodeId,
-            client,
-            status,
-            msg.sender
-        );
-    }
-
     /// @notice Verifies the authorization status of a request
     /// @dev This method has redundant arguments because all authorizer
     /// contracts have to have the same interface and potential authorizer
@@ -110,7 +35,7 @@ abstract contract ClientRrpAuthorizer is
     ) external view override returns (bool) {
 
             WhitelistStatus storage whitelistStatus
-         = airnodeIdToClientToWhitelistStatus[airnodeId][client];
+         = serviceIdToClientToWhitelistStatus[airnodeId][client];
         return
             designatedWallet.balance != 0 &&
             (whitelistStatus.whitelistPastExpiration ||
---
+++ packages/protocol/contracts/authorizers/interfaces/IClientWhitelister.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+interface IClientWhitelister {
+    enum AdminRank {
+        Unauthorized,
+        Admin,
+        SuperAdmin
+    }
+
+    struct WhitelistStatus {
+        uint64 expirationTimestamp;
+        bool whitelistPastExpiration;
+    }
+
+    event ExtendedWhitelistExpiration(
+        bytes32 indexed serviceId,
+        address indexed client,
+        uint256 expiration,
+        address indexed admin
+    );
+
+    event SetWhitelistExpiration(
+        bytes32 indexed serviceId,
+        address indexed client,
+        uint256 expiration,
+        address indexed admin
+    );
+
+    event SetWhitelistStatusPastExpiration(
+        bytes32 indexed serviceId,
+        address indexed client,
+        bool status,
+        address indexed admin
+    );
+
+    function extendWhitelistExpiration(
+        bytes32 serviceId,
+        address client,
+        uint64 expirationTimestamp
+    ) external;
+
+    function setWhitelistExpiration(
+        bytes32 serviceId,
+        address client,
+        uint64 expirationTimestamp
+    ) external;
+
+    function setWhitelistStatusPastExpiration(
+        bytes32 serviceId,
+        address client,
+        bool status
+    ) external;
+
+    function serviceIdToClientToWhitelistStatus(
+        bytes32 serviceId,
+        address client
+    )
+        external
+        view
+        returns (uint64 expirationTimestamp, bool whitelistPastExpiration);
+}
