5b178a1451ce470156e5150fc329a8fb5ba750f3
bbenligiray <bbenligiray@gmail.com>
2021-07-27 15:37:06 +0300

    Prettify

--- packages/protocol/contracts/rrp/authorizers/interfaces/IApi3RequesterRrpAuthorizer.sol
+++ packages/protocol/contracts/rrp/authorizers/interfaces/IApi3RequesterRrpAuthorizer.sol
@@ -4,4 +4,7 @@ pragma solidity 0.8.6;
 import "../../../admin/interfaces/IMetaAdminnable.sol";
 import "./IRequesterRrpAuthorizer.sol";
 
-interface IApi3RequesterRrpAuthorizer is IMetaAdminnable, IRequesterRrpAuthorizer {}
+interface IApi3RequesterRrpAuthorizer is
+    IMetaAdminnable,
+    IRequesterRrpAuthorizer
+{}
