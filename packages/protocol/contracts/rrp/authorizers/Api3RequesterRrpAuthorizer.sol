3bbaff0290de3ff1a2df04bb898d6ffaac53bc7c
Ashar2shahid <Ashar2shahid@gmail.com>
2021-07-30 11:16:17 -0400

    add missing interface methods

--- packages/protocol/contracts/rrp/authorizers/Api3RequesterRrpAuthorizer.sol
+++ packages/protocol/contracts/rrp/authorizers/Api3RequesterRrpAuthorizer.sol
@@ -19,8 +19,9 @@ contract Api3RequesterRrpAuthorizer is
     constructor(address metaAdmin_) MetaAdminnable(metaAdmin_) {}
 
     /// @notice Called to get the rank of an admin for an adminned entity
-    /// @dev Overriden to use metaAdminned ranks
-    /// @param adminnedId ID of the entity being adminned
+    /// @dev Overriden to use the bytes32(0) as the only adminnedId
+    ///      Overriden to use metaAdminned ranks
+    /// @param adminnedId ID of the entity being adminned(not used)
     /// @param admin Admin address whose rank will be returned
     /// @return Admin rank
     function getRank(bytes32 adminnedId, address admin)
@@ -37,14 +38,14 @@ contract Api3RequesterRrpAuthorizer is
     /// @dev Use this instead of setRank(bytes32 adminnedId,address targetAdmin, uint256 newRank)
     /// @param targetAdmin Target admin address
     /// @param newRank Rank to be set
-    function setRank(address targetAdmin, uint256 newRank) public {
+    function setRank(address targetAdmin, uint256 newRank) public override {
         RankedAdminnable.setRank(bytes32(0), targetAdmin, newRank);
     }
 
     /// @notice Called by an admin to decrease its rank
     /// @dev Use this instead of setRank(bytes32 adminnedId, uint256 newRank)
     /// @param newRank Rank to be set
-    function decreaseSelfRank(uint256 newRank) public {
+    function decreaseSelfRank(uint256 newRank) public override {
         RankedAdminnable.decreaseSelfRank(bytes32(0), newRank);
     }
 }
