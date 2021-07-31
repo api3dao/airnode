19d47f8e46f0d3cad58b4d0fc2464df894a7562b
bbenligiray <bbenligiray@gmail.com>
2021-07-31 15:56:24 +0300

    Improve inline docs

--- packages/protocol/contracts/rrp/authorizers/Api3RequesterRrpAuthorizer.sol
+++ packages/protocol/contracts/rrp/authorizers/Api3RequesterRrpAuthorizer.sol
@@ -7,6 +7,12 @@ import "./interfaces/IApi3RequesterRrpAuthorizer.sol";
 
 /// @title Authorizer contract that whitelists requesters where the API3 DAO is
 /// the metaAdmin
+/// @dev Unlike the MetaAdminnable contract that it inherits, this contract
+/// does not support the independent adminning of different entities. In other
+/// words, when an address is assigned to be an Admin/SuperAdmin, it becomes
+/// the Admin/SuperAdmin of all entities being adminned. To achieve this, when
+/// interacting with this contract, always use `bytes32(0)` as the
+/// `adminnedId`.
 contract Api3RequesterRrpAuthorizer is
     MetaAdminnable,
     RequesterRrpAuthorizer,
@@ -19,12 +25,17 @@ contract Api3RequesterRrpAuthorizer is
     constructor(address metaAdmin_) MetaAdminnable(metaAdmin_) {}
 
     /// @notice Called to get the rank of an admin for an adminned entity
-    /// @dev Overriden to use the bytes32(0) as the only adminnedId
-    ///      Overriden to use metaAdminned ranks
-    /// @param adminnedId ID of the entity being adminned(not used)
+    /// @dev Overriden to use MetaAdminnable ranks and ignore the `adminnedId`
+    /// the caller provides. The caller is recommended to use `bytes32(0)` as
+    /// the `adminnedId` here, see the @dev string of the contract and the docs
+    /// for more information.
+    /// @param adminnedId ID of the entity being adminned (not used)
     /// @param admin Admin address whose rank will be returned
     /// @return Admin rank
-    function getRank(bytes32 adminnedId, address admin)
+    function getRank(
+        bytes32 adminnedId, // solhint-disable-line no-unused-vars
+        address admin
+    )
         public
         view
         override(MetaAdminnable, RankedAdminnable, IRankedAdminnable)
