d2285cd9502fae3de31fcd2191e4c0239a98fa22
drgy <drgonjak@gmail.com>
2021-08-13 19:54:43 +0200

    Remove unnecessary checks

--- packages/validator/src/commands/validateCmd.ts
+++ packages/validator/src/commands/validateCmd.ts
@@ -25,20 +25,12 @@ const args = yargs(hideBin(process.argv))
 // eslint-disable-next-line prefer-const
 let [template, version] = args.template.split('@');
 
-if (template) {
-  if (templates[template.toLowerCase() as keyof typeof templates]) {
-    template = utils.getPath(templates[template.toLowerCase() as keyof typeof templates], messages, version);
-  } else if (version) {
-    messages.push(logger.warn('Version argument will be ignored when validating provided template file'));
-  }
-
-  if (args['specification']) {
-    const res = validate(args['specification'], template);
-    res.messages.push(...messages);
-    console.log(JSON.stringify(res, null, 2));
-  } else {
-    console.log(logger.error('Path to json specification must be provided'));
-  }
-} else {
-  console.log(logger.error('Validator template must be provided'));
+if (templates[template.toLowerCase() as keyof typeof templates]) {
+  template = utils.getPath(templates[template.toLowerCase() as keyof typeof templates], messages, version);
+} else if (version) {
+  messages.push(logger.warn('Version argument will be ignored when validating provided template file'));
 }
+
+const res = validate(args['specification'], template);
+res.messages.push(...messages);
+console.log(JSON.stringify(res, null, 2));
