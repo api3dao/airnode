ae8afb885e3e87bf863469cb74ac0bf33fd561d8
Santiago Aceñolaza <acenolaza@users.noreply.github.com>
2021-08-18 14:13:00 -0300

    Fix admin package (#456)
    * Fixes admin package after protocol contracts refactor
    
    * Updates README
    
    * fix(#427):Admin package is not listening for the correct event
    
    * Simplified fulfillWithdrawal event log filtering
    
    * Reverted derive-sponsor-wallet to use xpub instead of mnemonic but now allows to pass xpub as command line arg
    
    * Renames deriveWalletPathFromAddress function
    
    * Endorse -> sponsor
    
    * Removes withdrawal destination from AirnodeRrp

--- packages/admin/src/implementation.test.ts
+++ packages/admin/src/implementation.test.ts
@@ -1,41 +1,41 @@
 import { ethers } from 'ethers';
-import { addressToDerivationPath } from './implementation';
+import { deriveWalletPathFromSponsorAddress } from './implementation';
 
-describe('addressToDerivationPath', () => {
+describe('deriveWalletPathFromSponsorAddress', () => {
   it('converts address to derivation path', () => {
     const address = '0x8A45eac0267dD0803Fd957723EdE10693A076698';
-    const res = addressToDerivationPath(address);
-    expect(res).toEqual('973563544/2109481170/2137349576/871269377/610184194/17');
+    const res = deriveWalletPathFromSponsorAddress(address);
+    expect(res).toEqual('m/0/973563544/2109481170/2137349576/871269377/610184194/17');
 
     const randomAddress = ethers.utils.getAddress(ethers.utils.hexlify(ethers.utils.randomBytes(20)));
-    const randomPath = addressToDerivationPath(randomAddress);
+    const randomPath = deriveWalletPathFromSponsorAddress(randomAddress);
     expect(res).not.toEqual(randomPath);
   });
   it('converts zero address to derivation path', () => {
     const address = ethers.constants.AddressZero;
-    const res = addressToDerivationPath(address);
-    expect(res).toEqual('0/0/0/0/0/0');
+    const res = deriveWalletPathFromSponsorAddress(address);
+    expect(res).toEqual('m/0/0/0/0/0/0/0');
   });
   it('throws if address is null', () => {
     const address = null;
-    expect(() => addressToDerivationPath(address!)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address!)).toThrow('invalid address');
   });
   it('throws if address is undefined', () => {
     const address = undefined;
-    expect(() => addressToDerivationPath(address!)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address!)).toThrow('invalid address');
   });
   it('throws if address is an empty string', () => {
     const address = '';
-    expect(() => addressToDerivationPath(address)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address)).toThrow('invalid address');
   });
   it('throws if address is invalid', () => {
     let address = '7dD0803Fd957723EdE10693A076698';
-    expect(() => addressToDerivationPath(address)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address)).toThrow('invalid address');
 
     address = ethers.utils.hexlify(ethers.utils.randomBytes(4));
-    expect(() => addressToDerivationPath(address)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address)).toThrow('invalid address');
 
     address = ethers.utils.hexlify(ethers.utils.randomBytes(32));
-    expect(() => addressToDerivationPath(address)).toThrow('invalid address');
+    expect(() => deriveWalletPathFromSponsorAddress(address)).toThrow('invalid address');
   });
 });
