9e9bf1a21cd3e2e5b39f1e8381d1ccf9df07d410
Santiago Aceñolaza <acenolaza@users.noreply.github.com>
2021-08-19 10:26:43 -0300

    AN-107: Fixes protocol event names (#511)

--- packages/node/src/evm/requests/events.ts
+++ packages/node/src/evm/requests/events.ts
@@ -1,48 +1,42 @@
 import {
   EVMEventLog,
-  EVMRequestCreatedLog,
-  EVMRequestFulfilledLog,
-  EVMWithdrawalRequestLog,
-  EVMWithdrawalFulfilledLog,
-  EVMFullApiRequestCreatedLog,
-  EVMTemplateRequestCreatedLog,
+  EVMMadeRequestLog,
+  EVMFulfilledRequestLog,
+  EVMRequestedWithdrawalLog,
+  EVMFulfilledWithdrawalLog,
+  EVMMadeFullRequestLog,
+  EVMMadeTemplateRequestLog,
 } from '../../types';
 import { airnodeRrpTopics } from '../contracts';
 
-export const API_CALL_REQUEST_TOPICS = [
-  airnodeRrpTopics.ClientRequestCreated,
-  airnodeRrpTopics.ClientFullRequestCreated,
-];
+export const API_CALL_REQUEST_TOPICS = [airnodeRrpTopics.MadeTemplateRequest, airnodeRrpTopics.MadeFullRequest];
 
-export const API_CALL_FULFILLED_TOPICS = [
-  airnodeRrpTopics.ClientRequestFulfilled,
-  airnodeRrpTopics.ClientRequestFailed,
-];
+export const API_CALL_FULFILLED_TOPICS = [airnodeRrpTopics.MadeTemplateRequest, airnodeRrpTopics.FailedRequest];
 
-export const WITHDRAWAL_REQUEST_TOPICS = [airnodeRrpTopics.WithdrawalRequested];
+export const WITHDRAWAL_REQUEST_TOPICS = [airnodeRrpTopics.RequestedWithdrawal];
 
-export const WITHDRAWAL_FULFILLED_TOPICS = [airnodeRrpTopics.WithdrawalFulfilled];
+export const WITHDRAWAL_FULFILLED_TOPICS = [airnodeRrpTopics.FulfilledWithdrawal];
 
-export function isApiCallRequest(log: EVMEventLog): log is EVMRequestCreatedLog {
+export function isApiCallRequest(log: EVMEventLog): log is EVMMadeRequestLog {
   return API_CALL_REQUEST_TOPICS.includes(log.parsedLog.topic);
 }
 
-export function isTemplateApiRequest(log: EVMEventLog): log is EVMTemplateRequestCreatedLog {
-  return log.parsedLog.topic === airnodeRrpTopics.ClientRequestCreated;
+export function isTemplateApiRequest(log: EVMEventLog): log is EVMMadeTemplateRequestLog {
+  return log.parsedLog.topic === airnodeRrpTopics.MadeTemplateRequest;
 }
 
-export function isFullApiRequest(log: EVMEventLog): log is EVMFullApiRequestCreatedLog {
-  return log.parsedLog.topic === airnodeRrpTopics.ClientFullRequestCreated;
+export function isFullApiRequest(log: EVMEventLog): log is EVMMadeFullRequestLog {
+  return log.parsedLog.topic === airnodeRrpTopics.MadeFullRequest;
 }
 
-export function isApiCallFulfillment(log: EVMEventLog): log is EVMRequestFulfilledLog {
+export function isApiCallFulfillment(log: EVMEventLog): log is EVMFulfilledRequestLog {
   return API_CALL_FULFILLED_TOPICS.includes(log.parsedLog.topic);
 }
 
-export function isWithdrawalRequest(log: EVMEventLog): log is EVMWithdrawalRequestLog {
+export function isWithdrawalRequest(log: EVMEventLog): log is EVMRequestedWithdrawalLog {
   return WITHDRAWAL_REQUEST_TOPICS.includes(log.parsedLog.topic);
 }
 
-export function isWithdrawalFulfillment(log: EVMEventLog): log is EVMWithdrawalFulfilledLog {
+export function isWithdrawalFulfillment(log: EVMEventLog): log is EVMFulfilledWithdrawalLog {
   return WITHDRAWAL_FULFILLED_TOPICS.includes(log.parsedLog.topic);
 }
