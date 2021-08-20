a4df329d1ecbd6c62999e1f9008f2b987e843f54
Michal Kimle <kimle.michal@gmail.com>
2021-08-20 14:33:11 +0200

    Remove unnecessary env variable setup for tests

--- packages/node/jest-unit.config.js
+++ packages/node/jest-unit.config.js
@@ -5,6 +5,6 @@ module.exports = {
   // Add custom settings below
   displayName: 'unit',
   name: 'unit',
-  setupFiles: ['<rootDir>/test/setup/init/set-define-property.ts', '<rootDir>/test/setup/init/set-env-vars.ts'],
+  setupFiles: ['<rootDir>/test/setup/init/set-define-property.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 };
