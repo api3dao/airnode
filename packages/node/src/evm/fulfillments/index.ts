baa01f64fa89cc457d2b5ef92b712410269db341
Emanuel Tesar <e.tesarr@gmail.com>
2021-09-02 15:30:58 +0200

    Do sponsor requests sequentially, fix e2e tests

--- packages/node/src/evm/fulfillments/index.ts
+++ packages/node/src/evm/fulfillments/index.ts
@@ -1,4 +1,5 @@
-import flatMap from 'lodash/flatMap';
+import { Transaction } from 'ethers';
+import { flatten } from 'lodash';
 import { submitApiCall } from './api-calls';
 import { submitWithdrawal } from './withdrawals';
 import * as grouping from '../../requests/grouping';
@@ -14,7 +15,29 @@ export interface Receipt {
   readonly type: RequestType;
 }
 
-export async function submit(state: ProviderState<EVMProviderState>) {
+interface RequestReceipt {
+  readonly id: string;
+  readonly type: RequestType;
+  readonly data?: Transaction;
+  readonly err?: Error | null;
+}
+
+// eslint-disable-next-line functional/no-mixed-type
+interface OrderedRequest {
+  readonly nonce: number;
+  readonly makeRequest: () => Promise<RequestReceipt>;
+}
+
+/**
+ * Sponsor requests are performed from the sponsor wallet. Each transaction has a fixed nonce and blochchain rejects out
+ * of order transactions.
+ *
+ * This function makes all the sponsor requests sequentially (ordered by transaction nonce increasingly).
+ *
+ * There is a concept of batched requests, but that doesn't work with transactions. See:
+ * https://github.com/ethers-io/ethers.js/issues/892#issuecomment-828897859
+ */
+const submitSponsorRequestsSequentially = async (state: ProviderState<EVMProviderState>, sponsorAddress: string) => {
   const { chainId, chainType, name: providerName } = state.settings;
   const { coordinatorId } = state;
 
@@ -25,47 +48,72 @@ export async function submit(state: ProviderState<EVMProviderState>) {
   };
 
   const { AirnodeRrp } = state.contracts;
-
   const requestsBySponsorAddress = grouping.groupRequestsBySponsorAddress(state.requests);
+  const requests = requestsBySponsorAddress[sponsorAddress];
+  const sponsorWallet = wallet.deriveSponsorWallet(state.masterHDNode, sponsorAddress);
+  const signer = sponsorWallet.connect(state.provider);
+  const contract = AirnodeRrpFactory.connect(AirnodeRrp, signer);
 
-  const sponsorAddresses = Object.keys(requestsBySponsorAddress);
+  const txOptions: TransactionOptions = {
+    gasPrice: state.gasPrice!,
+    masterHDNode: state.masterHDNode,
+    provider: state.provider,
+  };
 
-  const promises = flatMap(sponsorAddresses, (sponsorAddress) => {
-    const requests = requestsBySponsorAddress[sponsorAddress];
-    const sponsorWallet = wallet.deriveSponsorWallet(state.masterHDNode, sponsorAddress);
-    const signer = sponsorWallet.connect(state.provider);
-    const contract = AirnodeRrpFactory.connect(AirnodeRrp, signer);
-
-    const txOptions: TransactionOptions = {
-      gasPrice: state.gasPrice!,
-      masterHDNode: state.masterHDNode,
-      provider: state.provider,
-    };
-
-    // Submit transactions for API calls
-    const submittedApiCalls = requests.apiCalls.map(async (apiCall) => {
+  // Submit transactions for API calls
+  const submittedApiCalls = requests.apiCalls.map((apiCall): OrderedRequest => {
+    const makeRequest = async () => {
       const [logs, err, data] = await submitApiCall(contract, apiCall, txOptions);
       logger.logPending(logs, baseLogOptions);
       if (err || !data) {
         return { id: apiCall.id, type: RequestType.ApiCall, error: err };
       }
       return { id: apiCall.id, type: RequestType.ApiCall, data };
-    });
+    };
 
-    // Submit transactions for withdrawals
-    const submittedWithdrawals = requests.withdrawals.map(async (withdrawal) => {
+    return {
+      nonce: apiCall.nonce!,
+      makeRequest,
+    };
+  });
+
+  // Submit transactions for withdrawals
+  const submittedWithdrawals = requests.withdrawals.map((withdrawal): OrderedRequest => {
+    const makeRequest = async () => {
       const [logs, err, data] = await submitWithdrawal(contract, withdrawal, txOptions);
       logger.logPending(logs, baseLogOptions);
       if (err || !data) {
         return { id: withdrawal.id, type: RequestType.Withdrawal, error: err };
       }
       return { id: withdrawal.id, type: RequestType.Withdrawal, data };
-    });
+    };
 
-    return [...submittedApiCalls, ...submittedWithdrawals];
+    return {
+      nonce: withdrawal.nonce!,
+      makeRequest,
+    };
   });
 
-  const responses = await Promise.all(promises);
+  const allRequests = [...submittedApiCalls, ...submittedWithdrawals];
+  // Sort by the nonce value increasingly
+  allRequests.sort((a, b) => a.nonce - b.nonce);
 
-  return responses;
+  const receipts: RequestReceipt[] = [];
+  // Perform the requests sequentially to in order to respect the nonce value
+  //
+  // eslint-disable-next-line functional/no-loop-statement
+  for (const request of allRequests) {
+    receipts.push(await request.makeRequest());
+  }
+
+  return receipts;
+};
+
+export async function submit(state: ProviderState<EVMProviderState>) {
+  const requestsBySponsorAddress = grouping.groupRequestsBySponsorAddress(state.requests);
+  const sponsorAddresses = Object.keys(requestsBySponsorAddress);
+
+  const promises = sponsorAddresses.map((address) => submitSponsorRequestsSequentially(state, address));
+  const nestedReceipts = await Promise.all(promises);
+  return flatten(nestedReceipts);
 }
--- packages/node/test/e2e/requester-fulfill.feature.ts
+++ packages/node/test/e2e/requester-fulfill.feature.ts
@@ -3,6 +3,8 @@ import * as handlers from '../../src/workers/local-handlers';
 import * as fixtures from '../fixtures';
 import * as e2e from '../setup/e2e';
 
+const expectSameRequestId = (req1: any, req2: any) => expect(req1.args.requestId).toBe(req2.args.requestId);
+
 it('should call fail function on AirnodeRrp contract and emit FailedRequest if requester contract fulfillment fails', async () => {
   jest.setTimeout(45_000);
 
@@ -80,16 +82,14 @@ it('should call fail function on AirnodeRrp contract and emit FailedRequest if r
 
   await e2e.makeRequests(deployConfig, deployment);
 
-  const preinvokeLogs = await e2e.fetchAllLogs(provider, deployment.contracts.AirnodeRrp);
-
-  const preinvokeTemplateRequests = preinvokeLogs.filter((log) => log.name === 'MadeTemplateRequest');
-  const preinvokeFullRequests = preinvokeLogs.filter((log) => log.name === 'MadeFullRequest');
-  const preinvokeFulfillments = preinvokeLogs.filter((log) => log.name === 'FulfilledRequest');
-
-  expect(preinvokeLogs.length).toEqual(5);
-  expect(preinvokeTemplateRequests.length).toEqual(1);
-  expect(preinvokeFullRequests.length).toEqual(1);
-  expect(preinvokeFulfillments.length).toEqual(0);
+  const preInvokelogNames = (await e2e.fetchAllLogs(provider, deployment.contracts.AirnodeRrp)).map(({ name }) => name);
+  expect(preInvokelogNames).toEqual([
+    'SetAirnodeXpub',
+    'SetSponsorshipStatus',
+    'CreatedTemplate',
+    'MadeTemplateRequest',
+    'MadeFullRequest',
+  ]);
 
   const chain = e2e.buildChainConfig(deployment.contracts);
   const config = fixtures.buildConfig({ chains: [chain] });
@@ -97,22 +97,22 @@ it('should call fail function on AirnodeRrp contract and emit FailedRequest if r
 
   await handlers.startCoordinator();
 
-  const postinvokeLogs = await e2e.fetchAllLogs(provider, deployment.contracts.AirnodeRrp);
+  const postInvokeLogs = await e2e.fetchAllLogs(provider, deployment.contracts.AirnodeRrp);
+  expect(postInvokeLogs.map(({ name }) => name)).toEqual([
+    'SetAirnodeXpub',
+    'SetSponsorshipStatus',
+    'CreatedTemplate',
+    'MadeTemplateRequest',
+    'MadeFullRequest',
+    'FailedRequest',
+    'FulfilledRequest',
+  ]);
 
-  const postinvokeTemplateRequests = postinvokeLogs.filter((log) => log.name === 'MadeTemplateRequest');
-  const postinvokeFullRequests = postinvokeLogs.filter((log) => log.name === 'MadeFullRequest');
-  const postinvokeFulfillmentsFailed = postinvokeLogs.filter((log) => log.name === 'FailedRequest');
-  const postinvokeFulfillments = postinvokeLogs.filter((log) => log.name === 'FulfilledRequest');
+  const failedRequest = postInvokeLogs.filter(({ name }) => name === 'FailedRequest')[0];
+  const templateRequest = postInvokeLogs.filter(({ name }) => name === 'MadeTemplateRequest')[0];
+  expectSameRequestId(templateRequest, failedRequest);
 
-  expect(postinvokeLogs.length).toEqual(7);
-  expect(postinvokeTemplateRequests.length).toEqual(1);
-  expect(postinvokeFullRequests.length).toEqual(1);
-  expect(postinvokeFulfillmentsFailed.length).toEqual(1);
-  expect(postinvokeFulfillments.length).toEqual(1);
-
-  // Check each fulfillment failed is linked to a request
-  postinvokeFulfillmentsFailed.forEach((fulfillment) => {
-    const request = postinvokeLogs.find((log) => log.args.requestId === fulfillment.args.requestId);
-    expect(request).toBeDefined();
-  });
+  const fulfilledRequest = postInvokeLogs.filter(({ name }) => name === 'FulfilledRequest')[0];
+  const fullRequest = postInvokeLogs.filter(({ name }) => name === 'MadeFullRequest')[0];
+  expectSameRequestId(fulfilledRequest, fullRequest);
 });
