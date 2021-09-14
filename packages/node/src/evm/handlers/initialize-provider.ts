650df1a1c3cd308b1d452c668de01ac99c28360e
Santiago Aceñolaza <acenolaza@users.noreply.github.com>
2021-09-14 11:35:51 -0300

    AN-69: Verify sponsor wallets for withdrawal requests (#560)

--- packages/node/src/evm/handlers/initialize-provider.ts
+++ packages/node/src/evm/handlers/initialize-provider.ts
@@ -94,13 +94,13 @@ export async function initializeProvider(
   });
 
   // =================================================================
-  // STEP 5: Verify API calls
+  // STEP 5: Verify requests
   // =================================================================
-  const [verifyLogs, verifiedApiCalls] = verification.verifySponsorWallets(
+  const [verifyApiCallLogs, verifiedApiCalls] = verification.verifySponsorWallets(
     state4.requests.apiCalls,
     state4.masterHDNode
   );
-  logger.logPending(verifyLogs, baseLogOptions);
+  logger.logPending(verifyApiCallLogs, baseLogOptions);
 
   const [verifyRrpTriggersLogs, verifiedApiCallsForRrpTriggers] = verification.verifyRrpTriggers(
     verifiedApiCalls,
@@ -109,8 +109,14 @@ export async function initializeProvider(
   );
   logger.logPending(verifyRrpTriggersLogs, baseLogOptions);
 
+  const [verifyWithdrawalLogs, verifiedWithdrawals] = verification.verifySponsorWallets(
+    state4.requests.withdrawals,
+    state4.masterHDNode
+  );
+  logger.logPending(verifyWithdrawalLogs, baseLogOptions);
+
   const state5 = state.update(state4, {
-    requests: { ...state3.requests, apiCalls: verifiedApiCallsForRrpTriggers },
+    requests: { ...state3.requests, apiCalls: verifiedApiCallsForRrpTriggers, withdrawals: verifiedWithdrawals },
   });
 
   // =================================================================
