33a6ecd9843c85cc573ef9984ae60ff22ff34805
Santiago Aceñolaza <santiago@api3.org>
2021-09-21 11:19:14 -0300

    AN-208: Fix node e2e tests in beta-protocol-revision after merging beta-protocol changes

--- packages/node/test/e2e/requester-fulfill.feature.ts
+++ packages/node/test/e2e/requester-fulfill.feature.ts
@@ -43,29 +43,15 @@ it('should call fail function on AirnodeRrp contract and emit FailedRequest if r
     operation.buildFullRequest(),
   ]);
 
+  const preInvokeExpectedLogs = ['MadeTemplateRequest', 'MadeFullRequest'];
   const preInvokelogNames = await fetchAllLogNames(provider, deployment.contracts.AirnodeRrp);
-  expect(preInvokelogNames).toEqual([
-    'SetSponsorshipStatus', // RrpRequester constructor
-    'SetAirnodeXpub',
-    'SetSponsorshipStatus',
-    'CreatedTemplate',
-    'MadeTemplateRequest',
-    'MadeFullRequest',
-  ]);
+  expect(preInvokelogNames).toEqual(expect.arrayContaining(preInvokeExpectedLogs));
 
   await startCoordinator();
 
+  const postInvokeExpectedLogs = [...preInvokeExpectedLogs, 'FailedRequest', 'FulfilledRequest'];
   const postInvokeLogs = await fetchAllLogs(provider, deployment.contracts.AirnodeRrp);
-  expect(postInvokeLogs.map(({ name }) => name)).toEqual([
-    'SetSponsorshipStatus', // RrpRequester constructor
-    'SetAirnodeXpub',
-    'SetSponsorshipStatus',
-    'CreatedTemplate',
-    'MadeTemplateRequest',
-    'MadeFullRequest',
-    'FailedRequest',
-    'FulfilledRequest',
-  ]);
+  expect(postInvokeLogs.map(({ name }) => name)).toEqual(expect.arrayContaining(postInvokeExpectedLogs));
 
   const failedRequest = filterLogsByName(postInvokeLogs, 'FailedRequest')[0];
   const templateRequest = filterLogsByName(postInvokeLogs, 'MadeTemplateRequest')[0];
