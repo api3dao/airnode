e653927653739db66d961e5a80eb64f0f92b56c4
Santiago Aceñolaza <santiago@api3.org>
2021-09-23 13:47:42 -0300

    Deploys AirnodeRrp and AirnodeRequesterRrpAuthorizer to Rinkebi

--- packages/protocol/deploy/1_deploy.js
+++ packages/protocol/deploy/1_deploy.js
@@ -2,8 +2,15 @@ module.exports = async ({ getUnnamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const accounts = await getUnnamedAccounts();
 
+  const airnodeRequesterRrpAuthorizer = await deploy('AirnodeRequesterRrpAuthorizer', {
+    from: accounts[2],
+    log: true,
+  });
+  log(`Deployed AirnodeRequesterRrpAuthorizer at ${airnodeRequesterRrpAuthorizer.address}`);
+
   const airnodeRrp = await deploy('AirnodeRrp', {
-    from: accounts[0],
+    from: accounts[2],
+    log: true,
   });
   log(`Deployed Airnode RRP at ${airnodeRrp.address}`);
 };
