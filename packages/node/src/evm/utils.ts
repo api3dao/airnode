5cb97cab5f13e4265c3732b200431f0a24e9d359
Santiago Aceñolaza <santiago@api3.org>
2021-09-28 17:28:25 -0300

    AN-225: Return an error string instead of a status code

--- packages/node/src/evm/utils.ts
+++ packages/node/src/evm/utils.ts
@@ -16,3 +16,14 @@ export function sortBigNumbers(bigNumbers: ethers.BigNumber[]) {
   // Puts the highest BigNumber first
   return bigNumbers.sort((a, b) => (a.gte(b) ? -1 : 1));
 }
+
+export function decodeRevertString(callData: string) {
+  // Refer to https://ethereum.stackexchange.com/a/83577
+  // eslint-disable-next-line functional/no-try-statement
+  try {
+    // Skip the signature, only get the revert string
+    return ethers.utils.defaultAbiCoder.decode(['string'], `0x${callData.substring(2 + 4 * 2)}`)[0];
+  } catch {
+    return 'No revert string';
+  }
+}
