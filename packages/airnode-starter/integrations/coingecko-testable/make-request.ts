4490eb43b09e5bc7474e1c78b7c7ab4fbd1145f0
Emanuel Tesar <e.tesarr@gmail.com>
2021-09-28 15:44:49 +0200

    Simplify make request script

--- packages/airnode-starter/integrations/coingecko-testable/make-request.ts
+++ packages/airnode-starter/integrations/coingecko-testable/make-request.ts
@@ -1,55 +1,17 @@
-import { execSync } from 'child_process';
 import { encode } from '@api3/airnode-abi';
-import { ethers } from 'ethers';
-import {
-  getAirnodeWallet,
-  getAirnodeXpub,
-  getDeployedContract,
-  getProvider,
-  readConfig,
-  readIntegrationInfo,
-} from '../../src';
+import { getDeployedContract, readIntegrationInfo } from '../../src';
 
 const coinLabel = 'Ethereum';
 const coinId = coinLabel.toLowerCase();
 
-export async function makeRequest(): Promise<string> {
-  const integrationInfo = readIntegrationInfo();
-  const requester = await getDeployedContract(`contracts/${integrationInfo.integration}/Requester.sol`);
-  const airnodeRrp = await getDeployedContract('@api3/protocol/contracts/rrp/AirnodeRrp.sol');
-  const airnodeWallet = getAirnodeWallet();
-  const sponsor = ethers.Wallet.fromMnemonic(integrationInfo.mnemonic);
-  const endpointId = readConfig().triggers.rrp[0].endpointId;
-
-  const args = [
-    `--providerUrl ${integrationInfo.providerUrl}`,
-    `--airnodeRrp ${airnodeRrp.address}`,
-    `--airnodeAddress ${airnodeWallet.address}`,
-    `--sponsorAddress ${sponsor.address}`,
-    `--xpub ${getAirnodeXpub(airnodeWallet)}`,
-  ];
-  const output = execSync(`yarn api3-admin derive-sponsor-wallet-address ${args.join(' ')}`).toString();
-  const sponsorWalletAddress = output.split('Sponsor wallet address:')[1].trim();
-
-  const receipt = await requester.makeRequest(
-    airnodeWallet.address,
-    endpointId,
-    sponsor.address,
-    sponsorWalletAddress,
-    encode([{ name: 'coinId', type: 'bytes32', value: coinId }])
-  );
-
-  return new Promise((resolve) =>
-    getProvider().once(receipt.hash, (tx) => {
-      const parsedLog = airnodeRrp.interface.parseLog(tx.logs[0]);
-      resolve(parsedLog.args.requestId);
-    })
-  );
+export async function getEncodedParameters() {
+  return encode([{ name: 'coinId', type: 'bytes32', value: coinId }]);
 }
 
 export async function printResponse(requestId: string) {
   const integrationInfo = readIntegrationInfo();
   const requester = await getDeployedContract(`contracts/${integrationInfo.integration}/Requester.sol`);
 
-  console.log(`${coinLabel} price is ${await requester.fulfilledData(requestId)} USD`);
+  // Divided by 1e6, because the response value is multiplied with 1e6 by Airnode
+  console.log(`${coinLabel} price is ${(await requester.fulfilledData(requestId)) / 1e6} USD`);
 }
--- packages/airnode-starter/scripts/make-request.ts
+++ packages/airnode-starter/scripts/make-request.ts
@@ -1,4 +1,15 @@
-import { getDeployedContract, getProvider, readIntegrationInfo, runAndHandleErrors } from '../src';
+import { execSync } from 'child_process';
+import { ethers } from 'ethers';
+import {
+  getDeployedContract,
+  getProvider,
+  readIntegrationInfo,
+  runAndHandleErrors,
+  getAirnodeWallet,
+  getAirnodeXpub,
+  readConfig,
+  IntegrationInfo,
+} from '../src';
 
 async function fulfilled(requestId: string) {
   const airnodeRrp = await getDeployedContract('@api3/protocol/contracts/rrp/AirnodeRrp.sol');
@@ -6,19 +17,63 @@ async function fulfilled(requestId: string) {
   return new Promise((resolve) => provider.once(airnodeRrp.filters.FulfilledRequest(null, requestId), resolve));
 }
 
-// TODO: maybe just call the make-request and handle all logic there...
-async function main() {
-  const integrationInfo = readIntegrationInfo();
-  // TODO: What to do if this file is misssing?
-  const { makeRequest, printResponse } = await import(`../integrations/${integrationInfo.integration}/make-request.ts`);
+export const deriveSponsorWalletAddress = (
+  integrationInfo: IntegrationInfo,
+  airnodeRrp: ethers.Contract,
+  airnodeWallet: ethers.Wallet,
+  sponsor: ethers.Wallet
+) => {
+  const command = [
+    `yarn api3-admin derive-sponsor-wallet-address`,
+    `--providerUrl ${integrationInfo.providerUrl}`,
+    `--airnodeRrp ${airnodeRrp.address}`,
+    `--airnodeAddress ${airnodeWallet.address}`,
+    `--sponsorAddress ${sponsor.address}`,
+    `--xpub ${getAirnodeXpub(airnodeWallet)}`,
+  ].join(' ');
 
+  const output = execSync(command).toString();
+  const sponsorWalletAddress = output.split('Sponsor wallet address:')[1].trim();
+  return sponsorWalletAddress;
+};
+
+export async function makeRequest(): Promise<string> {
+  const integrationInfo = readIntegrationInfo();
+  const requester = await getDeployedContract(`contracts/${integrationInfo.integration}/Requester.sol`);
+  const airnodeRrp = await getDeployedContract('@api3/protocol/contracts/rrp/AirnodeRrp.sol');
+  const airnodeWallet = getAirnodeWallet();
+  const sponsor = ethers.Wallet.fromMnemonic(integrationInfo.mnemonic);
+  const endpointId = readConfig().triggers.rrp[0].endpointId;
+  const sponsorWalletAddress = deriveSponsorWalletAddress(integrationInfo, airnodeRrp, airnodeWallet, sponsor);
+
+  // Make the request
+  const { getEncodedParameters } = await import(`../integrations/${integrationInfo.integration}/make-request.ts`);
+  const receipt = await requester.makeRequest(
+    airnodeWallet.address,
+    endpointId,
+    sponsor.address,
+    sponsorWalletAddress,
+    getEncodedParameters()
+  );
+
+  // Wait until the transaction was mined
+  return new Promise((resolve) =>
+    getProvider().once(receipt.hash, (tx) => {
+      const parsedLog = airnodeRrp.interface.parseLog(tx.logs[0]);
+      resolve(parsedLog.args.requestId);
+    })
+  );
+}
+
+async function main() {
   console.log('Making request...');
   const requestId = await makeRequest();
   console.log('Waiting for fulfillment...');
-
   await fulfilled(requestId);
   console.log('Request fulfilled');
 
+  const integrationInfo = readIntegrationInfo();
+  const { printResponse } = await import(`../integrations/${integrationInfo.integration}/make-request.ts`);
   await printResponse(requestId);
 }
 
