4767298db0cd2f9b38adb9422edc4a32be8a45ca
Emanuel Tesar <e.tesarr@gmail.com>
2021-09-28 15:23:55 +0200

    Simplify create secrets script

--- packages/airnode-starter/integrations/coingecko-testable/create-secrets.ts
+++ packages/airnode-starter/integrations/coingecko-testable/create-secrets.ts
@@ -1,25 +1,10 @@
-import { writeFileSync } from 'fs';
-import { join } from 'path';
 import { randomUUID } from 'crypto';
-import { ethers } from 'ethers';
-import { formatSecrets, getDeployedContract, readChainId, readIntegrationInfo } from '../../src';
+
+import { getCommonSecrets, writeSecrets } from '../utils';
 
 async function createSecrets() {
-  const integrationInfo = readIntegrationInfo();
-  const wallet = ethers.Wallet.createRandom();
-  const airnodeRrp = await getDeployedContract('@api3/protocol/contracts/rrp/AirnodeRrp.sol');
-
-  const secrets = [
-    `PROVIDER_URL=${integrationInfo.providerUrl}`,
-    `AIRNODE_WALLET_MNEMONIC=${wallet.mnemonic.phrase}`,
-    `HTTP_GATEWAY_API_KEY=${randomUUID()}`,
-    `AIRNODE_RRP_ADDRESS=${airnodeRrp.address}`,
-    `CHAIN_ID=${await readChainId()}`,
-    `CLOUD_PROVIDER_TYPE=${integrationInfo.airnodeType}`,
-  ];
-
-  writeFileSync(join(__dirname, 'secrets.env'), formatSecrets(secrets));
-  console.log(`We have created 'secrets.env' file with the necessary credentials for you.`);
+  const commonSecrets = await getCommonSecrets();
+  writeSecrets([...commonSecrets, `HTTP_GATEWAY_API_KEY=${randomUUID()}`]);
 }
 
 export default createSecrets;
--- packages/airnode-starter/integrations/coingecko/create-secrets.ts
+++ packages/airnode-starter/integrations/coingecko/create-secrets.ts
@@ -1,23 +1,8 @@
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-import { ethers } from 'ethers';
-import { readChainId, readIntegrationInfo, formatSecrets, getDeployedContract } from '../../src';
+import { getCommonSecrets, writeSecrets } from '../utils';
 
 async function createSecrets() {
-  const integrationInfo = readIntegrationInfo();
-  const wallet = ethers.Wallet.createRandom();
-  const airnodeRrp = await getDeployedContract('@api3/protocol/contracts/rrp/AirnodeRrp.sol');
-
-  const secrets = [
-    `PROVIDER_URL=${integrationInfo.providerUrl}`,
-    `AIRNODE_WALLET_MNEMONIC=${wallet.mnemonic.phrase}`,
-    `AIRNODE_RRP_ADDRESS=${airnodeRrp.address}`,
-    `CHAIN_ID=${await readChainId()}`,
-    `CLOUD_PROVIDER_TYPE=${integrationInfo.airnodeType}`,
-  ];
-
-  writeFileSync(join(__dirname, 'secrets.env'), formatSecrets(secrets));
-  console.log(`We have created 'secrets.env' file with the necessary credentials for you.`);
+  const secrets = await getCommonSecrets();
+  writeSecrets(secrets);
 }
 
 export default createSecrets;
