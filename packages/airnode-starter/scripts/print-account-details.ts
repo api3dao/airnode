1bf18063d932298e0e672fbabe6dc13a120ee371
Emanuel Tesar <e.tesarr@gmail.com>
2021-09-29 15:30:54 +0200

    Show more details when displaying user and sponsor wallets

--- packages/airnode-starter/scripts/print-account-details.ts
+++ packages/airnode-starter/scripts/print-account-details.ts
@@ -3,8 +3,16 @@ import { getUserWallet, runAndHandleErrors } from '../src';
 
 const main = async () => {
   const wallet = await getUserWallet();
-  const balance = ethers.utils.formatEther(await wallet.getBalance());
-  console.log(`The account derived from the mnemonic has address: "${wallet.address}" and contains "${balance}" ETH`);
+  const balance = await wallet.getBalance();
+
+  console.log(
+    `The account derived from the mnemonic has address: "${wallet.address}" and contains ${ethers.utils.formatEther(
+      balance
+    )} ETH`
+  );
+  if (balance.lt(ethers.utils.parseEther('0.3'))) {
+    console.log('Please make sure your account has at least 0.3 ETH');
+  }
 };
 
 runAndHandleErrors(main);
