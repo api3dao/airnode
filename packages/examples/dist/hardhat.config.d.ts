fec2aa0542f77d5b05c738d1d453cd208c980bfc
Emanuel Tesar <e.tesarr@gmail.com>
2021-10-08 19:33:02 +0200

    Remove dist directory from examples

--- packages/examples/dist/hardhat.config.d.ts
+++
@@ -1,5 +0,0 @@
-import { HardhatUserConfig } from 'hardhat/types';
-import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-ethers';
-declare const config: HardhatUserConfig;
-export default config;
