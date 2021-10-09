1a8b3234041e8104952a757cd8786a6c3a90a047
Emanuel Tesar <e.tesarr@gmail.com>
2021-10-09 14:08:29 +0200

    Add test configuration, plan for e2e tests

---
+++ packages/examples/test/coingecko-aws.feature.ts
@@ -0,0 +1,7 @@
+// NOTE: This test is skipped by default because it would be a nuisance to make sure the testnet wallet is funded. Also,
+// this test could fail a number of reasons and is not a good target to be run on CI.
+//
+// Instead, the developers should run this before making a release to double check that the most basic flow is correct.
+it.skip('TODO:', () => {
+  expect(1).toBe(1);
+});
---
+++ packages/examples/test/coingecko-local.feature.ts
@@ -0,0 +1,3 @@
+it('TODO:', () => {
+  expect(1).toBe(1);
+});
