8380e606b2128621834a21a7026d1cdc25163cb6
Emanuel Tesar <e.tesarr@gmail.com>
2021-10-09 21:25:45 +0200

    Add docker on aws + rinkeby test

--- packages/examples/test/coingecko-aws.feature.ts
+++
@@ -1,9 +0,0 @@
-// NOTE: This test is skipped by default because it would be a nuisance to make sure the testnet wallet is funded. Also,
-// this test could fail a number of reasons and is not a good target to be run on CI.
-//
-// Instead, the developers should run this before making a release to double check that the most basic flow is correct.
-
-// eslint-disable-next-line jest/no-disabled-tests
-it.skip('TODO:', () => {
-  expect(1).toBe(1);
-});
