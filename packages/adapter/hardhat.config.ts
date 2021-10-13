2febc5f09267d612347f281349cf0bd698fe38e9
Emanuel Tesar <e.tesarr@gmail.com>
2021-10-13 12:55:23 +0200

    Add contract testing configuration

---
+++ packages/adapter/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.6',
+};
+
+export default config;
