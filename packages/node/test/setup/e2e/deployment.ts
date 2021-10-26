4eb4730d5641c8a1ce0a224a584d461210bfc1b9
bbenligiray <bbenligiray@gmail.com>
2021-10-26 17:21:56 +0300

    Fix e2e-node tests

--- packages/node/test/setup/e2e/deployment.ts
+++ packages/node/test/setup/e2e/deployment.ts
@@ -6,25 +6,26 @@ export async function deployAirnodeRrp(config: operation.Config): Promise<operat
   // Deploy contracts
   const state2 = await operation.deployAirnodeRrp(state1);
   const state3 = await operation.deployRequesters(state2);
-  const state4 = await operation.deployAuthorizers(state3);
+  const state4 = await operation.deployAccessControlRegistry(state3);
+  const state5 = await operation.deployAuthorizers(state4);
 
   // Assign wallets
-  const state5 = await operation.assignAirnodeAccounts(state4);
-  const state6 = await operation.assignRequesterAccounts(state5);
-  const state7 = await operation.assignSponsorWallets(state6);
+  const state6 = await operation.assignAirnodeAccounts(state5);
+  const state7 = await operation.assignRequesterAccounts(state6);
+  const state8 = await operation.assignSponsorWallets(state7);
 
   // Fund wallets
-  const state8 = await operation.fundAirnodeAccounts(state7);
-  const state9 = await operation.fundSponsorAccounts(state8);
-  const state10 = await operation.fundSponsorWallets(state9);
+  const state9 = await operation.fundAirnodeAccounts(state8);
+  const state10 = await operation.fundSponsorAccounts(state9);
+  const state11 = await operation.fundSponsorWallets(state10);
 
   // Sponsor requester contracts
-  const state11 = await operation.sponsorRequesters(state10);
+  const state12 = await operation.sponsorRequesters(state11);
 
   // Create templates
-  const state12 = await operation.createTemplates(state11);
+  const state13 = await operation.createTemplates(state12);
 
-  const deployment = operation.buildSaveableDeployment(state12);
+  const deployment = operation.buildSaveableDeployment(state13);
 
   return deployment;
 }
