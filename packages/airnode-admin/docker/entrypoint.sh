5dceda547a624e39bb904bdbb0b6258e2243db85
Emanuel Tesar <e.tesarr@gmail.com>
2021-11-03 12:12:17 +0100

    Apply PR suggestions

--- packages/airnode-admin/docker/entrypoint.sh
+++ packages/airnode-admin/docker/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 ADMIN_BINARY="/usr/local/bin/airnode-admin"
-USER_ID="${USER_ID:-0}"
-GROUP_ID="${GROUP_ID:-0}"
 
-su-exec ${USER_ID}:${GROUP_ID} ${ADMIN_BINARY} "$@"
+node ${ADMIN_BINARY} "$@"
