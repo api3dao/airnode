b908e307acf07022bcd482a8e46c0afcde520110
Emanuel Tesar <e.tesarr@gmail.com>
2021-11-15 16:05:26 +0100

    Fix building docker artifacts

--- packages/airnode-adapter/hardhat.config.ts
+++ packages/airnode-adapter/hardhat.config.ts
@@ -3,7 +3,7 @@ import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9',
+  solidity: { version: '0.8.9' },
   paths: {
     tests: 'e2e',
   },
