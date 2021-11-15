8c583c2d622cc7aea731c49f6a3be478a92e8f7a
Emanuel Tesar <e.tesarr@gmail.com>
2021-11-15 22:42:59 +0100

    Remove misleading path separator

--- packages/airnode-adapter/src/constants.ts
+++ packages/airnode-adapter/src/constants.ts
@@ -3,4 +3,3 @@ export const artificialTypes = ['string32'] as const;
 export const baseResponseTypes = [...solidityBaseTypes, ...artificialTypes] as const;
 
 export const MULTIPLE_PARAMETERS_DELIMETER = ',';
-export const PATH_DELIMETER = ',';
