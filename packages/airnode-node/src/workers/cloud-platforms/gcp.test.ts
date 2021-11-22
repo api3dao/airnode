664247f96db3ff970ed9598981685011ef49dad6
Michal Kimle <kimle.michal@gmail.com>
2021-11-22 14:56:22 +0100

    Add GCP worker to airnode-node package

---
+++ packages/airnode-node/src/workers/cloud-platforms/gcp.test.ts
@@ -0,0 +1,67 @@
+const requestMock = jest.fn();
+const getIdTokenClientMock = jest.fn().mockImplementation(() => ({
+  request: requestMock,
+}));
+jest.mock('google-auth-library', () => ({
+  GoogleAuth: jest.fn().mockImplementation(() => ({
+    getIdTokenClient: getIdTokenClientMock,
+  })),
+}));
+
+import * as gcp from './gcp';
+import * as fixtures from '../../../test/fixtures';
+import { WorkerFunctionName } from '../../types';
+
+describe('spawn', () => {
+  it('derives the function URL, authenticates, invokes and returns the response', async () => {
+    requestMock.mockImplementationOnce(() => ({ data: { value: 7777 } }));
+    const workerOpts = fixtures.buildWorkerOptions({
+      cloudProvider: { name: 'gcp', region: 'us-east1', projectId: 'projectId123' },
+    });
+    const parameters = {
+      ...workerOpts,
+      functionName: 'some-function' as WorkerFunctionName,
+      payload: { from: 'ETH', to: 'USD' },
+    };
+    const url = 'https://us-east1-projectId123.cloudfunctions.net/airnode-19255a4-test-some-function';
+
+    const res = await gcp.spawn(parameters);
+    expect(res).toEqual({ value: 7777 });
+
+    expect(getIdTokenClientMock).toHaveBeenCalledTimes(1);
+    expect(getIdTokenClientMock).toHaveBeenCalledWith(url);
+
+    expect(requestMock).toHaveBeenCalledTimes(1);
+    expect(requestMock).toHaveBeenCalledWith({
+      url,
+      method: 'POST',
+      data: parameters.payload,
+    });
+  });
+
+  it('throws an error if the cloud function returns an error', async () => {
+    expect.assertions(5);
+    requestMock.mockRejectedValueOnce(new Error('Something went wrong'));
+    const workerOpts = fixtures.buildWorkerOptions({
+      cloudProvider: { name: 'gcp', region: 'us-east1', projectId: 'projectId123' },
+    });
+    const parameters = {
+      ...workerOpts,
+      functionName: 'some-function' as WorkerFunctionName,
+      payload: { from: 'ETH', to: 'USD' },
+    };
+    const url = 'https://us-east1-projectId123.cloudfunctions.net/airnode-19255a4-test-some-function';
+
+    await expect(gcp.spawn(parameters)).rejects.toThrow(new Error('Something went wrong'));
+
+    expect(getIdTokenClientMock).toHaveBeenCalledTimes(1);
+    expect(getIdTokenClientMock).toHaveBeenCalledWith(url);
+
+    expect(requestMock).toHaveBeenCalledTimes(1);
+    expect(requestMock).toHaveBeenCalledWith({
+      url,
+      method: 'POST',
+      data: parameters.payload,
+    });
+  });
+});
---
+++ packages/airnode-node/src/workers/cloud-platforms/gcp.ts
@@ -0,0 +1,20 @@
+import { GoogleAuth } from 'google-auth-library';
+import { GcpCloudProvider, WorkerParameters, WorkerResponse } from '../../types';
+
+export async function spawn(params: WorkerParameters): Promise<WorkerResponse> {
+  const auth = new GoogleAuth();
+
+  const resolvedName = `airnode-${params.airnodeAddressShort}-${params.stage}-${params.functionName}`;
+  const cloudProvider = params.cloudProvider as GcpCloudProvider;
+  const url = `https://${cloudProvider.region}-${cloudProvider.projectId}.cloudfunctions.net/${resolvedName}`;
+
+  const axiosClient = await auth.getIdTokenClient(url);
+
+  const response = await axiosClient.request<WorkerResponse>({
+    url,
+    method: 'POST',
+    data: params.payload,
+  });
+
+  return response.data;
+}
