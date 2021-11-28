4d51a379381bd7962511238ad2e2e7072eb122bb
aquarat <blue@aquarat.co.za>
2021-11-28 12:08:07 +0200

    AN-364: Replace Alpine with Debian in all docker builds

--- packages/airnode-deployer/docker/entrypoint.sh
+++ packages/airnode-deployer/docker/entrypoint.sh
@@ -4,4 +4,4 @@ DEPLOYER_BINARY="/usr/local/bin/airnode-deployer"
 USER_ID="${USER_ID:-0}"
 GROUP_ID="${GROUP_ID:-0}"
 
-su-exec ${USER_ID}:${GROUP_ID} ${DEPLOYER_BINARY} "$@"
+gosu ${USER_ID}:${GROUP_ID} ${DEPLOYER_BINARY} "$@"
