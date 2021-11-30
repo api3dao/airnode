65cf90bbb8a27d7adf5e50f86778b385de8571fe
Michal Kimle <kimle.michal@gmail.com>
2021-11-30 13:06:04 +0100

    Improve common handler typing

--- packages/airnode-deployer/src/entrypoint.ts
+++ packages/airnode-deployer/src/entrypoint.ts
@@ -8,27 +8,27 @@ async function cloudHandler() {
   return await import(/* webpackIgnore: true */ `./handlers/${cloudProvider}`);
 }
 
-export async function startCoordinator(...args: any[]) {
+export async function startCoordinator(...args: unknown[]) {
   const handler = await cloudHandler();
   return await handler.startCoordinator(...args);
 }
 
-export async function initializeProvider(...args: any[]) {
+export async function initializeProvider(...args: unknown[]) {
   const handler = await cloudHandler();
   return await handler.initializeProvider(...args);
 }
 
-export async function callApi(...args: any[]) {
+export async function callApi(...args: unknown[]) {
   const handler = await cloudHandler();
   return await handler.callApi(...args);
 }
 
-export async function processProviderRequests(...args: any[]) {
+export async function processProviderRequests(...args: unknown[]) {
   const handler = await cloudHandler();
   return await handler.processProviderRequests(...args);
 }
 
-export async function testApi(...args: any[]) {
+export async function testApi(...args: unknown[]) {
   const handler = await cloudHandler();
   return await handler.testApi(...args);
 }
