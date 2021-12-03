b08d1db60d886ba74e64c5910632a08579834a16
Emanuel Tesar <e.tesarr@gmail.com>
2021-12-03 21:35:13 +0100

    Fix airnode-examples always looking for AWS credentials

--- packages/airnode-examples/scripts/deploy-airnode.ts
+++ packages/airnode-examples/scripts/deploy-airnode.ts
@@ -12,8 +12,8 @@ const main = async () => {
   const secretsFilePath = join(__dirname, '../aws.env');
   const deployCommand = [
     `docker run -it --rm`,
-    `--env-file ${secretsFilePath}`,
     `-e USER_ID=$(id -u) -e GROUP_ID=$(id -g)`,
+    integrationInfo.airnodeType === 'aws' && `--env-file ${secretsFilePath}`,
     integrationInfo.airnodeType === 'gcp' && `-v "\${HOME}/.config/gcloud:/app/gcloud"`,
     `-v ${integrationPath}:/app/config`,
     `-v ${integrationPath}:/app/output`,
