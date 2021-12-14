6c0749c24338fce38c14284bdadfc0c4a64f4db4
Emanuel Tesar <e.tesarr@gmail.com>
2021-12-14 16:36:18 +0100

    Fix e2e test

--- packages/airnode-node/test/e2e/test-api.feature.ts
+++ packages/airnode-node/test/e2e/test-api.feature.ts
@@ -10,18 +10,20 @@ it('makes a call to test the API', async () => {
     _type: 'int256',
     _path: 'result',
   };
-
   // EndpointID from the trigger fixture ../fixtures/config/config.ts
   const endpointId = '0x13dea3311fe0d6b84f4daeab831befbc49e19e6494c41e9e065a09c3c68f43b6';
-  // Value is returned by the mock server from the operation package
+
+  const result = await testApi(endpointId, parameters);
+
   const expected = {
+    // Value is returned by the mock server from the operation package
     value: JSON.stringify({
       rawValue: { success: true, result: '723.392028' },
       encodedValue: '0x00000000000000000000000000000000000000000000000000000000044fcf02',
       values: ['72339202'],
     }),
+    success: true,
+    signature: 'not-yet-supported',
   };
-
-  const result = await testApi(endpointId, parameters);
   expect(result).toEqual(expected);
 });
