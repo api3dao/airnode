681515e58f69bfbecfdf9f9b53cae940c8c7eb09
bbenligiray <bbenligiray@gmail.com>
2021-12-17 19:52:32 +0300

    Provide additional documentation about the admin role in WhitelistRoles

--- packages/airnode-protocol/contracts/whitelist/WhitelistRoles.sol
+++ packages/airnode-protocol/contracts/whitelist/WhitelistRoles.sol
@@ -16,6 +16,10 @@ contract WhitelistRoles is RoleDeriver, AccessControlClient, IWhitelistRoles {
     //     └── (4) Indefinite whitelister
     // Their IDs are derived from the descriptions below. Refer to
     // AccessControlRegistry for more information.
+    // To clarify, the root role of the manager is the admin of (1), while (1)
+    // is the admin of (2), (3) and (4). So (1) is more of a "contract admin",
+    // while the `adminRole` used in AccessControl and AccessControlRegistry
+    // refers to a more general adminship relationship between roles.
     string public override adminRoleDescription;
     string
         public constant
@@ -27,7 +31,7 @@ contract WhitelistRoles is RoleDeriver, AccessControlClient, IWhitelistRoles {
         "Whitelist expiration setter";
     string public constant override INDEFINITE_WHITELISTER_ROLE_DESCRIPTION =
         "Indefinite whitelister";
-    bytes32 internal immutable adminRoleDescriptionHash;
+    bytes32 internal adminRoleDescriptionHash;
     bytes32
         internal constant WHITELIST_EXPIRATION_EXTENDER_ROLE_DESCRIPTION_HASH =
         keccak256(
