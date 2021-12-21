e85a88141144bdc3140c0247b1767ceea3f9a7d7
bbenligiray <bbenligiray@gmail.com>
2021-12-21 13:25:05 +0300

    Implement AirnodeUser

---
+++ packages/airnode-protocol/contracts/WIP/AirnodeUser.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "./interfaces/IAirnodeProtocol.sol";
+import "./interfaces/IAirnodeUser.sol";
+
+/// @title The contract to be inherited to interact with the Airnode protocol
+contract AirnodeUser is IAirnodeUser {
+    address public immutable override airnodeProtocol;
+
+    /// @dev Reverts if the caller is not the Airnode protocol contract
+    modifier onlyAirnodeProtocol() {
+        require(
+            msg.sender == address(airnodeProtocol),
+            "Caller not Airnode protocol"
+        );
+        _;
+    }
+
+    /// @dev Airnode protocol contract address is set at deployment and is
+    /// immutable. AirnodeUser is made its own sponsor by default. AirnodeUser
+    /// can also be sponsored by others and use these sponsorships while making
+    /// requests, i.e., using this default sponsorship is optional.
+    /// @param _airnodeProtocol Airnode RRP contract address
+    constructor(address _airnodeProtocol) {
+        airnodeProtocol = _airnodeProtocol;
+        IAirnodeProtocol(_airnodeProtocol).setSponsorshipStatus(
+            address(this),
+            true
+        );
+    }
+}
---
+++ packages/airnode-protocol/contracts/WIP/interfaces/IAirnodeUser.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+interface IAirnodeUser {
+    function airnodeProtocol() external view returns (address);
+}
