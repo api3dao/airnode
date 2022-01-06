1e0d4bb6ad3df8fab4f9b27ffdaff3c975674630
bbenligiray <bbenligiray@gmail.com>
2022-01-06 15:12:21 +0300

    Fix test

--- packages/airnode-node/src/evm/contracts/airnodeRrp.test.ts
+++ packages/airnode-node/src/evm/contracts/airnodeRrp.test.ts
@@ -24,6 +24,7 @@ describe('AirnodeRrp', () => {
       'sponsorToRequesterToSponsorshipStatus',
       'sponsorToWithdrawalRequestCount',
       'templates',
+      'verifySignature',
     ]);
   });
 
