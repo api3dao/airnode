ab4f98029e497a652bf19f1005a25c94ce5a3618
Michal Kimle <kimle.michal@gmail.com>
2022-01-06 09:16:43 +0100

    Force stage deployment variable to be lowercase

--- packages/airnode-deployer/terraform/state/aws/main.tf
+++ packages/airnode-deployer/terraform/state/aws/main.tf
@@ -1,9 +1,9 @@
 module "terraform_state_backend_aws" {
   source        = "cloudposse/tfstate-backend/aws"
   version       = "0.33.0"
-  namespace     = var.infrastructure_name
-  environment   = var.airnode_address_short
-  stage         = var.stage
+  namespace     = lower(var.infrastructure_name)
+  environment   = lower(var.airnode_address_short)
+  stage         = lower(var.stage)
   name          = "terraform"
   delimiter     = "-"
   force_destroy = true
