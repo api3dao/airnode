c2502948e538ab1c3960aa40a416da0582f0b4b4
bbenligiray <bbenligiray@gmail.com>
2022-01-08 23:01:06 +0300

    Update AirnodeUser modifier

--- packages/airnode-protocol/contracts/WIP/AirnodeUser.sol
+++ packages/airnode-protocol/contracts/WIP/AirnodeUser.sol
@@ -9,12 +9,20 @@ contract AirnodeUser is IAirnodeUser {
     /// @notice AirnodeProtocol contract address
     address public immutable override airnodeProtocol;
 
-    /// @dev Reverts if the sender is not the Airnode protocol contract
-    modifier onlyAirnodeProtocol() {
+    /// @dev Reverts if the sender is not the Airnode protocol contract or if
+    /// the signature is not at most 1 hour old. Use this modifier with methods
+    /// that are meant to receive Airnode request or subscription fulfillments.
+    /// The signature age is checked to confirm that:
+    /// (1) The fulfillment was confirmed in less than an hour
+    /// (2) The sponsor wallet address was attested to less than an hour ago
+    /// @param timestamp Timestamp used in the signature
+    modifier onlyValidAirnodeFulfillment(uint256 timestamp) {
         require(
             msg.sender == address(airnodeProtocol),
             "Sender not Airnode protocol"
         );
+        require(timestamp + 1 hours > block.timestamp, "Fulfillment stale");
+        require(timestamp < block.timestamp, "Fulfillment from future");
         _;
     }
 
