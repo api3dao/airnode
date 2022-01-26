dab38a97042923dd6012aaade1ce87c45e767827
bbenligiray <bbenligiray@gmail.com>
2022-01-26 10:50:08 +0300

    Revert v0.4 contract changes

--- packages/airnode-protocol/contracts/utils/interfaces/IOwnableCallForwarder.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-interface IOwnableCallForwarder {
-    function forwardCall(
-        address forwardTarget,
-        bytes calldata forwardedCalldata
-    ) external payable returns (bytes memory returnedData);
-}
