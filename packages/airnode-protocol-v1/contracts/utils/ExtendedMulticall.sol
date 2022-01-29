e8ee8ca6cf5f5bdc3e9626ccd870e27a6eb5e64a
bbenligiray <bbenligiray@gmail.com>
2022-01-29 11:28:34 +0300

    Add functionality to ExtendedMulticall

--- packages/airnode-protocol-v1/contracts/utils/ExtendedMulticall.sol
+++ packages/airnode-protocol-v1/contracts/utils/ExtendedMulticall.sol
@@ -4,11 +4,23 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/utils/Multicall.sol";
 
 contract ExtendedMulticall is Multicall {
+    function getChainId() external view returns (uint256) {
+        return block.chainid;
+    }
+
     function getBalance(address account) external view returns (uint256) {
         return account.balance;
     }
 
+    function getCurrentBlockNumber() external view returns (uint256) {
+        return block.number;
+    }
+
     function getCurrentBlockTimestamp() external view returns (uint256) {
         return block.timestamp;
+    }
+
+    function getCurrentBasefee() external view returns (uint256) {
+        return block.basefee;
     }
 }
