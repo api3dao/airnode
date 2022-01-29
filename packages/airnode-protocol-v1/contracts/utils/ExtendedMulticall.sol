11799952925d23d7eede5de4bfd84568c78026b1
bbenligiray <bbenligiray@gmail.com>
2022-01-29 10:28:44 +0300

    Prettify

--- packages/airnode-protocol-v1/contracts/utils/ExtendedMulticall.sol
+++ packages/airnode-protocol-v1/contracts/utils/ExtendedMulticall.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/utils/Multicall.sol";
 
 contract ExtendedMulticall is Multicall {
-  function getBalance(address account) external view returns (uint256) {
+    function getBalance(address account) external view returns (uint256) {
         return account.balance;
     }
+
     function getCurrentBlockTimestamp() external view returns (uint256) {
         return block.timestamp;
     }
