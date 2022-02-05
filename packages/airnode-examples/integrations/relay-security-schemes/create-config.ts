40c655bd577652e56b3c0ca7a011e86d524d701f
Derek Croote <dcroote@users.noreply.github.com>
2022-02-04 18:34:40 -0800

    Updated _times reserved parameter as a precaution

--- packages/airnode-examples/integrations/relay-security-schemes/create-config.ts
+++ packages/airnode-examples/integrations/relay-security-schemes/create-config.ts
@@ -131,7 +131,7 @@ const createConfig = async (generateExampleFile: boolean): Promise<Config> => ({
             },
             {
               name: '_times',
-              fixed: '',
+              fixed: ',,,,',
             },
           ],
           parameters: [],
