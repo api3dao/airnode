fc5cda77ff3e99618c88845e97487a6ce6670adc
Michal Kimle <kimle.michal@gmail.com>
2022-02-08 13:23:23 +0100

    Add HTTP gateway `maxConcurrency` field to `config.json` examples and validator templates

--- packages/airnode-node/src/types.ts
+++ packages/airnode-node/src/types.ts
@@ -455,6 +455,7 @@ export interface ChainConfig {
 export interface HttpGateway {
   readonly enabled: boolean;
   readonly apiKey?: string;
+  readonly maxConcurrency?: number;
 }
 
 export interface Heartbeat {
