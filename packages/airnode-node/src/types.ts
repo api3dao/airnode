3c0b84d248765a4e62b389347e9e1ba8fcf9dbe9
Derek Croote <dcroote@users.noreply.github.com>
2022-02-14 22:58:33 -0800

    Remove Submitted variant of RequestStatus enum

--- packages/airnode-node/src/types.ts
+++ packages/airnode-node/src/types.ts
@@ -47,9 +47,6 @@ export enum RequestStatus {
   // Request was already processed by previous Airnode run (fulfilled or failed)
   // TODO: We should just have "Processed" status or just drop these immediately
   Fulfilled = 'Fulfilled',
-  // The fulfillment for this request was submitted on chain during the current Airnode run
-  // TODO: Not really needed for anything
-  Submitted = 'Submitted',
   // Request is not valid and should be ignored (e.g. sponsor and sponsorWallet do not match).
   // Any request after the ignored request is processed as if this request didn't exist at all
   // TODO: We should just drop these requests immediately
