d1f7ed6ae6ba0faef420d7f3359b78039fbe54a7
Emanuel Tesar <e.tesarr@gmail.com>
2022-02-17 16:15:39 +0100

    Rename example config.json and secrets.env files

--- packages/airnode-deployer/src/cli/__tests__/nodeVersion.test.ts
+++ packages/airnode-deployer/src/cli/__tests__/nodeVersion.test.ts
@@ -22,8 +22,8 @@ describe('deployer commands - invalid node version', () => {
   it.skip('fails deployment when the node version does not match the config version', async () => {
     await expect(
       deploy(
-        join(__dirname, '../../../config/config.json.example'),
-        join(__dirname, '../../../config/secrets.env.example'),
+        join(__dirname, '../../../config/config.example.json'),
+        join(__dirname, '../../../config/secrets.example.env'),
         'mocked receipt filename'
       )
     ).rejects.toThrow();
