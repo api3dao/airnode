0599418a5ac743baa2a04917cb59a043ab3828d5
vponline <65347007+vponline@users.noreply.github.com>
2022-02-24 20:15:15 +0200

    Update packages/airnode-admin/src/cli.ts
    Co-authored-by: Emanuel Tesař <e.tesarr@gmail.com>

--- packages/airnode-admin/src/cli.ts
+++ packages/airnode-admin/src/cli.ts
@@ -523,7 +523,7 @@ yargs
   .example([
     ['$0 derive-airnode-address --airnode-mnemonic "test test test test test test test test test test test junk"\n'],
     [
-      '$0 derive-sponsor-wallet-address --airnode-xpub xpub6Ce9NcJvTk36xtLSrJLZqE7wtgA5deCeYs7rSQtreh4cj6\nByPtrg9sD7V2FNFLPnf8heNP3FGkeV9qwfzvZNSd54JoNXVsXFYSYwHsnJxqP --airnode-address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --sponsor-address 0x70997970c51812dc3a010c7d01b50e0d17dc79c8\n',
+      '$0 derive-sponsor-wallet-address --airnode-xpub xpub6Ce9NcJvTk36xtLSrJLZqE7wtgA5deCeYs7rSQtreh4cj6ByPtrg9sD7V2FNFLPnf8heNP3FGkeV9qwfzvZNSd54JoNXVsXFYSYwHsnJxqP --airnode-address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --sponsor-address 0x70997970c51812dc3a010c7d01b50e0d17dc79c8\n',
     ],
     ['$0 derive-endpoint-id --ois-title "title" --endpoint-name "endpoint"\n'],
   ])
