b24ed27d9961fd2d95147b38c4a29bcad6ba2942
vponline <65347007+vponline@users.noreply.github.com>
2022-03-14 09:05:36 +0200

    Update packages/airnode-utilities/src/gas-prices/gas-prices.ts
    Co-authored-by: André Ogle <andreogle@users.noreply.github.com>

--- packages/airnode-utilities/src/gas-prices/gas-prices.ts
+++ packages/airnode-utilities/src/gas-prices/gas-prices.ts
@@ -6,7 +6,7 @@ import { LogsData } from '../logging';
 // The default amount of time before a "retryable" promise is timed out and retried
 export const DEFAULT_RETRY_TIMEOUT_MS = 5_000;
 // The Priority Fee in Wei
-export const PRIORITY_FEE = 3120000000;
+export const PRIORITY_FEE_IN_WEI = 3120000000;
 // The Base Fee to Max Fee multiplier
 export const BASE_FEE_MULTIPLIER = 2;
 
