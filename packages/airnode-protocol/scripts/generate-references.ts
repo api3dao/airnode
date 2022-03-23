7624cdde0d2fe794897e2b0b8d106236e74f8ebd
bbenligiray <bbenligiray@gmail.com>
2022-03-23 14:26:06 +0300

    Collect deployment addresses in a file to be imported in one go

---
+++ packages/airnode-protocol/scripts/generate-references.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { logger } from '@api3/airnode-utilities';
+const hre = require('hardhat');
+
+async function main() {
+  const networks = Object.keys(JSON.parse(fs.readFileSync('credentials.example.json', 'utf8')).networks);
+  const contractNames = ['AccessControlRegistry', 'RequesterAuthorizerWithAirnode', 'AirnodeRrp'];
+
+  const references: any = {};
+  for (const contractName of contractNames) {
+    references[contractName] = {};
+    for (const network of networks) {
+      const deployment = JSON.parse(fs.readFileSync(path.join('deployments', network, `${contractName}.json`), 'utf8'));
+      references[contractName]![hre.config.networks[network].chainId] = deployment.address;
+    }
+  }
+  fs.writeFileSync(path.join('deployments', 'references.json'), JSON.stringify(references, null, 2));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    logger.error(error);
+    process.exit(1);
+  });
