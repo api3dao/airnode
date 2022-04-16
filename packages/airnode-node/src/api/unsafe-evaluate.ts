44f31c70ce690e86c46f39604d0bd2bf46fb3f96
Emanuel Tesar <e.tesarr@gmail.com>
2022-04-16 15:34:54 +0200

    Use Function constructor instead of eval

--- packages/airnode-node/src/api/unsafe-evaluate.ts
+++ packages/airnode-node/src/api/unsafe-evaluate.ts
@@ -1,14 +1,16 @@
+import { ethers } from 'ethers';
+
 /**
  * This function is dangerous. Make sure to use it only with Trusted code.
  */
-export const unsafeEvaluate = (_input: any, code: string) => {
-  return eval(`
-    const ethers = require('ethers');
+export const unsafeEvaluate = (input: any, code: string) => {
+  return Function(`
+    "use strict";
+    const [ethers, input] = arguments
     const {BigNumber} = ethers;
-    const input = _input;
 
     ${code};
 
-    output;
-  `);
+    return output;
+  `)(ethers, input);
 };
