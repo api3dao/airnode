e7e071a78e528c160a3c46c8ff48ba54ddd59d6c
Derek Croote <dcroote@users.noreply.github.com>
2022-04-21 23:23:47 -0700

    Replace multiple ternaries

--- packages/airnode-examples/scripts/run-airnode-locally.ts
+++ packages/airnode-examples/scripts/run-airnode-locally.ts
@@ -13,11 +13,14 @@ const main = async () => {
   // If neither of the first two, use the version from package.json
   const specificImage = process.argv.slice(2);
   const packageVersion = readPackageVersion();
-  const imageName = process.env.CI
-    ? `api3/airnode-client-dev:${process.env.GITHUB_SHA}`
-    : specificImage.length === 1
-    ? specificImage
-    : `api3/airnode-client:${packageVersion}`;
+  let imageName: string;
+  if (process.env.CI) {
+    imageName = `api3/airnode-client-dev:${process.env.GITHUB_SHA}`;
+  } else if (specificImage.length === 1) {
+    imageName = specificImage[0];
+  } else {
+    imageName = `api3/airnode-client:${packageVersion}`;
+  }
 
   const integrationPath = join(__dirname, '../integrations', integrationInfo.integration);
   runShellCommand(`docker run --rm -v ${integrationPath}:/app/config --network="host" --name airnode ${imageName}`);
