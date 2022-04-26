5322eaa4dffba2db9eb13fd1abd2897497e24f78
vponline <vepant@gmail.com>
2022-04-26 10:00:29 +0300

    Remove airnode address from templateId derivation

--- packages/airnode-node/src/api/index.ts
+++ packages/airnode-node/src/api/index.ts
@@ -6,7 +6,7 @@ import { getMasterHDNode } from '../evm';
 import { getReservedParameters } from '../adapters/http/parameters';
 import { API_CALL_TIMEOUT, API_CALL_TOTAL_TIMEOUT } from '../constants';
 import { isValidSponsorWallet, isValidRequestId } from '../evm/verification';
-import { getExpectedTemplateId } from '../evm/templates';
+import { getExpectedTemplateIdV0, getExpectedTemplateIdV1 } from '../evm/templates';
 import { AggregatedApiCall, ApiCallResponse, LogsData, RequestErrorMessage, ApiCallErrorResponse } from '../types';
 import { Config } from '../config/types';
 
@@ -145,7 +145,11 @@ export function verifyTemplateId(payload: CallApiPayload): LogsData<ApiCallError
     ];
   }
 
-  const expectedTemplateId = getExpectedTemplateId(template);
+  const expectedTemplateId =
+    aggregatedApiCall.type === 'http-signed-data-gateway'
+      ? getExpectedTemplateIdV1(template)
+      : getExpectedTemplateIdV0(template);
+
   if (templateId === expectedTemplateId) return null;
 
   const message = `Invalid template ID:${templateId} found for Request:${id}. Expected template ID:${expectedTemplateId}`;
