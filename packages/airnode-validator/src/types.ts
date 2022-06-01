303e157b4064f68b46ef83dd22d8898ae48df06e
Michal Kimle <kimle.michal@gmail.com>
2022-06-01 09:14:12 +0200

    Add strict validation for OIS schema

--- packages/airnode-validator/src/types.ts
+++ packages/airnode-validator/src/types.ts
@@ -1,7 +1,5 @@
-import { RefinementCtx, z, ZodFirstPartySchemaTypes } from 'zod';
+import { z, ZodFirstPartySchemaTypes } from 'zod';
 
 export type SchemaType<Schema extends ZodFirstPartySchemaTypes> = z.infer<Schema>;
-
-export type ValidatorRefinement<T> = (arg: T, ctx: RefinementCtx) => void;
 
 export type Secrets = Record<string, string | undefined>;
