2ae9d677df9acc626ad9edaee9ce45b8fced48d3
Emanuel Tesar <e.tesarr@gmail.com>
2022-06-30 17:13:00 +0200

    Add tests

--- packages/airnode-deployer/src/cli/commands.test.ts
+++ packages/airnode-deployer/src/cli/commands.test.ts
@@ -19,15 +19,42 @@ jest.mock('../utils', () => ({
   writeReceiptFile: jest.fn(),
 }));
 
+const gcpReceipt: receipt.Receipt = {
+  airnodeWallet: {
+    airnodeAddress: '0xF347ADEd76F7AC2013e379078738aBfF75780C2e',
+    airnodeAddressShort: 'f347ade',
+    airnodeXpub:
+      'xpub6CZqcAR5RtRPYYGbJe7MzFGbJkJ86ub9KtYvTtenPQRgxFXNCmR7woXjV8SdFPWrTBooAqWVLKe19KWBnaktkwUsvSEfH18HyxeNZQRJq8k',
+  },
+  api: {},
+  deployment: {
+    airnodeAddressShort: 'f347ade',
+    cloudProvider: {
+      type: 'gcp',
+      region: 'us-east1',
+      projectId: 'airnode-4',
+      disableConcurrencyReservations: false,
+    },
+    nodeVersion: packageVersion,
+    stage: 'stage',
+    timestamp: new Date('23 March 2022 14:48 UTC').toISOString(),
+  },
+  success: true,
+};
+
 describe('deployer commands', () => {
   let mockDeployAirnode: jest.SpyInstance;
   let mockRemoveAirnode: jest.SpyInstance;
   let mockWriteReceiptFile: jest.SpyInstance;
+  let loggerFailSpy: jest.SpyInstance;
+  let loggerSucceedSpy: jest.SpyInstance;
 
   beforeEach(() => {
     mockDeployAirnode = jest.requireMock('../infrastructure').deployAirnode;
     mockRemoveAirnode = jest.requireMock('../infrastructure').removeAirnode;
     mockWriteReceiptFile = jest.requireMock('../utils').writeReceiptFile;
+    loggerFailSpy = jest.spyOn(logger, 'fail').mockImplementation(() => {});
+    loggerSucceedSpy = jest.spyOn(logger, 'succeed').mockImplementation(() => {});
   });
 
   it('can deploy Airnode', async () => {
@@ -55,92 +82,93 @@ describe('deployer commands', () => {
 
   it('can remove Airnode with receipt', async () => {
     const receiptFile = 'mockedReceiptFile';
-    const receipt: receipt.Receipt = {
-      airnodeWallet: {
-        airnodeAddress: '0xF347ADEd76F7AC2013e379078738aBfF75780C2e',
-        airnodeAddressShort: 'f347ade',
-        airnodeXpub:
-          'xpub6CZqcAR5RtRPYYGbJe7MzFGbJkJ86ub9KtYvTtenPQRgxFXNCmR7woXjV8SdFPWrTBooAqWVLKe19KWBnaktkwUsvSEfH18HyxeNZQRJq8k',
-      },
-      api: {},
-      deployment: {
-        airnodeAddressShort: 'f347ade',
-        cloudProvider: {
-          type: 'gcp',
-          region: 'us-east1',
-          projectId: 'airnode-4',
-          disableConcurrencyReservations: false,
-        },
-        nodeVersion: packageVersion,
-        stage: 'stage',
-        timestamp: new Date('23 March 2022 14:48 UTC').toISOString(),
-      },
-      success: true,
-    };
-    mockReadFileSync('mockedReceiptFile', JSON.stringify(receipt));
+    mockReadFileSync('mockedReceiptFile', JSON.stringify(gcpReceipt));
     await removeWithReceipt(receiptFile);
 
     expect(mockRemoveAirnode).toHaveBeenCalledTimes(1);
   });
 
-  it('writes receipt even when deployment fails', async () => {
+  it('writes receipt and removes Airnode when deployment fails', async () => {
     mockDeployAirnode.mockImplementation(() => {
       throw new Error('deployment failed');
     });
-    const failSpy = jest.spyOn(logger, 'fail').mockImplementation(() => {});
+    mockReadFileSync('mockedReceiptFile', JSON.stringify(gcpReceipt));
 
     await expect(() =>
       deploy(
         join(__dirname, '../../config/config.example.json'),
         join(__dirname, '../../config/secrets.example.env'),
-        'mocked receipt filename'
+        'mockedReceiptFile'
       )
-    ).rejects.toThrow('deployment failed');
+    ).rejects.toThrow(['Deployment error:', 'deployment failed'].join('\n'));
 
     expect(mockDeployAirnode).toHaveBeenCalledTimes(1);
+    expect(mockRemoveAirnode).toHaveBeenCalledTimes(1);
     expect(mockWriteReceiptFile).toHaveBeenCalledTimes(1);
-    expect(failSpy).toHaveBeenCalledTimes(1);
+    expect(loggerFailSpy).toHaveBeenCalledTimes(1);
+    expect(loggerSucceedSpy).toHaveBeenCalledTimes(1);
   });
-});
 
-describe('deployer commands fail with invalid node version', () => {
-  it('when using deploy', async () => {
-    const config = readExampleConfig();
-    (config.nodeSettings.nodeVersion as any) = '0.4.0';
-    mockReadFileSync('config.example.json', JSON.stringify(config));
+  it('prints error details when both deployment and removal fail', async () => {
+    mockDeployAirnode.mockImplementation(() => {
+      throw new Error('deployment failed');
+    });
+    mockRemoveAirnode.mockImplementation(() => {
+      throw new Error('removal failed');
+    });
+    mockReadFileSync('mockedReceiptFile', JSON.stringify(gcpReceipt));
 
-    const throwingFn = () =>
+    await expect(() =>
       deploy(
         join(__dirname, '../../config/config.example.json'),
         join(__dirname, '../../config/secrets.example.env'),
-        'mocked receipt filename'
-      );
+        'mockedReceiptFile'
+      )
+    ).rejects.toThrow(['Deployment error:', 'deployment failed', 'Removal error:', `removal failed`].join('\n'));
 
-    const issues = [
-      {
-        code: 'custom',
-        message: `The "nodeVersion" must be ${packageVersion}`,
-        path: ['nodeSettings', 'nodeVersion'],
-      },
-    ];
-    const expectedError = new Error(`Invalid Airnode configuration file: ${JSON.stringify(issues, null, 2)}`);
-    await expect(throwingFn).rejects.toThrow(expectedError);
+    expect(mockDeployAirnode).toHaveBeenCalledTimes(1);
+    expect(mockRemoveAirnode).toHaveBeenCalledTimes(1);
+    expect(mockWriteReceiptFile).toHaveBeenCalledTimes(1);
+    expect(loggerFailSpy).toHaveBeenCalledTimes(2);
   });
 
-  it('when using removeWithReceipt', async () => {
-    const failSpy = jest.spyOn(logger, 'fail').mockImplementation(() => {});
+  describe('fail with invalid node version', () => {
+    it('when using deploy', async () => {
+      const config = readExampleConfig();
+      (config.nodeSettings.nodeVersion as any) = '0.4.0';
+      mockReadFileSync('config.example.json', JSON.stringify(config));
 
-    const throwingFn = () => removeWithReceipt(join(__dirname, '../../test/fixtures/invalid-receipt.json'));
+      const throwingFn = () =>
+        deploy(
+          join(__dirname, '../../config/config.example.json'),
+          join(__dirname, '../../config/secrets.example.env'),
+          'mocked receipt filename'
+        );
 
-    const issues = [
-      {
-        code: 'custom',
-        message: `The "nodeVersion" must be ${packageVersion}`,
-        path: ['deployment', 'nodeVersion'],
-      },
-    ];
-    const expectedError = new Error(`Invalid Airnode receipt file: ${JSON.stringify(issues, null, 2)}`);
-    await expect(throwingFn).rejects.toThrow(expectedError);
-    expect(failSpy).toHaveBeenCalledTimes(1);
+      const issues = [
+        {
+          code: 'custom',
+          message: `The "nodeVersion" must be ${packageVersion}`,
+          path: ['nodeSettings', 'nodeVersion'],
+        },
+      ];
+      const expectedError = new Error(`Invalid Airnode configuration file: ${JSON.stringify(issues, null, 2)}`);
+      await expect(throwingFn).rejects.toThrow(expectedError);
+    });
+
+    it('when using removeWithReceipt', async () => {
+      const throwingFn = () => removeWithReceipt(join(__dirname, '../../test/fixtures/invalid-receipt.json'));
+
+      const issues = [
+        {
+          code: 'custom',
+          message: `The "nodeVersion" must be ${packageVersion}`,
+          path: ['deployment', 'nodeVersion'],
+        },
+      ];
+      const expectedError = new Error(`Invalid Airnode receipt file: ${JSON.stringify(issues, null, 2)}`);
+      await expect(throwingFn).rejects.toThrow(expectedError);
+      expect(loggerFailSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
