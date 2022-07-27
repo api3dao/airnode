7255f42e5055436c107fb5bfeaabe0e89be7b690
Emanuel Tesar <e.tesarr@gmail.com>
2022-07-27 15:22:17 +0200

    Fix bug where the local container would start with invalid config

--- packages/airnode-node/docker/entrypoint.sh
+++ packages/airnode-node/docker/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-node /app/src/cli/validate-config.js && node /app/src/workers/local-gateways/run-server.js & crond -f
+set -e
+
+node /app/src/cli/validate-config.js
+node /app/src/workers/local-gateways/run-server.js & crond -f
