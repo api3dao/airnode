7decb3461e4b57d184a40422a3474f785155d2ee
Michal Kimle <kimle.michal@gmail.com>
2022-08-23 12:32:23 +0200

    Remove Docker container building from examples

--- packages/airnode-examples/src/scripts/rebuild-deployer-container.ts
+++
@@ -1,7 +0,0 @@
-import { runAndHandleErrors, runShellCommand } from '../';
-
-const main = async () => {
-  runShellCommand(`yarn --cwd ../../../ docker:build:deployer`);
-};
-
-runAndHandleErrors(main);
