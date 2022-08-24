47ddc74ad1be3a7248732a685d9e847fee14d991
Emanuel Tesar <e.tesarr@gmail.com>
2022-08-24 20:21:35 +0200

    Use local git clone instead of copy, fix missing deps

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,9 +6,7 @@ GIT_REF=${GIT_REF:-master}
 
 # Use Git respository as a source when there's no `/airnode` directory mounted
 if [ -d /airnode ]; then
-  # Efficient way to copy the whole directory with a lot of small files
-  # Keeping any local code changes but excluding platform-specific files (dependencies, builds, etc.)
-  rsync -a --include packages/airnode-protocol/deployments --exclude node_modules --exclude build --exclude dist --exclude coverage --exclude artifacts --exclude cache --exclude .webpack --exclude deployments /airnode/ /build
+  git clone --depth 1 file:///airnode /build
 else
   git clone https://github.com/api3dao/airnode.git .
   git checkout ${GIT_REF}
