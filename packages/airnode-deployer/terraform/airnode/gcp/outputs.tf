a9fad59f48773d83800b0d806a83249c433dfd18
vponline <vepant@gmail.com>
2022-09-05 11:52:27 +0300

    Fix GCP gateway output url

--- packages/airnode-deployer/terraform/airnode/gcp/outputs.tf
+++ packages/airnode-deployer/terraform/airnode/gcp/outputs.tf
@@ -1,7 +1,7 @@
 output "http_gateway_url" {
-  value = var.http_path_key == null ? null : "${module.httpGw[0].api_url}/${var.http_path_key}"
+  value = var.http_path_key == null ? null : "https://${module.httpGw[0].api_url}/${var.http_path_key}"
 }
 
 output "http_signed_data_gateway_url" {
-  value = var.http_signed_data_path_key == null ? null : "${module.httpSignedGw[0].api_url}/${var.http_signed_data_path_key}"
+  value = var.http_signed_data_path_key == null ? null : "https://${module.httpSignedGw[0].api_url}/${var.http_signed_data_path_key}"
 }
