24034ea87938667f5c6eb712719b0ec9419024a4
Emanuel Tesar <e.tesarr@gmail.com>
2022-09-14 08:34:37 +0200

    Fix deployer CLI example

--- packages/airnode-deployer/src/cli/index.ts
+++ packages/airnode-deployer/src/cli/index.ts
@@ -46,7 +46,7 @@ async function runCommand(command: () => Promise<void>) {
 const cliExamples = [
   'deploy -c config/config.json -s config/secrets.env -r config/receipt.json',
   'remove-with-receipt -r config/receipt.json',
-  'remove-with-deployment-details --airnode-address-short abd9eaa --stage dev --cloud-provider aws --region us-east-1',
+  'remove-with-deployment-details --airnode-address abd9eaa --stage dev --cloud-provider aws --region us-east-1',
 ];
 
 drawHeader();
