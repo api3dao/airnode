ed41de4facdcacc3ff9e32c111c7c68ac16b597f
Michal Kimle <kimle.michal@gmail.com>
2022-09-20 14:16:04 +0200

    Remove unnecesary arguments for cloud connectors

--- packages/airnode-deployer/src/infrastructure/aws.test.ts
+++ packages/airnode-deployer/src/infrastructure/aws.test.ts
@@ -39,7 +39,6 @@ jest.mock('../utils/infrastructure', () => ({
   generateBucketName: jest.fn(),
 }));
 
-const awsConfigUpdateSpy = jest.requireMock('aws-sdk').config.update;
 const awsListBucketsSpy: jest.SpyInstance = jest.requireMock('aws-sdk').S3().listBuckets;
 const awsCreateBucketSpy: jest.SpyInstance = jest.requireMock('aws-sdk').S3().createBucket;
 const awsPutBucketEncryptionSpy: jest.SpyInstance = jest.requireMock('aws-sdk').S3().putBucketEncryption;
@@ -68,36 +67,30 @@ describe('getAirnodeBucket', () => {
   it('returns a name of Airnode S3 bucket', async () => {
     awsListBucketsSpy.mockImplementation(mockPromise(() => ({ Buckets: [{ Name: bucketName }] })));
 
-    const fetchedBucketName = await getAirnodeBucket(cloudProvider);
+    const fetchedBucketName = await getAirnodeBucket();
     expect(fetchedBucketName).toEqual(bucketName);
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`ignores incorrect Airnode S3 bucket names`, async () => {
     awsListBucketsSpy.mockImplementation(mockPromise(() => ({ Buckets: [{ Name: 'airnode-123456' }] })));
 
-    const fetchedBucketName = await getAirnodeBucket(cloudProvider);
+    const fetchedBucketName = await getAirnodeBucket();
     expect(fetchedBucketName).toBeNull();
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't fetch the list of S3 buckets`, async () => {
     awsListBucketsSpy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(getAirnodeBucket(cloudProvider)).rejects.toThrow(
-      new Error(`Failed to list S3 buckets: Error: ${s3ErrorMessage}`)
-    );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
+    await expect(getAirnodeBucket()).rejects.toThrow(new Error(`Failed to list S3 buckets: Error: ${s3ErrorMessage}`));
   });
 
   it(`throws an error if there are more then one Airnode S3 buckets`, async () => {
     const listBucketsResponse = { Buckets: [{ Name: bucketName }, { Name: 'airnode-eeff99887766' }] };
     awsListBucketsSpy.mockImplementation(mockPromise(() => listBucketsResponse));
 
-    await expect(getAirnodeBucket(cloudProvider)).rejects.toThrow(
+    await expect(getAirnodeBucket()).rejects.toThrow(
       new Error(`Multiple Airnode buckets found, stopping. Buckets: ${JSON.stringify(listBucketsResponse.Buckets)}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -128,7 +121,6 @@ describe('createAirnodeBucket', () => {
         RestrictPublicBuckets: true,
       },
     });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't create a bucket`, async () => {
@@ -137,7 +129,6 @@ describe('createAirnodeBucket', () => {
     await expect(createAirnodeBucket(cloudProvider)).rejects.toThrow(
       new Error(`Failed to create an S3 bucket: Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't set encryption`, async () => {
@@ -146,7 +137,6 @@ describe('createAirnodeBucket', () => {
     await expect(createAirnodeBucket(cloudProvider)).rejects.toThrow(
       new Error(`Failed to enable encryption for bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't set public access`, async () => {
@@ -155,7 +145,6 @@ describe('createAirnodeBucket', () => {
     await expect(createAirnodeBucket(cloudProvider)).rejects.toThrow(
       new Error(`Failed to setup a public access block for bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -168,19 +157,17 @@ describe('getBucketDirectoryStructure', () => {
       }))
     );
 
-    const directoryStructure = await getBucketDirectoryStructure(cloudProvider, bucketName);
+    const directoryStructure = await getBucketDirectoryStructure(bucketName);
     expect(directoryStructure).toEqual(mockBucketDirectoryStructure);
     expect(awsListObjectsV2Spy).toHaveBeenCalledWith({ Bucket: bucketName });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't list bucket content`, async () => {
     awsListObjectsV2Spy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(getBucketDirectoryStructure(cloudProvider, bucketName)).rejects.toThrow(
+    await expect(getBucketDirectoryStructure(bucketName)).rejects.toThrow(
       new Error(`Failed to list content of bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -192,22 +179,20 @@ describe('storeFileToBucket', () => {
   it('stores file in S3 bucket', async () => {
     awsPutObjectSpy.mockImplementation(mockPromise(() => {}));
 
-    await storeFileToBucket(cloudProvider, bucketName, bucketFilePath, filePath);
+    await storeFileToBucket(bucketName, bucketFilePath, filePath);
     expect(awsPutObjectSpy).toHaveBeenCalledWith({
       Bucket: bucketName,
       Key: bucketFilePath,
       Body: fileContent,
     });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throws an error if can't store file in bucket`, async () => {
     awsPutObjectSpy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(storeFileToBucket(cloudProvider, bucketName, bucketFilePath, filePath)).rejects.toThrow(
+    await expect(storeFileToBucket(bucketName, bucketFilePath, filePath)).rejects.toThrow(
       new Error(`Failed to store file '${filePath}' to S3 bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -215,19 +200,17 @@ describe('getFileFromBucket', () => {
   it('fetches file from S3 bucket', async () => {
     awsGetObjectSpy.mockImplementation(mockPromise(() => ({ Body: fileContent })));
 
-    const fetchedFileContent = await getFileFromBucket(cloudProvider, bucketName, bucketFilePath);
+    const fetchedFileContent = await getFileFromBucket(bucketName, bucketFilePath);
     expect(fetchedFileContent).toEqual(fileContent);
     expect(awsGetObjectSpy).toHaveBeenCalledWith({ Bucket: bucketName, Key: bucketFilePath });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throw an error if can't fetch file from bucket`, async () => {
     awsGetObjectSpy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(getFileFromBucket(cloudProvider, bucketName, bucketFilePath)).rejects.toThrow(
+    await expect(getFileFromBucket(bucketName, bucketFilePath)).rejects.toThrow(
       new Error(`Failed to fetch file '${bucketFilePath}' from S3 bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -237,24 +220,22 @@ describe('copyFileInBucket', () => {
   it('copies file within a bucket', async () => {
     awsCopyObjectSpy.mockImplementation(mockPromise(() => {}));
 
-    await copyFileInBucket(cloudProvider, bucketName, bucketFilePath, toBucketFilePath);
+    await copyFileInBucket(bucketName, bucketFilePath, toBucketFilePath);
     expect(awsCopyObjectSpy).toHaveBeenCalledWith({
       Bucket: bucketName,
       CopySource: `/${bucketName}/${bucketFilePath}`,
       Key: toBucketFilePath,
     });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throw an error if can't copy file within bucket`, async () => {
     awsCopyObjectSpy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(copyFileInBucket(cloudProvider, bucketName, bucketFilePath, toBucketFilePath)).rejects.toThrow(
+    await expect(copyFileInBucket(bucketName, bucketFilePath, toBucketFilePath)).rejects.toThrow(
       new Error(
         `Failed to copy file '${bucketFilePath}' to file '${toBucketFilePath}' within S3 bucket '${bucketName}': Error: ${s3ErrorMessage}`
       )
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -266,7 +247,6 @@ describe('deleteObjects', () => {
     awsDeleteObjectsSpy.mockImplementation(mockPromise(() => {}));
 
     await deleteBucketDirectory(
-      cloudProvider,
       bucketName,
       mockBucketDirectoryStructure['0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace'] as Directory
     );
@@ -274,7 +254,6 @@ describe('deleteObjects', () => {
       Bucket: bucketName,
       Delete: { Objects: bucketKeys.map((bucketKey) => ({ Key: bucketKey })) },
     });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throw an error if can't delete files from bucket`, async () => {
@@ -282,7 +261,6 @@ describe('deleteObjects', () => {
 
     await expect(
       deleteBucketDirectory(
-        cloudProvider,
         bucketName,
         mockBucketDirectoryStructure['0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace'] as Directory
       )
@@ -291,7 +269,6 @@ describe('deleteObjects', () => {
         `Failed to delete bucket directory '${mockBucketDirectoryStructure['0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace'].bucketKey}' and its content: Error: ${s3ErrorMessage}`
       )
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
 
@@ -299,17 +276,15 @@ describe('deleteBucket', () => {
   it('deletes an empty S3 bucket', async () => {
     awsDeleteBucketSpy.mockImplementation(mockPromise(() => {}));
 
-    await deleteBucket(cloudProvider, bucketName);
+    await deleteBucket(bucketName);
     expect(awsDeleteBucketSpy).toHaveBeenCalledWith({ Bucket: bucketName });
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 
   it(`throw an error if can't delete files from bucket`, async () => {
     awsDeleteBucketSpy.mockImplementation(mockPromise(jest.fn().mockRejectedValue(s3Error)));
 
-    await expect(deleteBucket(cloudProvider, bucketName)).rejects.toThrow(
+    await expect(deleteBucket(bucketName)).rejects.toThrow(
       new Error(`Failed to delete S3 bucket '${bucketName}': Error: ${s3ErrorMessage}`)
     );
-    expect(awsConfigUpdateSpy).toHaveBeenCalledWith({ region: cloudProvider.region });
   });
 });
