a4d014da9f7646984c6e76a80442884584233445
Michal Kimle <kimle.michal@gmail.com>
2022-09-21 15:20:43 +0200

    Add `list` command to deployer

--- packages/airnode-deployer/src/utils/cli.ts
+++ packages/airnode-deployer/src/utils/cli.ts
@@ -1,3 +1,6 @@
+import * as crypto from 'crypto';
+import { format } from 'date-fns-tz';
+import { CloudProvider } from '@api3/airnode-node';
 import join from 'lodash/join';
 import omitBy from 'lodash/omitBy';
 
@@ -10,4 +13,30 @@ export function printableArguments(args: string[]) {
     args.map((arg) => `--${arg}`),
     ', '
   );
+}
+
+export function hashDeployment(
+  cloudProvider: CloudProvider['type'],
+  region: string,
+  airnodeAddress: string,
+  stage: string,
+  airnodeVersion: string
+) {
+  return crypto
+    .createHash('sha256')
+    .update([cloudProvider, region, airnodeAddress, stage, airnodeVersion].join(''))
+    .digest('hex')
+    .substring(0, 8);
+}
+
+export function cloudProviderReadable(cloudProvider: CloudProvider['type'], region: string) {
+  return `${cloudProvider.toUpperCase()} (${region})`;
+}
+
+export function airnodeAddressReadable(airnodeAddress: string) {
+  return `${airnodeAddress.slice(0, 8)}...${airnodeAddress.slice(-6)}`;
+}
+
+export function lastUpdateReadable(deploymentTimestamp: string) {
+  return format(parseInt(deploymentTimestamp), 'yyyy-MM-dd HH:mm:ss zzz');
 }
