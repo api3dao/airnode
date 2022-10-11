b5b2d04faf84755c3a6f3615b2bc6560bcc80d53
Derek Croote <dcroote@users.noreply.github.com>
2022-10-10 17:57:25 -0700

    Lint authorizer contracts

--- packages/airnode-examples/contracts/coingecko-e2e/EverythingAuthorizer.sol
+++ packages/airnode-examples/contracts/coingecko-e2e/EverythingAuthorizer.sol
@@ -1,20 +1,16 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
 import "@api3/airnode-protocol/contracts/authorizers/interfaces/IAuthorizerV0.sol";
 
-/* solhint-disable no-unused-vars */
-// prettier-ignore
-contract EverythingAuthorizer is IAuthorizerV0
-{
-  function isAuthorizedV0(
-    bytes32 requestId,
-    address airnode,
-    bytes32 endpointId,
-    address sponsor,
-    address requester
-  ) external pure override returns (bool) {
-    return true;
-  }
+contract EverythingAuthorizer is IAuthorizerV0 {
+    function isAuthorizedV0(
+        bytes32,
+        address,
+        bytes32,
+        address,
+        address
+    ) external pure override returns (bool) {
+        return true;
+    }
 }
-/* solhint-enable no-unused-vars */
