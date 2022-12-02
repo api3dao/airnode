1271ea5cfb3c9293ca14be41f28b603b8f9b846e
metobom <unutkan2013@yandex.com>
2022-12-02 18:06:38 +0300

    update ois from 1.2.0 to 1.3.0

--- packages/airnode-examples/integrations/weather-multi-value/create-config.ts
+++ packages/airnode-examples/integrations/weather-multi-value/create-config.ts
@@ -85,7 +85,7 @@ const createConfig = async (generateExampleFile: boolean): Promise<Config> => ({
   templates: [],
   ois: [
     {
-      oisFormat: '1.2.0',
+      oisFormat: '1.3.0',
       title: 'OpenWeather Multiple Encoded Values',
       version: '1.0.0',
       apiSpecifications: {
