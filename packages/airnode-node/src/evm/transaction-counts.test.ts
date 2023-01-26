5860999abefc08984be9969a6c44ab93bd21b0d5
Derek Croote <dcroote@users.noreply.github.com>
2023-01-25 22:31:25 -0800

    Add transaction counts unit test

--- packages/airnode-node/src/evm/transaction-counts.test.ts
+++ packages/airnode-node/src/evm/transaction-counts.test.ts
@@ -55,6 +55,24 @@ describe('fetchBySponsor', () => {
     );
   });
 
+  it('fetches up to current block when minConfirmations may be overridden', async () => {
+    getTransactionCountMock.mockResolvedValueOnce(5);
+    const currentBlock = 10716084;
+    const options = {
+      currentBlock: currentBlock,
+      masterHDNode: wallet.getMasterHDNode(config),
+      provider: new ethers.providers.JsonRpcProvider(),
+      minConfirmations: 2,
+      mayOverrideMinConfirmations: true,
+    };
+    const addresses = ['0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181', '0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181'];
+    const [logs, res] = await transactions.fetchBySponsor(addresses, options);
+    expect(logs).toEqual([]);
+    expect(res).toEqual({ '0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181': 5 });
+    expect(getTransactionCountMock).toHaveBeenCalledTimes(1);
+    expect(getTransactionCountMock).toHaveBeenCalledWith('0xdBFe14C250643DEFE92C9AbC52103bf4978C7113', currentBlock);
+  });
+
   it('returns transaction counts for multiple wallets', async () => {
     getTransactionCountMock.mockResolvedValueOnce(45);
     getTransactionCountMock.mockResolvedValueOnce(123);
