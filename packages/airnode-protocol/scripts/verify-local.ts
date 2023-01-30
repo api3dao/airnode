787dfa9f7af0d04054f915ba33f848b6262b5f7d
Ashar2shahid <Ashar2shahid@gmail.com>
2023-01-30 01:57:12 -0500

    update verify script, add workflow

--- packages/airnode-protocol/scripts/verify-local.ts
+++ packages/airnode-protocol/scripts/verify-local.ts
@@ -4,19 +4,27 @@
 // that fetches the deployed bytecode from a chain and compares that with the output
 // of the local compilation.
 
+import * as path from 'path';
+import * as fs from 'fs';
 import { assert } from 'console';
 import { logger } from '@api3/airnode-utilities';
+import { go } from '@api3/promise-utils';
 import { contractNames } from './contract-names';
 const hre = require('hardhat');
 
-const verifyDeployedBytecode = (deployedBytecode: string, generatedBytecode: string, contractName: string) => {
+const verifyDeployedBytecode = (
+  deployedBytecode: string,
+  generatedBytecode: string,
+  contractName: string,
+  networkName: string
+) => {
   if (deployedBytecode === generatedBytecode) {
     return true;
   }
   // The compiler appends by default the IPFS hash of the metadata file to the end of the bytecode (the last 53 bytes/106 char)
   // for reference: https://docs.soliditylang.org/en/v0.8.17/metadata.html#encoding-of-the-metadata-hash-in-the-bytecode
   else if (deployedBytecode.slice(0, -106) === generatedBytecode.slice(0, -106)) {
-    logger.log(`⚠️  ${contractName} metadata is different from onchain value`);
+    logger.log(`⚠️ ${contractName} metadata is different from onchain value on ${networkName}!`);
     return true;
   } else {
     return false;
@@ -24,53 +32,106 @@ const verifyDeployedBytecode = (deployedBytecode: string, generatedBytecode: str
 };
 
 async function main() {
-  for (const contractName of contractNames) {
-    const deployment = await hre.deployments.get(contractName);
-    const artifact = await hre.deployments.getArtifact(contractName);
-    const constructor = artifact.abi.find((method: any) => method.type === 'constructor');
-    let generatedBytecode = artifact.bytecode;
-    if (constructor) {
-      // If a constructor is defined, encode and add the constructor arguments to the contract bytecode
-      const constructorArgumentTypes = constructor.inputs.map((input: any) => input.type);
-      const encodedConstructorArguments = hre.ethers.utils.defaultAbiCoder.encode(
-        constructorArgumentTypes,
-        deployment.args
-      );
-      generatedBytecode = hre.ethers.utils.solidityPack(
-        ['bytes', 'bytes'],
-        [artifact.bytecode, encodedConstructorArguments]
-      );
-    }
+  const networkNames = fs
+    .readdirSync(path.join('deployments'), { withFileTypes: true })
+    .filter((item) => item.isDirectory())
+    .map((item) => item.name);
 
-    const creationTx = await hre.ethers.provider.getTransaction(deployment.transactionHash);
-    const creationData = creationTx.data;
+  const deployments: any = {};
 
-    if (creationTx.creates) {
-      // creationTx.creates is defined if the deployment is undeterministic
-      // Verify that the creation tx hash belongs to the address
-      assert(creationTx.creates === deployment.address);
-      if (!verifyDeployedBytecode(creationData, generatedBytecode, contractName)) {
-        throw new Error(
-          `${contractName} undeterministic deployment on ${hre.network.name} DOES NOT match the local build!`
+  const verificationPromises = await Promise.all(
+    networkNames.flatMap((network) => {
+      deployments[network] = fs
+        .readdirSync(path.join('deployments', network), { withFileTypes: true })
+        .filter((item) => item.isFile() && item.name.endsWith('.json'))
+        .reduce(
+          (acc, item) => ({
+            ...acc,
+            [item.name.slice(0, -5)]: JSON.parse(fs.readFileSync(path.join('deployments', network, item.name), 'utf8')),
+          }),
+          {}
         );
-      }
-      logger.log(`✅  ${contractName} undeterministic deployment on ${hre.network.name} matches the local build!`);
-    } else {
-      const salt = hre.ethers.constants.HashZero;
-      const deterministicDeploymentAddress = hre.ethers.utils.getCreate2Address(
-        '0x4e59b44847b379578588920ca78fbf26c0b4956c', // default create2 factory address in hardhat
-        salt,
-        hre.ethers.utils.keccak256(artifact.bytecode)
-      );
-      assert(deterministicDeploymentAddress === deployment.address);
-      generatedBytecode = hre.ethers.utils.hexConcat([salt, generatedBytecode]);
-      if (!verifyDeployedBytecode(creationData, generatedBytecode, contractName)) {
-        throw new Error(
-          `${contractName} deterministic deployment on ${hre.network.name} DOES NOT match the local build!`
-        );
-      }
-      logger.log(`✅  ${contractName} deterministic deployment on ${hre.network.name} matches the local build!`);
-    }
+
+      return contractNames.map(async (contractName) => {
+        const deployment = deployments[network][contractName];
+        const artifact = await hre.deployments.getArtifact(contractName);
+        const constructor = artifact.abi.find((method: any) => method.type === 'constructor');
+        let generatedBytecode = artifact.bytecode;
+        if (constructor) {
+          // If a constructor is defined, encode and add the constructor arguments to the contract bytecode
+          const constructorArgumentTypes = constructor.inputs.map((input: any) => input.type);
+          const encodedConstructorArguments = hre.ethers.utils.defaultAbiCoder.encode(
+            constructorArgumentTypes,
+            deployment.args
+          );
+          generatedBytecode = hre.ethers.utils.solidityPack(
+            ['bytes', 'bytes'],
+            [artifact.bytecode, encodedConstructorArguments]
+          );
+        }
+
+        const provider = new hre.ethers.providers.JsonRpcProvider(hre.config.networks[network].url);
+        const goFetchCreationTx = await go(() => provider.getTransaction(deployment.transactionHash), {
+          retries: 10,
+          attemptTimeoutMs: 5000,
+          totalTimeoutMs: 50000,
+          delay: {
+            type: 'random',
+            minDelayMs: 2000,
+            maxDelayMs: 5000,
+          },
+        });
+        if (!goFetchCreationTx.success || !goFetchCreationTx.data) {
+          return {
+            status: false,
+            data: `❗ Could not fetch creation tx for ${contractName} on ${network}!`,
+          };
+        }
+        const creationTx: any = goFetchCreationTx.data;
+        const creationData = creationTx.data;
+
+        if (creationTx.creates) {
+          // creationTx.creates is defined if the deployment is undeterministic
+          // Verify that the creation tx hash belongs to the address
+          assert(creationTx.creates === deployment.address);
+          if (!verifyDeployedBytecode(creationData, generatedBytecode, contractName, network)) {
+            return {
+              success: false,
+              data: `❗ ${contractName} undeterministic deployment on ${network} DOES NOT match the local build!`,
+            };
+          }
+          return {
+            success: true,
+            data: `✅  ${contractName} undeterministic deployment on ${network} matches the local build!`,
+          };
+        } else {
+          const salt = hre.ethers.constants.HashZero;
+          const deterministicDeploymentAddress = hre.ethers.utils.getCreate2Address(
+            '0x4e59b44847b379578588920ca78fbf26c0b4956c', // default create2 factory address in hardhat
+            salt,
+            hre.ethers.utils.keccak256(generatedBytecode)
+          );
+          generatedBytecode = hre.ethers.utils.hexConcat([salt, generatedBytecode]);
+          assert(deterministicDeploymentAddress === deployment.address);
+          if (!verifyDeployedBytecode(creationData, generatedBytecode, contractName, network)) {
+            return {
+              success: false,
+              data: `❗ ${contractName} deterministic deployment on ${network} DOES NOT match the local build!`,
+            };
+          }
+          return {
+            success: true,
+            data: `✅  ${contractName} deterministic deployment on ${network} matches the local build!`,
+          };
+        }
+      });
+    })
+  );
+
+  verificationPromises.forEach((verificationPromise) => logger.log(verificationPromise.data));
+
+  if (verificationPromises.some((verificationPromise) => !verificationPromise.success)) {
+    throw new Error('❗ Some deployments do not match the local build!');
   }
 }
 
