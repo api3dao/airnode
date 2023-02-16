d65e38f58ffb30b5d1dee83fd6a3a2bbb95b07cd
Derek Croote <dcroote@users.noreply.github.com>
2023-02-15 20:08:06 -0800

    Update dependencies: @api3/ois@2.0.0, zod@^3.20.6
    @api3/ois
      * @api3/airnode-adapter: 1.4.0 → 2.0.0
      * @api3/airnode-node: 1.4.0 → 2.0.0
      * @api3/airnode-validator: 1.4.0 → 2.0.0
    
    zod
      * @api3/airnode-deployer: ^3.20.0 → ^3.20.6
      * @api3/airnode-node: ^3.20.0 → ^3.20.6
      * @api3/airnode-validator: ^3.20.0 → ^3.20.6
    
    zod bump necessary for @api3/ois@2.0.0 compatibility

--- packages/airnode-examples/integrations/coingecko-post-processing/create-config.ts
+++ packages/airnode-examples/integrations/coingecko-post-processing/create-config.ts
@@ -85,7 +85,7 @@ const createConfig = async (generateExampleFile: boolean): Promise<Config> => ({
   templates: [],
   ois: [
     {
-      oisFormat: '1.4.0',
+      oisFormat: '2.0.0',
       title: 'CoinGecko coins markets request',
       version: '1.0.0',
       apiSpecifications: {
