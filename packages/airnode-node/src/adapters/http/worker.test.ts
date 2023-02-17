6bfe44430684c7e0dff1253dfcb37fb38de0ffe2
Michal Kimle <kimle.michal@gmail.com>
2023-02-17 12:56:52 +0100

    Update AWS SDK for airnode-node

--- packages/airnode-node/src/adapters/http/worker.test.ts
+++ packages/airnode-node/src/adapters/http/worker.test.ts
@@ -1,13 +1,12 @@
-const invokeMock = jest.fn();
-jest.mock('aws-sdk', () => ({
-  Lambda: jest.fn().mockImplementation(() => ({
-    invoke: invokeMock,
-  })),
-}));
-
+import { mockClient } from 'aws-sdk-client-mock';
+import 'aws-sdk-client-mock-jest';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import { logger, LogOptions, setLogOptions } from '@api3/airnode-utilities';
 import * as worker from './worker';
 import * as fixtures from '../../../test/fixtures';
+import { encodeUtf8 } from '../../workers/cloud-platforms/aws';
+
+const mockLambdaClient = mockClient(LambdaClient);
 
 describe('spawnNewApiCall', () => {
   const logOptions: LogOptions = {
@@ -17,30 +16,33 @@ describe('spawnNewApiCall', () => {
   };
   setLogOptions(logOptions);
 
+  const aggregatedApiCall = fixtures.buildAggregatedRegularApiCall();
+  const mockInvokeCommandInput = {
+    FunctionName: 'airnode-local02cce763-run',
+    Payload: encodeUtf8(JSON.stringify({ aggregatedApiCall, logOptions, functionName: 'callApi' })),
+  };
+
+  beforeEach(() => {
+    mockLambdaClient.reset();
+  });
+
   it('handles remote AWS calls', async () => {
-    invokeMock.mockImplementationOnce((_params, callback) =>
-      callback(null, { Payload: JSON.stringify({ body: JSON.stringify({ ok: true, data: { value: '0x123' } }) }) })
-    );
-    const aggregatedApiCall = fixtures.buildAggregatedRegularApiCall();
+    mockLambdaClient.on(InvokeCommand, mockInvokeCommandInput).resolves({
+      Payload: encodeUtf8(JSON.stringify({ body: JSON.stringify({ ok: true, data: { value: '0x123' } }) })),
+    });
+
     const workerOpts = fixtures.buildWorkerOptions({
       cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
     });
     const [logs, res] = await worker.spawnNewApiCall(aggregatedApiCall, workerOpts);
     expect(logs).toEqual([]);
     expect(res).toEqual({ value: '0x123' });
-    expect(invokeMock).toHaveBeenCalledTimes(1);
-    expect(invokeMock).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ aggregatedApiCall, logOptions, functionName: 'callApi' }),
-      },
-      expect.anything()
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 
   it('returns an error if the worker rejects', async () => {
-    invokeMock.mockImplementationOnce((_params, callback) => callback(new Error('Something went wrong'), null));
-    const aggregatedApiCall = fixtures.buildAggregatedRegularApiCall();
+    mockLambdaClient.on(InvokeCommand, mockInvokeCommandInput).rejects(new Error('Something went wrong'));
+
     const workerOpts = fixtures.buildWorkerOptions({
       cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
     });
@@ -49,42 +51,29 @@ describe('spawnNewApiCall', () => {
       { level: 'ERROR', message: 'Unable to call API endpoint:convertToUSD', error: new Error('Something went wrong') },
     ]);
     expect(res).toEqual(null);
-    expect(invokeMock).toHaveBeenCalledTimes(1);
-    expect(invokeMock).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ aggregatedApiCall, logOptions, functionName: 'callApi' }),
-      },
-      expect.anything()
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 
   it('returns an error if the response has an error log', async () => {
     const errorLog = logger.pend('ERROR', 'Something went wrong');
-    invokeMock.mockImplementationOnce((_params, callback) =>
-      callback(null, { Payload: JSON.stringify({ body: JSON.stringify({ ok: false, errorLog }) }) })
-    );
-    const aggregatedApiCall = fixtures.buildAggregatedRegularApiCall();
+    mockLambdaClient.on(InvokeCommand, mockInvokeCommandInput).resolves({
+      Payload: encodeUtf8(JSON.stringify({ body: JSON.stringify({ ok: false, errorLog }) })),
+    });
+
     const workerOpts = fixtures.buildWorkerOptions({
       cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
     });
     const [logs, res] = await worker.spawnNewApiCall(aggregatedApiCall, workerOpts);
     expect(logs).toEqual([errorLog]);
     expect(res).toEqual(null);
-    expect(invokeMock).toHaveBeenCalledTimes(1);
-    expect(invokeMock).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ aggregatedApiCall, logOptions, functionName: 'callApi' }),
-      },
-      expect.anything()
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 
   it('returns an error if the response is not ok', async () => {
-    invokeMock.mockImplementationOnce((_params, callback) =>
-      callback(null, { Payload: JSON.stringify({ body: JSON.stringify({ ok: false }) }) })
-    );
+    mockLambdaClient
+      .on(InvokeCommand, mockInvokeCommandInput)
+      .resolves({ Payload: encodeUtf8(JSON.stringify({ body: JSON.stringify({ ok: false }) })) });
+
     const aggregatedApiCall = fixtures.buildAggregatedRegularApiCall();
     const workerOpts = fixtures.buildWorkerOptions({
       cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
@@ -92,13 +81,6 @@ describe('spawnNewApiCall', () => {
     const [logs, res] = await worker.spawnNewApiCall(aggregatedApiCall, workerOpts);
     expect(logs).toEqual([{ level: 'ERROR', message: 'Unable to call API endpoint:convertToUSD' }]);
     expect(res).toEqual(null);
-    expect(invokeMock).toHaveBeenCalledTimes(1);
-    expect(invokeMock).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ aggregatedApiCall, logOptions, functionName: 'callApi' }),
-      },
-      expect.anything()
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 });
--- packages/airnode-node/src/workers/cloud-platforms/aws.test.ts
+++ packages/airnode-node/src/workers/cloud-platforms/aws.test.ts
@@ -1,66 +1,62 @@
-const invokeMock = jest.fn();
-jest.mock('aws-sdk', () => ({
-  Lambda: jest.fn().mockImplementation(() => ({
-    invoke: invokeMock,
-  })),
-}));
-
-import AWS from 'aws-sdk';
+import { mockClient } from 'aws-sdk-client-mock';
+import 'aws-sdk-client-mock-jest';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import * as aws from './aws';
 import * as fixtures from '../../../test/fixtures';
 import { WorkerParameters } from '../../types';
 
+const mockLambdaClient = mockClient(LambdaClient);
+
+const encodingTestString = 'test string';
+const encodingTestByteArray = new Uint8Array([116, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103]);
+
+describe('encodeUtf8', () => {
+  it('converts input string into a byte stream with UTF-8 encoding', () => {
+    expect(aws.encodeUtf8(encodingTestString)).toEqual(encodingTestByteArray);
+  });
+});
+
+describe('decodeUtf8', () => {
+  it('converts a byte stream with UTF-8 encoding into a string', () => {
+    expect(aws.decodeUtf8(encodingTestByteArray)).toEqual(encodingTestString);
+  });
+});
+
 describe('spawn', () => {
   fixtures.setEnvVariables({ AIRNODE_WALLET_PRIVATE_KEY: fixtures.getAirnodeWalletPrivateKey() });
 
+  const state = fixtures.buildEVMProviderState();
+  const workerOpts = fixtures.buildWorkerOptions({
+    cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
+  });
+  const logOptions = fixtures.buildLogOptions();
+  const parameters: WorkerParameters = {
+    ...workerOpts,
+    payload: { functionName: 'initializeProvider', state, logOptions },
+  };
+  const mockInvokeCommandInput = {
+    FunctionName: 'airnode-local02cce763-run',
+    Payload: aws.encodeUtf8(JSON.stringify({ functionName: 'initializeProvider', state, logOptions })),
+  };
+
+  beforeEach(() => {
+    mockLambdaClient.reset();
+  });
+
   it('derives the function name, invokes and returns the response', async () => {
-    const lambda = new AWS.Lambda();
-    const invoke = lambda.invoke as jest.Mock;
-    invoke.mockImplementationOnce((_params, callback) =>
-      callback(null, { Payload: JSON.stringify({ body: JSON.stringify({ value: 7777 }) }) })
-    );
-    const state = fixtures.buildEVMProviderState();
-    const workerOpts = fixtures.buildWorkerOptions({
-      cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
-    });
-    const logOptions = fixtures.buildLogOptions();
-    const parameters: WorkerParameters = {
-      ...workerOpts,
-      payload: { functionName: 'initializeProvider', state, logOptions },
-    };
+    mockLambdaClient
+      .on(InvokeCommand, mockInvokeCommandInput)
+      .resolves({ Payload: aws.encodeUtf8(JSON.stringify({ body: JSON.stringify({ value: 7777 }) })) });
+
     const res = await aws.spawn(parameters);
     expect(res).toEqual({ value: 7777 });
-    expect(invoke).toHaveBeenCalledTimes(1);
-    expect(invoke).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ functionName: 'initializeProvider', state, logOptions }),
-      },
-      expect.any(Function)
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 
   it('throws an error if the lambda returns an error', async () => {
-    const lambda = new AWS.Lambda();
-    const invoke = lambda.invoke as jest.Mock;
-    invoke.mockImplementationOnce((_params, callback) => callback(new Error('Something went wrong'), null));
-    const state = fixtures.buildEVMProviderState();
-    const workerOpts = fixtures.buildWorkerOptions({
-      cloudProvider: { type: 'aws', region: 'us-east-1', disableConcurrencyReservations: false },
-    });
-    const logOptions = fixtures.buildLogOptions();
-    const parameters: WorkerParameters = {
-      ...workerOpts,
-      payload: { functionName: 'initializeProvider', state, logOptions },
-    };
+    mockLambdaClient.on(InvokeCommand, mockInvokeCommandInput).rejects(new Error('Something went wrong'));
+
     await expect(aws.spawn(parameters)).rejects.toThrow(new Error('Something went wrong'));
-    expect(invoke).toHaveBeenCalledTimes(1);
-    expect(invoke).toHaveBeenCalledWith(
-      {
-        FunctionName: 'airnode-local02cce763-run',
-        Payload: JSON.stringify({ functionName: 'initializeProvider', state, logOptions }),
-      },
-      expect.any(Function)
-    );
+    expect(mockLambdaClient).toHaveReceivedCommandWith(InvokeCommand, mockInvokeCommandInput);
   });
 });
