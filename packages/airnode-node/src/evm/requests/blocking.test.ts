f73b3649f9f397b9c4c3f970e12975aaa206114a
alikonuk1 <alikonuk1@gmail.com>
2023-05-15 12:34:06 +0300

    add back blockRequestsWithWithdrawals

--- packages/airnode-node/src/evm/requests/blocking.test.ts
+++ packages/airnode-node/src/evm/requests/blocking.test.ts
@@ -6,35 +6,41 @@ import { GroupedRequests } from '../../types';
 const buildApiCallsWithSponsor = (count: number, sponsorAddress: string) =>
   range(count).map((i) => fixtures.requests.buildApiCall({ sponsorAddress, id: `id-${sponsorAddress}-${i}` }));
 
-describe('blockRequests', () => {
-  it('does not block any requests', () => {
-    const sponsorAddresses = [
-      '0xab296574a9FB30d11d06B3b50cFd2a85E4b203b6',
-      '0x719BFe83fc029420B6eDd4e0D3F4E1000E5ce0f9',
-      '0x26B3dA8C6B45b1e917a7670C8E091D032f31538E',
-    ];
-    const apiCalls = [
-      ...buildApiCallsWithSponsor(7, sponsorAddresses[0]),
-      ...buildApiCallsWithSponsor(6, sponsorAddresses[1]),
-      ...buildApiCallsWithSponsor(4, sponsorAddresses[2]),
-    ];
-    const withdrawals = [
-      fixtures.requests.buildWithdrawal({
-        sponsorAddress: sponsorAddresses[1],
-      }),
-    ];
+describe('blockRequestsWithWithdrawals', () => {
+  it('drops API calls with pending withdrawals from the same sponsor', () => {
+    const apiCall = buildApiCallsWithSponsor(1, '0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181')[0];
+    const withdrawal = fixtures.requests.buildWithdrawal({
+      sponsorAddress: '0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181',
+    });
     const requests: GroupedRequests = {
-      apiCalls: apiCalls,
-      withdrawals: withdrawals,
+      apiCalls: [apiCall],
+      withdrawals: [withdrawal],
     };
-
-    const [logs, res] = blocking.blockRequests(requests);
-
-    expect(logs).toEqual([]);
-    expect(res.apiCalls.length).toEqual(17);
-    expect(res.apiCalls.filter((apiCall) => apiCall.sponsorAddress === sponsorAddresses[0])).toHaveLength(7);
-    expect(res.apiCalls.filter((apiCall) => apiCall.sponsorAddress === sponsorAddresses[1])).toHaveLength(6);
-    expect(res.apiCalls.filter((apiCall) => apiCall.sponsorAddress === sponsorAddresses[2])).toHaveLength(4);
+    const [logs, res] = blocking.blockRequestsWithWithdrawals([[], requests]);
+    expect(logs).toEqual([
+      {
+        level: 'WARN',
+        message: `Dropping Request ID:${apiCall.id} as it has a pending Withdrawal ID:${withdrawal.id}`,
+      },
+    ]);
+    expect(res.apiCalls.length).toEqual(0);
     expect(res.withdrawals.length).toEqual(1);
+  });
+
+  it('does nothing if API call and withdrawal wallet indices do not match', () => {
+    const apiCall = buildApiCallsWithSponsor(1, '0x69e2B095fbAc6C3f9E528Ef21882b86BF1595181')[0];
+    const withdrawal = fixtures.requests.buildWithdrawal({
+      sponsorAddress: '0x99bd3a5A045066F1CEf37A0A952DFa87Af9D898E',
+    });
+    const requests: GroupedRequests = {
+      apiCalls: [apiCall],
+      withdrawals: [withdrawal],
+    };
+    const [logs, res] = blocking.blockRequestsWithWithdrawals([[], requests]);
+    expect(logs).toEqual([]);
+    expect(res.apiCalls.length).toEqual(1);
+    expect(res.apiCalls[0].id).toEqual(apiCall.id);
+    expect(res.withdrawals.length).toEqual(1);
+    expect(res.withdrawals[0].id).toEqual(withdrawal.id);
   });
 });
--- packages/airnode-node/src/evm/requests/blocking.ts
+++ packages/airnode-node/src/evm/requests/blocking.ts
@@ -1,4 +1,39 @@
-import { GroupedRequests, LogsData } from '../../types';
+import keyBy from 'lodash/keyBy';
+import { logger, PendingLog } from '@api3/airnode-utilities';
+import { ApiCall, Request, GroupedRequests, LogsData } from '../../types';
+
+interface ApiCallsWithLogs {
+  readonly apiCalls: Request<ApiCall>[];
+  readonly logs: PendingLog[];
+}
+
+export function blockRequestsWithWithdrawals([
+  prevLogs,
+  requests,
+]: LogsData<GroupedRequests>): LogsData<GroupedRequests> {
+  // API calls related to a wallet with a pending withdrawal cannot be processed
+  const withdrawalsBySponsorAddress = keyBy(requests.withdrawals, 'sponsorAddress');
+  const initialState: ApiCallsWithLogs = { logs: [], apiCalls: [] };
+
+  const { logs, apiCalls } = requests.apiCalls.reduce((acc, apiCall) => {
+    const pendingWithdrawal = withdrawalsBySponsorAddress[apiCall.sponsorAddress];
+    if (!pendingWithdrawal) {
+      return { ...acc, apiCalls: [...acc.apiCalls, apiCall] };
+    }
+
+    const warningLog = logger.pend(
+      'WARN',
+      `Dropping Request ID:${apiCall.id} as it has a pending Withdrawal ID:${pendingWithdrawal.id}`
+    );
+
+    return {
+      ...acc,
+      logs: [...acc.logs, warningLog],
+    };
+  }, initialState);
+
+  return [[...prevLogs, ...logs], { ...requests, apiCalls }];
+}
 
 export const blockRequests = (requests: GroupedRequests): LogsData<GroupedRequests> => {
   return [[], requests];
