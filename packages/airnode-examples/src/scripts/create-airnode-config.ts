806e699334c26a76f6ecc04229dc05287a614601
Derek Croote <dcroote@users.noreply.github.com>
2023-10-08 23:54:37 -0700

    fix(examples): check for AirnodeRrp deployment in localhost flow

--- packages/airnode-examples/src/scripts/create-airnode-config.ts
+++ packages/airnode-examples/src/scripts/create-airnode-config.ts
@@ -1,7 +1,24 @@
-import { readIntegrationInfo, runAndHandleErrors } from '../';
+import { readIntegrationInfo, runAndHandleErrors, getDeployedContract, cliPrint } from '../';
 
 const main = async () => {
   const integrationInfo = readIntegrationInfo();
+
+  // If using the localhost network, check that AirnodeRrp was deployed
+  if (integrationInfo.network === 'localhost') {
+    try {
+      await getDeployedContract('@api3/airnode-protocol/contracts/rrp/AirnodeRrpV0.sol');
+    } catch (e) {
+      if (e instanceof Error && (e.message.includes('ENOENT') || e.message.includes('invalid contract address'))) {
+        cliPrint.error(
+          'AirnodeRrpV0 contract deployment not found. Please follow the RRP deployment ' +
+            'instructions in the README before running this command.'
+        );
+        process.exit(1);
+      }
+      throw e;
+    }
+  }
+
   // Import the "create-config" file from the chosen integration. See the respective "create-config.ts" file for
   // details.
   const createConfig = await import(`../../integrations/${integrationInfo.integration}/create-config.ts`);
