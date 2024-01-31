c9a94bf8074bdb4251c060c2c10bfdece861ac37
Derek Croote <dcroote@users.noreply.github.com>
2024-01-31 07:33:31 -0800

    fix: remove unnecessary eslint-disable-line (#1942)

--- packages/airnode-protocol/src/index.ts
+++ packages/airnode-protocol/src/index.ts
@@ -93,9 +93,6 @@ export {
   FailedRequestEvent,
   RequestedWithdrawalEvent,
   FulfilledWithdrawalEvent,
-} from './contracts/rrp/AirnodeRrpV0'; // eslint-disable-line import/no-unresolved
-// NOTE: there seems to be an issue with eslint-plugin-import
-// not being able to find the above files. Hopefully a future version (> 2.26.0)
-// fixes this. https://github.com/api3dao/airnode/pull/1004#issuecomment-1096152730
+} from './contracts/rrp/AirnodeRrpV0';
 
 export { TypedEventFilter } from './contracts/common';
