06b193e475f820e7c757a3d810ef6b38a7d7e6bd
Derek Croote <dcroote@users.noreply.github.com>
2024-02-01 21:50:43 -0800

    Remove outdated hardhat account config

--- packages/airnode-operation/hardhat.config.ts
+++ packages/airnode-operation/hardhat.config.ts
@@ -7,15 +7,6 @@ const config: HardhatUserConfig = {
     compilers: [{ version: '0.6.12', settings: {} }],
   },
   networks: {
-    hardhat: {
-      accounts: {
-        // These accounts are used to deploy contracts and fund all Airnode
-        // related accounts. Make sure they have more than enough ETH to
-        // do this (1m ETH each).
-        accountsBalance: '1000000000000000000000000',
-        count: 100,
-      },
-    },
     localhost: {
       url: 'http://127.0.0.1:8545/',
     },
